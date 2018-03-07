90a63e15c366432f713b7484b082ed585ebb65df
Matt Brennan <matt@153.io>
2018-03-07 16:51:33 +0000

    do unspeakable things in the require hook

--- packages/x-build/register.js
+++ packages/x-build/register.js
@@ -1,23 +1,46 @@
+// this file commits all kinds of sin. i'm sorry.
+
 const {addHook} = require('pirates');
 const pkgUp = require('pkg-up');
 const fs = require('fs');
+const path = require('path');
+const {default: compile} = require('./lib/compile');
+const wait = require('blocking-await');
 
-const readPkgJson = file => JSON.parse(
+const readJSON = file => JSON.parse(
 	fs.readFileSync(
-		pkgUp.sync(file),
+		file,
 		'utf8'
 	)
 );
 
-function compile(code) {
-	//TODO use compiler. ensure the sync bits of it are accessible
+// block the main thread until the compile promise resolves.
+//
+// yeah, i feel as icky about this as you. but the actual require hook
+// needs to be synchronous and i'd rather have this one little bit of
+// bleugh than rewrite the compiler to by sync mmkay.
+const targetMap = wait(
+	compile(process.cwd())
+);
+
+// TODO component package.json main points to src/index.tsx. we map that all the
+// way through targetMap (which requires shipping the precompile targetMap)
+// to the target file
+
+function getCompiled(code, file) {
+	console.log(file);
 	return '';
 }
 
-const removeHook = addHook(compile, {
+const removeHook = addHook(getCompiled, {
 	matcher(file) {
-		const pkg = readPkgJson(file);
-		return pkg.name.startsWith('@financial-times/x-')
+		const pkgPath = pkgUp.sync(file)
+		const base = path.dirname(pkgPath);
+		const pkg = readJSON(pkgPath);
+
+		return file.match(/\.jsx?$/)
+			&& base !== process.cwd()
+			&& pkg.name.startsWith('@financial-times/x-')
 			&& pkg.name !== '@financial-times/x-build';
 	},
 
