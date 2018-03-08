2603d248375c20528a74b877345a66c0026ad09a
Matt Brennan <matt@153.io>
2018-03-08 11:38:14 +0000

    add react test

---
+++ packages/x-consumer-test-react/index.js
@@ -0,0 +1,7 @@
+const {renderToString} = require('react-dom/server');
+const {createElement: h} = require('react');
+const Testbed = require('@financial-times/x-testbed');
+
+console.log(renderToString(
+	h(Testbed, {foo: 'hello '})
+));
