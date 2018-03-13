a01d2284a52c41ca13f60e76a191d66c631698f4
Matt Hinchliffe <matt.hinchliffe@ft.com>
2018-03-13 17:01:57 +0000

    Use engine module from within storybook component workbench

--- packages/x-engine/src/engines.ts
+++ packages/x-engine/src/engines.ts
@@ -1,24 +1,11 @@
-const engines = new Map();
+const engines = new Set([ 'react', 'preact', 'vhtml' ]);
 
-engines.set('react', (): Function => {
-	const { createElement } = require('react');
-	return createElement;
-});
+export default (runtime: string): Function => {
+	const engine = engines.has(runtime) ? require('./engines/' + runtime) : null;
 
-engines.set('preact', (): Function => {
-	const { createElement } = require('preact');
-	return createElement;
-});
-
-engines.set('vhtml', (): Function => {
-	return require('vhtml');
-});
-
-export default (engine: string): Function => {
-	if (engines.has(engine)) {
-		return engines.get(engine)();
+	if (engine) {
+		return engine;
 	} else {
-		const available = Array.from(engines.keys()).join(', ');
-		throw new Error(`"${engine}" is not a valid engine, available engines are: ${available}`);
+		throw new Error(`"${runtime}" is not a valid runtime engine`);
 	}
 };
