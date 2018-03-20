b9eac0293a47b0bf0eab91da951ca40ebb0af719
Matt Hinchliffe <matt.hinchliffe@ft.com>
2018-03-20 12:32:07 +0000

    Export generic React-like dom builder interface in engine

--- packages/x-engine/src/index.ts
+++ packages/x-engine/src/index.ts
@@ -1,7 +1,7 @@
+import { SFC, Attributes, ReactNode, ReactElement } from 'react';
 import { join } from 'path';
 import deepGet from './concerns/deep-get';
 import readJSON from './concerns/read-json';
-import engines from './engines';
 
 const pkg = readJSON(join(process.cwd(), 'package.json'));
 
@@ -11,4 +11,22 @@ if (runtime === undefined) {
 	throw new Error('x-engine requires a server runtime engine to be specified');
 }
 
-export default engines(runtime);
+// Force the engine we load dynamically to have a generic, dom-builder interface
+export type Factory = <P>(
+	type: SFC<P> | string,
+	props?: Attributes & P | null,
+	...children: ReactNode[]
+) => ReactElement<P>;
+
+let engine;
+
+try {
+	engine = require('./engines/' + runtime) as Factory;
+} catch (error) {
+	// TODO: A friendlier error message
+	throw new Error(`Failed to load "${runtime}"`);
+}
+
+export const h = engine;
+
+export type Component = SFC;
