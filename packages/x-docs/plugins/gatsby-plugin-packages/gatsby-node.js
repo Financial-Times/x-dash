77fe5bc1236ca12be7d7b20d8ab68d5c6eb456a1
Matt Brennan <matt@153.io>
2018-03-21 16:15:54 +0000

    generate nodes and pages for packages

---
+++ packages/x-docs/plugins/gatsby-plugin-packages/gatsby-node.js
@@ -0,0 +1,67 @@
+const getComponents = require('@financial-times/x-workbench');
+const fs = require('fs-extra');
+const paramCase = require('param-case');
+const path = require('path');
+const findUp = require('find-up');
+
+exports.createPages = ({boundActionCreators, graphql}) => {
+	const {createPage} = boundActionCreators;
+	const packageTemplate = path.resolve(`src/templates/package.js`);
+
+	return graphql(`
+		{
+			allPackage {
+				edges {
+					node {
+						pkgJson {
+							name
+						}
+					}
+				}
+			}
+		}
+	`).then(result => {
+		if (result.errors) {
+			return Promise.reject(result.errors);
+		}
+
+		result.data.allPackage.edges.forEach(({node}) => {
+			const unscoped = path.basename(node.pkgJson.name);
+
+			createPage({
+				path: `/package/${unscoped}`,
+				component: packageTemplate,
+				context: node.pkgJson,
+			});
+		});
+	});
+};
+
+exports.sourceNodes = async ({boundActionCreators}) => {
+	const {createNode} = boundActionCreators;
+
+	const root = path.resolve(
+		await findUp('lerna.json'),
+		'../packages'
+	);
+
+	const packages = await fs.readdir(root);
+
+	packages.forEach(pkg => {
+		const dir = path.resolve(root, pkg);
+		const pkgJson = require(path.resolve(dir, 'package.json'));
+		const id = `package ${pkgJson.name}`;
+		const contentDigest = pkgJson.version;
+
+		createNode({
+			id,
+			parent: null,
+			children: [],
+			internal: {
+				contentDigest,
+				type: 'Package'
+			},
+			pkgJson,
+		});
+	});
+};
