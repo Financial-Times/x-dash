279a97822fc385f71f2cec1811966b99e399f64f
Matt Hinchliffe <matt.hinchliffe@ft.com>
2018-03-22 13:51:37 +0000

    Support title and standfirst variants

--- packages/x-teaser/src/Title.jsx
+++ packages/x-teaser/src/Title.jsx
@@ -1,9 +1,9 @@
 const h = require('@financial-times/x-engine');
 
-module.exports = ({ title, url, premium }) => (
+module.exports = ({ title, titleVariant, useTitleVariant, url, premium }) => (
 	<div className="o-teaser__heading">
 		<a href={url} className="js-teaser-heading-link" data-trackable="heading-link">
-			{title}
+			{useTitleVariant && titleVariant ? titleVariant : title}
 		</a>
 		{premium ? (
 			<span className="o-labels o-labels--premium" aria-label="Premium content">
