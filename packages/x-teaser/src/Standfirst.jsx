3de2d25ab6704f9a6716dd76815fd50a377d8206
Matt Hinchliffe <matt.hinchliffe@ft.com>
2018-03-22 13:51:37 +0000

    Support title and standfirst variants

--- packages/x-teaser/src/Standfirst.jsx
+++ packages/x-teaser/src/Standfirst.jsx
@@ -1,3 +1,7 @@
 const h = require('@financial-times/x-engine');
 
-module.exports = ({ standfirst }) => <p className="o-teaser__standfirst">{standfirst}</p>;
+module.exports = ({ standfirst, standfirstVariant, useStandfirstVariant }) => (
+	<p className="o-teaser__standfirst">
+		{useStandfirstVariant && standfirstVariant ? standfirstVariant : standfirst}
+	</p>
+);
