dd77ccd8df861aa819f4f4d539eb882eda1fd916
Matt Hinchliffe <matt@maketea.co.uk>
2018-03-24 15:35:12 +0000

    Only attempt to load engine config when webpack plugin is called

--- packages/x-engine/src/webpack.js
+++ packages/x-engine/src/webpack.js
@@ -3,23 +3,22 @@ const loadManifest = require('./concerns/load-manifest');
 const formatConfig = require('./concerns/format-config');
 const resolveModule = require('./concerns/resolve-module');
 
-// 1. try to load the application's package manifest
-const pkg = loadManifest();
-
-// 2. if we have the manifest then find the engine configuration
-const raw = deepGet(pkg, 'x-dash.engine.browser');
-
-if (raw === null) {
-	throw new Error('x-engine requires a browser runtime to be specified');
-}
-
-// 3. format the configuration we've loaded
-const config = formatConfig(raw);
-
-// 4. if this module is a linked dependency then resolve Webpack instance to CWD
-const webpack = resolveModule('webpack');
-
 module.exports = function() {
+	// 1. try to load the application's package manifest
+	const pkg = loadManifest();
+
+	// 2. if we have the manifest then find the engine configuration
+	const raw = deepGet(pkg, 'x-dash.engine.browser');
+
+	if (raw === null) {
+		throw new Error('x-engine requires a browser runtime to be specified');
+	}
+
+	// 3. format the configuration we've loaded
+	const config = formatConfig(raw);
+
+	// 4. if this module is a linked dependency then resolve Webpack instance to CWD
+	const webpack = resolveModule('webpack');
 	// The define plugin performs direct text replacement
 	// <https://webpack.js.org/plugins/define-plugin/>
 	return new webpack.DefinePlugin({
