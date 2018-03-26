9cc5d1d68b87c3da7be600b0e1132e42b40f8fc4
Matt Brennan <matt@153.io>
2018-03-26 21:01:26 +0100

    hero lick of paint

--- packages/x-docs/src/components/hero/index.js
+++ packages/x-docs/src/components/hero/index.js
@@ -3,7 +3,7 @@ import styles from './hero.module.scss';
 import XLogo from '@financial-times/x-logo';
 import c from 'classnames'
 
-const Hero = () => <div className={styles.header}>
+const Hero = () => <div className={styles.wrapper}>
 	<div className={styles.container}>
 		<div className={c('o-techdocs-hero', styles.hero)}>
 			<div className={styles.logo}>
@@ -11,7 +11,19 @@ const Hero = () => <div className={styles.header}>
 			</div>
 
 			<div className={styles.intro}>
-				<h1 className="o-techdocs-hero__title">x-dash</h1>
+				<h1 className={c("o-techdocs-hero__title", styles.title)}>x-dash</h1>
+
+				<p className={c('o-techdocs-hero__summary', styles.summary)}>
+					Shared frontend markup for FT.com apps.
+				</p>
+				<p className={c('o-techdocs-hero__summary', styles.summary)}>
+					Adaptable components and transparent build tooling.
+				</p>
+				<div className='o-techdocs-hero__actions'>
+					<a href="#get-started" class="o-buttons o-buttons--big o-buttons--inverse" title="Developer guide">
+						Get started
+					</a>
+				</div>
 			</div>
 		</div>
 	</div>
