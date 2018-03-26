0f0058bd581ed9b62b87ed66049555acfa692544
Matt Brennan <matt@153.io>
2018-03-26 13:32:44 +0100

    add logo

--- packages/x-docs/src/pages/index.js
+++ packages/x-docs/src/pages/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import XLogo from '@financial-times/x-logo';
 
 const IndexPage = () => (
   <div>
+    <XLogo />
+
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
