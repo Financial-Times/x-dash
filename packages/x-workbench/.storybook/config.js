bcafd4c97dce81f4cefbfb8788dfc1bc936a685c
Matt Brennan <matt@153.io>
2018-03-26 10:09:37 +0100

    fail to get stories loading in gatsby because of module resolution, again

--- packages/x-workbench/.storybook/config.js
+++ packages/x-workbench/.storybook/config.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import {configure} from '@storybook/react';
 import buildStory from './build-story';
-
-export const loadStories = () => [
-	require('../../x-teaser/stories'),
-].map(stories => [].concat(stories).map(buildStory));
+import loadStories from './load-stories';
 
 configure(loadStories, module);
