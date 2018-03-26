512f485c06bc4c4de12bbf45de95f8f38f4a428e
Matt Brennan <matt@153.io>
2018-03-26 16:11:10 +0100

    actually filter fixture

--- packages/x-docs/src/templates/story.js
+++ packages/x-docs/src/templates/story.js
@@ -1,11 +1,12 @@
 const React = require('react');
 const loadStories = require('@financial-times/x-workbench/.storybook/load-stories');
+const pick = require('lodash.pick')
 
 const stories = loadStories();
 
 module.exports = ({pathContext: {storyCategory, sitemap: {title}}}) => {
 	const story = stories[storyCategory];
 	return story.stories[title]({
-		createProps: () => Object.assign({}, story.fixture)
+		createProps: allowedProps => pick(story.fixture, allowedProps),
 	});
 };
