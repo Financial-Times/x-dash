7926d2f2d9a1bab6215fb8d9c2aae40cdd151578
Matt Brennan <matt@153.io>
2018-03-27 16:13:16 +0100

    stuff

--- packages/x-docs/src/components/footer/index.js
+++ packages/x-docs/src/components/footer/index.js
@@ -6,8 +6,10 @@ import parrot from './parrot.gif';
 export default () => <footer className={c("o-techdocs-footer", styles.footer)}>
 	<div className="o-techdocs-footer__inner">
 		<p className="o-techdocs-footer__secondary">
-			<a className={styles.githubLink} href="http://github.com/financial-times/ft-origami">View project on GitHub</a>
-			<img tabIndex="-1" src={parrot} width={64} alt='Party Parrot' className={styles.parrot} />
+			<a className={styles.githubLink} href="https://github.com/financial-times/x-dash">
+				x-dash on Github
+			</a>
+			<img src={parrot} width={64} alt='Party Parrot' className={styles.parrot} />
 		</p>
 		<p>&copy; THE FINANCIAL TIMES LTD. FT and 'Financial Times' are trademarks of The Financial Times Ltd.</p>
 	</div>
