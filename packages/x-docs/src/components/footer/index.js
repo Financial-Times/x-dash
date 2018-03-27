2cf26370506256fac9cac1c8ed2237ccee748a90
Matt Brennan <matt@153.io>
2018-03-27 15:29:17 +0100

    a bit of silliness

--- packages/x-docs/src/components/footer/index.js
+++ packages/x-docs/src/components/footer/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './footer.module.scss';
 import c from 'classnames';
+import parrot from './parrot.gif';
 
 export default () => <footer className={c("o-techdocs-footer", styles.footer)}>
 	<div className="o-techdocs-footer__inner">
-		<p className="o-techdocs-footer__secondary"><a href="http://github.com/financial-times/ft-origami">View project on GitHub</a></p>
+		<p className="o-techdocs-footer__secondary">
+			<a className={styles.githubLink} href="http://github.com/financial-times/ft-origami">View project on GitHub</a>
+			<img tabIndex="-1" src={parrot} width={64} alt='Party Parrot' className={styles.parrot} />
+		</p>
 		<p>&copy; THE FINANCIAL TIMES LTD. FT and 'Financial Times' are trademarks of The Financial Times Ltd.</p>
 	</div>
 </footer>
