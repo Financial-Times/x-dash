3ed9da3087a1d3fc424e1ce3e7eeae5e3f7700ca
Matt Brennan <matt@153.io>
2018-03-28 14:38:45 +0100

    slate & asphalt header

--- packages/x-docs/src/components/header/index.js
+++ packages/x-docs/src/components/header/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Link from 'gatsby-link'
 import c from 'classnames';
 import styles from './header.module.scss';
+import {splashBackground} from '../../styles/splash/splash.module.scss';
 
 const Header = ({splash}) => <header
-	className={c('o-header-services', {[styles.splash]: splash})}
+	className={c('o-header-services', splashBackground, {[styles.splash]: splash})}
 	data-o-component="o-header"
 >
-	<div className={c('o-header-services__top o-header-services__container', {[styles.splash]: splash})}>
+	<div className={c('o-header-services__top o-header-services__container', styles.splash)}>
 		<div className="o-header-services__ftlogo"></div>
 		<div className="o-header-services__title">
 			<h1 className="o-header-services__product-name">
