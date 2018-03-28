af0b5df9af24d636f7003082e8a43b8f5c4ca853
Matt Brennan <matt@153.io>
2018-03-28 17:23:12 +0100

    hipster header

--- packages/x-docs/src/components/header/index.js
+++ packages/x-docs/src/components/header/index.js
@@ -1,28 +1,37 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import c from 'classnames';
+import XLogo from '@financial-times/x-logo';
+import Search from '../search';
+
 import styles from './header.module.scss';
 import {splashBackground} from '../../styles/splash/splash.module.scss';
 
 const Header = ({splash}) => <header
-	className={c('o-header-services', splashBackground, {[styles.splash]: splash})}
-	data-o-component="o-header"
+	className={c(styles.header, {[splashBackground]: !splash})}
 >
-	<div className={c('o-header-services__top o-header-services__container', styles.splash)}>
-		<div className="o-header-services__ftlogo"></div>
-		<div className="o-header-services__title">
-			<h1 className="o-header-services__product-name">
-				<Link to="/">x-dash</Link>
-			</h1>
-		</div>
-		<div className="o-header-services__related-content">
-			<a className="o-header-services__related-content-link" href="https://origami.ft.com">
-				Origami
-			</a>
-			<Link className="o-header-services__related-content-link" to="/components">
-				Components
-			</Link>
-		</div>
+	<div className={styles.left}>
+		<span class={styles.item}>
+			<Search />
+		</span>
+	</div>
+
+		{!splash &&
+			<div className={styles.center}>
+				<Link className={styles.link} to="/">
+					<XLogo className={styles.logo} small />
+					<span className={styles.logoText}>x-dash</span>
+				</Link>
+			</div>
+		}
+
+	<div className={styles.right}>
+		<a className={styles.link} href="https://origami.ft.com">
+			Origami
+		</a>
+		<Link className={styles.link} to="/components">
+			Components
+		</Link>
 	</div>
 </header>
 
---
+++ packages/x-docs/src/components/search/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import styles from './search.module.scss';
+
+export default () => <input className={styles.search} type='search' placeholder='Search...' />
