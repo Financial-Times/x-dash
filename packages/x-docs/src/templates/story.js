bb21672d2099f479b55ccb2affdd10a35546876a
Matt Brennan <matt@153.io>
2018-03-29 10:49:52 +0100

    load stylesheets for demos

--- packages/x-docs/src/templates/story.js
+++ packages/x-docs/src/templates/story.js
@@ -1,8 +1,23 @@
-const React = require('react');
-const loadStories = require('@financial-times/x-workbench/.storybook/load-stories');
-const pick = require('lodash.pick')
+import React, {Fragment} from 'react';
+import loadStories from '@financial-times/x-workbench/.storybook/load-stories';
+import pick from 'lodash.pick';
+import Helmet from 'react-helmet';
+import url from 'url';
 
 const stories = loadStories();
+
+const formatModulesString = deps => Object.keys(deps).map(
+	dep => `${dep}@${deps[dep]}`
+).join(',');
+
+const formatBuildServiceUrl = deps => url.format({
+	protocol: 'https',
+	host: 'www.ft.com',
+	pathname: '__origami/service/build/v2/bundles/css',
+	query: {
+		modules: formatModulesString(deps)
+	}
+});
 
 module.exports = ({pathContext: {componentName, componentBook, sitemap: {title}}}) => {
 	console.log({
@@ -10,7 +25,14 @@ module.exports = ({pathContext: {componentName, componentBook, sitemap: {title}}
 		componentBook
 	})
 	const story = stories[componentName][componentBook];
-	return story.stories[title]({
-		createProps: allowedProps => pick(story.fixture, allowedProps),
-	});
+
+	return <Fragment>
+		<Helmet>
+			<link rel='stylesheet' href={formatBuildServiceUrl(story.origamiDependencies)} />
+		</Helmet>
+
+		{story.stories[title]({
+			createProps: allowedProps => pick(story.fixture, allowedProps),
+		})}
+	</Fragment>;
 };
