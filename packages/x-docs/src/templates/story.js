2f8364202ef415ef2a343fe708d197a506e78eff
Matt Brennan <matt@153.io>
2018-03-29 11:56:37 +0100

    load stories in shadow dom

--- packages/x-docs/src/templates/story.js
+++ packages/x-docs/src/templates/story.js
@@ -3,6 +3,10 @@ import loadStories from '@financial-times/x-workbench/.storybook/load-stories';
 import pick from 'lodash.pick';
 import Helmet from 'react-helmet';
 import url from 'url';
+import Shadow from 'react-shadow';
+
+import styles from './story.module.scss';
+import Content from '../components/content';
 
 const stories = loadStories();
 
@@ -16,21 +20,29 @@ const formatBuildServiceUrl = deps => url.format({
 	pathname: '__origami/service/build/v2/bundles/css',
 	query: {
 		modules: formatModulesString(deps)
-	}
+	},
+	hash: '.css', // fake extension so react-shadow knows how to load it
 });
 
 module.exports = ({pathContext: {componentName, componentBook, sitemap: {title}}}) => {
 	const story = stories[componentName][componentBook];
 
-	return <div>
+	return <Content>
 		<h1>{title}</h1>
 
 		<Helmet>
-			<link rel='stylesheet' href={formatBuildServiceUrl(story.origamiDependencies)} />
+			{/* component prolly needs fonts but apparently you can't load those in shadow dom */}
+			<link rel='stylesheet' href={formatBuildServiceUrl({
+				'o-fonts': '^3'
+			})} />
 		</Helmet>
 
-		{story.stories[title]({
-			createProps: allowedProps => pick(story.fixture, allowedProps),
-		})}
-	</div>;
+		<Shadow include={[formatBuildServiceUrl(story.origamiDependencies)]}>
+			<div className={styles.shadow}>
+				{story.stories[title]({
+					createProps: allowedProps => pick(story.fixture, allowedProps),
+				})}
+			</div>
+		</Shadow>
+	</Content>;
 };
