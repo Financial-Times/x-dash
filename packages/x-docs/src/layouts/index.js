61f0de9824d38f29cdd14d32f5639d6ad9f14618
Matt Brennan <matt@153.io>
2018-03-29 14:25:08 +0100

    full height footer on all pages

--- packages/x-docs/src/layouts/index.js
+++ packages/x-docs/src/layouts/index.js
@@ -1,16 +1,19 @@
 import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
+import c from 'classnames';
 
 import BasicLayout from './basic';
 import Header from '../components/header';
 import Sidebar from '../components/sidebar';
 import Footer from '../components/footer';
 
+import {wrapper, main} from './wrapper.module.scss';
+
 const TemplateWrapper = ({children, ...props}) => <BasicLayout {...props}>{({sidebarTree, headerTree}) =>
-	<Fragment>
+	<div className={wrapper}>
 		<Header tree={headerTree} />
 
-		<div className='o-techdocs-container'>
+		<div className={c('o-techdocs-container', main)}>
 			<div className='o-techdocs-layout'>
 				<Sidebar tree={sidebarTree} />
 
@@ -21,7 +24,7 @@ const TemplateWrapper = ({children, ...props}) => <BasicLayout {...props}>{({sid
 		</div>
 
 		<Footer />
-	</Fragment>
+	</div>
 }</BasicLayout>;
 
 TemplateWrapper.propTypes = {
