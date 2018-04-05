e7fd689af3e92b507b66951f815b37aa4438b80c
Matt Hinchliffe <matt.hinchliffe@ft.com>
2018-04-05 10:30:21 +0100

    Clean up unused engine package examples

--- packages/x-engine/test/react/test.js
+++
@@ -1,5 +0,0 @@
-const { renderToString } = require('react-dom/server');
-const { Teaser } = require('@financial-times/x-teaser');
-const data = require('../teaser');
-
-console.log(renderToString(Teaser(data)));
