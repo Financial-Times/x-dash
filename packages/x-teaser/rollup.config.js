15c24750e744e986f30a5783cd12227af633f3fd
Matt Hinchliffe <matt@maketea.co.uk>
2018-04-14 09:14:29 +0100

    Setup rollup config to compile teaser module into distributable bundles  🐿 v2.8.0

---
+++ packages/x-teaser/rollup.config.js
@@ -0,0 +1,44 @@
+import buble from 'rollup-plugin-buble';
+import commonjs from 'rollup-plugin-commonjs';
+import pkg from './package.json';
+
+const input = 'src/Teaser.jsx';
+
+const external = [
+	'@financial-times/x-engine',
+	'dateformat'
+];
+
+const bubleOptions = { objectAssign: 'Object.assign', jsx: 'h' };
+
+const commonPlugin = commonjs({ extensions: ['.js', '.jsx'] });
+
+export default [
+	{
+		input,
+		output: {
+			file: pkg.module,
+			format: 'es'
+		},
+		external,
+		plugins: [buble({ ...bubleOptions, target: { node: 6 } }), commonPlugin]
+	},
+	{
+		input,
+		output: {
+			file: pkg.main,
+			format: 'cjs'
+		},
+		external,
+		plugins: [buble({ ...bubleOptions, target: { node: 6 } }), commonPlugin]
+	},
+	{
+		input,
+		output: {
+			file: pkg.browser,
+			format: 'cjs'
+		},
+		external,
+		plugins: [buble({ ...bubleOptions, target: { ie: 11 } }), commonPlugin]
+	}
+];
