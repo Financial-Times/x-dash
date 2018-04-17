3965b43eae42ff1b6b9097904e4bba2ef8b2c42d
Matt Brennan <matt@153.io>
2018-04-17 11:16:22 +0100

    move snapshot tests to top level so new components get them automatically  🐿 v2.8.0

---
+++ __tests__/snapshots.test.js
@@ -0,0 +1,27 @@
+const renderer = require('react-test-renderer');
+const fs = require('fs');
+const path = require('path');
+
+const packagesDir = path.resolve(__dirname, '../packages');
+
+for(const pkg of fs.readdirSync(packagesDir)) {
+	const pkgDir = path.resolve(packagesDir, pkg)
+	const storiesDir = path.resolve(pkgDir, 'stories');
+
+	if(fs.existsSync(storiesDir)) {
+		const { name, stories, component } = require(storiesDir);
+		const { presets } = require(pkgDir);
+
+		describe(name, () => {
+			for (const { title, data } of stories) {
+				for (const [ name, options ] of Object.entries(presets)) {
+					it(`renders a ${name} ${title} teaser`, () => {
+						const props = { ...data, ...options };
+						const tree = renderer.create(component(props)).toJSON();
+						expect(tree).toMatchSnapshot();
+					});
+				}
+			}
+		});
+	}
+}
