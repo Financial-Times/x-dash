fa7928873c12b85988fa7dc70f1f360789ac9a70
Matt Brennan <matt@153.io>
2018-04-24 17:27:59 +0100

    split components into separate lerna namespace, use lerna.json globs for gatsby file stuff  🐿 v2.8.0

--- packages/x-docs/plugins/gatsby-plugin-doc-pages/gatsby-node.js
+++ packages/x-docs/plugins/gatsby-plugin-doc-pages/gatsby-node.js
@@ -1,20 +1,37 @@
 const path = require('path');
 const titleCase = require('title-case');
-const components = require('@financial-times/x-workbench');
 const findUp = require('find-up');
 const removeMarkdown = require('remove-markdown');
+const minimatch = require('minimatch');
+
+const repoBase = path.dirname(findUp.sync('lerna.json'));
 
 const closestDir = (dir, match) => !dir || dir === '/'
 	? false
-	: dir.match(match)
+	: (match instanceof RegExp ? dir.match(match) : minimatch(dir, match))
 		? dir
 		: closestDir(path.dirname(dir), match);
 
 exports.onCreateNode = ({node}) => {
 	if(node.internal.type === 'MarkdownRemark') {
+		const {packages: packageGlobs} = require(
+			path.resolve(repoBase, 'lerna.json')
+		);
+
+		const absoluteGlobs = packageGlobs.map(
+			g => path.resolve(repoBase, g)
+		);
+
+		const fullGlob = absoluteGlobs.length > 1
+			? `{${absoluteGlobs.join(',')}}`
+			: absoluteGlobs[0];
+
 		const docsFolder = closestDir(node.fileAbsolutePath, /(src)?\/docs$/);
-		const pkgFolder = closestDir(docsFolder || node.fileAbsolutePath, /packages\/[^\/]+$/);
+		const pkgFolder = closestDir(docsFolder || node.fileAbsolutePath, fullGlob);
+
 		const pkg = path.basename(pkgFolder);
+		const root = path.basename(path.dirname(pkgFolder));
+
 		const rel = path.relative(
 			docsFolder || pkgFolder,
 			node.fileAbsolutePath
@@ -26,8 +43,7 @@ exports.onCreateNode = ({node}) => {
 				: pkg;
 		}
 
-		//TODO matt there has to be a better way than this
-		const namespace = pkg in components
+		const namespace = root === 'components'
 			? 'Components'
 			: 'Packages';
 
