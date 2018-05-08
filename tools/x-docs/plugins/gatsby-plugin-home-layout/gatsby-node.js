755bcfa83328adda6ac8277d0cd0a42e9b1fbb7c
Matt Brennan <matt@153.io>
2018-05-08 11:30:28 +0100

    make components index a react page and generate sitemaps for those  🐿 v2.8.0

--- tools/x-docs/plugins/gatsby-plugin-home-layout/gatsby-node.js
+++ tools/x-docs/plugins/gatsby-plugin-home-layout/gatsby-node.js
@@ -1,8 +1,22 @@
+const titleCase = require('title-case');
+
 exports.onCreatePage = ({page, boundActionCreators}) => {
 	const { createPage } = boundActionCreators;
 
 	if(page.path === '/') {
 		page.layout = 'splash';
-		createPage(page);
 	}
+
+	if(!page.context.sitemap && page.path !== '/dev-404-page/') {
+		const breadcrumbs = page.path.split('/')
+			.filter(part => part !== '')
+			.map(titleCase);
+
+		page.context.sitemap = {
+			breadcrumbs,
+			title: breadcrumbs[breadcrumbs.length - 1]
+		};
+	}
+
+	createPage(page);
 }
---
+++ tools/x-docs/src/pages/components/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ComponentsPage = () => <div>
+	<h1>yeah hello</h1>
+</div>;
+
+export default ComponentsPage;
