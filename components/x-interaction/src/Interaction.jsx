6667bc287660d2aa49182745f898602a0a11a653
Matt Brennan <matt@153.io>
2018-05-09 16:47:33 +0100

    static render for static runtimes  🐿 v2.8.0

--- components/x-interaction/src/Interaction.jsx
+++ components/x-interaction/src/Interaction.jsx
@@ -1,20 +1,36 @@
 import h, {Component} from '@financial-times/x-engine';
 import mapValues from 'lodash-es/mapValues';
 
-class Interaction extends Component {
+const InteractionRender = ({actions, state, initialProps, render}) => render(
+	state || initialProps,
+	actions
+);
+
+class InteractionClass extends Component {
 	constructor(props, ...args) {
 		super(props, ...args);
+
 		this.state = props.initialProps;
 		this.actions = mapValues(
 			props.actions,
-			func => (...args) => this.setState(state => func(state, ...args))
+			func => (...args) => this.setState(
+				state => func(state, ...args)
+			)
 		);
 	}
 
 	render() {
-		const {actions, render} = this.props;
-		return render(this.state, this.actions);
+		return <InteractionRender
+			{...this.props}
+			state={this.state}
+			actions={this.actions}
+		/>;
 	}
 }
+
+// use the class version for interactive runtimes and the static version for static runtimes
+const Interaction = Component
+	? InteractionClass
+	: InteractionRender;
 
 export { Interaction };
