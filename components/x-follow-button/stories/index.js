ab2db363a60e28f3585c7f7c2b2fffc495dd039f
Katerina <katerina.loschinina@ft.com>
2018-05-24 17:06:39 +0100

    create skeleton of stories  🐿 v2.8.0

---
+++ components/x-follow-button/stories/index.js
@@ -0,0 +1,14 @@
+const { FollowButton } = require('../');
+
+module.exports = {
+	name: 'x-follow-button',
+	component: FollowButton,
+	dependencies: {
+		'o-fonts': '^3.0.0',
+		'o-typography': '^5.5.0'
+	},
+	stories: [
+		require('./follow-button')
+	],
+	knobs: () => { return 'undefined' }
+};
