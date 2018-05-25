f0b64fce8d83c07926c73876591b722f43b84a47
Katerina <katerina.loschinina@ft.com>
2018-05-25 14:39:37 +0100

    rename isSelected back to setFollowButtonStateToSelected  🐿 v2.8.0

--- components/x-follow-button/stories/knobs.js
+++ components/x-follow-button/stories/knobs.js
@@ -8,28 +8,28 @@ module.exports = (data, { object, text, number, boolean, date, selectV2 }) => {
 	};
 
     const Button = {
-        buttonText() {
+        buttonText () {
             return text('Default text', data.buttonText, Groups.Button);
         },
-        alternateText() {
+        alternateText () {
             return text('Alternate text', data.alternateText, Groups.Button);
 		}
     }
 
 	const Flags = {
-		cacheablePersonalisedUrl() {
+		cacheablePersonalisedUrl () {
 			return boolean('cacheablePersonalisedUrl', data.cacheablePersonalisedUrl, Groups.Flags)
 		},
-        followPlusDigestEmail() {
+        followPlusDigestEmail () {
 			return boolean('followPlusDigestEmail', data.followPlusDigestEmail, Groups.Flags)
 		},
-        isSelected() {
+        setFollowButtonStateToSelected () {
 			return boolean('isSelected', data.isSelected, Groups.Flags)
 		}
 	}
 
 	const Variant = {
-		variant() {
+		variant () {
             return text('Variant', data.variant, Groups.Variant);
 		}
 	}
