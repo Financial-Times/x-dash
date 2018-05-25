d9f1a546fb1da7655acd77797a9c0fa1a62522c8
Roger Rajaratnam <roger@sourcier.uk>
2018-05-25 12:41:43 +0100

    Refactor button into smaller components and wire up knobs  🐿 v2.8.0

--- components/x-follow-button/src/FollowButton.jsx
+++ components/x-follow-button/src/FollowButton.jsx
@@ -1,5 +1,8 @@
 import h from '@financial-times/x-engine';
 
+import Input from './Input';
+import Button from './Button';
+
 const getFormAction = (conceptId, followPlusDigestEmail, setFollowButtonStateToSelected, CacheablePersonalisedUrl) => {
 	if (followPlusDigestEmail) {
 		return `/__myft/api/core/follow-plus-digest-email/${conceptId}?method=put`
@@ -10,32 +13,17 @@ const getFormAction = (conceptId, followPlusDigestEmail, setFollowButtonStateToS
 	}
 }
 
-const getButtonDataAlternateText = (alternateText, buttonText, isSelected, cacheablePersonalisedUrl) => {
-	return alternateText ? alternateText : buttonText ? buttonText : (isSelected && cacheablePersonalisedUrl) ? 'Add to myFT' : 'Added';
-}
-
-const getButtonTitle = (name, isSelected, cacheablePersonalisedUrl) => {
-	return `${(isSelected && cacheablePersonalisedUrl) ? 'Remove' : 'Add'} ${name} from myFT`
-}
-
-const getAlternateButtonTitle = (name, isSelected, cacheablePersonalisedUrl) => {
-	return `${(isSelected && cacheablePersonalisedUrl) ? 'Add' : 'Remove'} ${name} from myFT`
-}
-
-const getButtonText = (buttonText, isSelected, cacheablePersonalisedUrl) => {
-	return buttonText ? buttonText : (isSelected && cacheablePersonalisedUrl) ? 'Added' : 'Add to myFT';
-}
-
 const FollowButton = ({
 	conceptId,
 	followPlusDigestEmail,
 	isSelected,
 	cacheablePersonalisedUrl,
 	csrfToken,
+	// Button specific props
 	extraButtonClasses,
 	variant,
 	alternateText,
-	buttonText
+	buttonText,
 }) => (
 	<div>
 		<form
@@ -44,28 +32,19 @@ const FollowButton = ({
 			data-myft-ui="follow"
 			data-concept-id={conceptId}
 			action={ getFormAction(conceptId, followPlusDigestEmail, isSelected, cacheablePersonalisedUrl) }
-			data-myft-ui-variant={followPlusDigestEmail ? "followPlusDigestEmail" : null } >
-			<input
-				data-myft-csrf-token
-				value={csrfToken}
-				type="hidden"
-				name="token" />
-			<button
-				data-alternate-text={ getButtonDataAlternateText(alternateText, buttonText, isSelected, cacheablePersonalisedUrl) }
-				aria-label={ getButtonTitle(name, isSelected, cacheablePersonalisedUrl) }
-				title={ getButtonTitle(name, isSelected, cacheablePersonalisedUrl) }
-				data-alternate-label={ getAlternateButtonTitle(name, isSelected, cacheablePersonalisedUrl) }
-				aria-pressed="true"
-
-				class={`${extraButtonClasses}
-					n-myft-follow-button
-					n-myft-follow-button--${variant}`}
-				data-concept-id={conceptId}
-				data-trackable-context-messaging={ followPlusDigestEmail ? 'add-to-myft-plus-digest-button' : null }
-				data-trackable="follow"
-				type="submit">
-				{ getButtonText(buttonText, isSelected, cacheablePersonalisedUrl) }
-			</button>
+			data-myft-ui-variant={ followPlusDigestEmail ? "followPlusDigestEmail" : null } >
+			<Input value={csrfToken}
+				type='hidden'
+				name='token'
+				data-myft-csrf-token />
+			<Button conceptId={conceptId}
+				followPlusDigestEmail={followPlusDigestEmail}
+				isSelected={isSelected}
+				cacheablePersonalisedUrl={cacheablePersonalisedUrl}
+				extraButtonClasses={extraButtonClasses}
+				variant={variant}
+				alternateText={alternateText}
+				buttonText={buttonText} />
 		</form>
 	</div>
 );
