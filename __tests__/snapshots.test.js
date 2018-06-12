a85f7e1b97b32f0c70d3c6bae2d9b55280e063ac
Matt Brennan <matt@153.io>
2018-06-12 18:31:19 +0100

    sort the bloody snapshots out  🐿 v2.9.0

--- __tests__/snapshots.test.js
+++ __tests__/snapshots.test.js
@@ -18,11 +18,12 @@ for(const pkg of packageDirs) {
 	if(fs.existsSync(storiesDir)) {
 		const { package: pkg, stories, component } = require(storiesDir);
 		const { presets = {default: {}} } = require(pkgDir);
+		const name = path.basename(pkg.name);
 
 		describe(pkg.name, () => {
 			for (const { title, data } of stories) {
 				for (const [ preset, options ] of Object.entries(presets)) {
-					it(`renders a ${preset} ${title} ${pkg.name}`, () => {
+					it(`renders a ${preset} ${title} ${name}`, () => {
 						const props = { ...data, ...options };
 						const tree = renderer.create(component(props)).toJSON();
 						expect(tree).toMatchSnapshot();
