e1fecc95571fbc2cba31719cb4c40b978594dd9e
Matt Hinchliffe <matt@maketea.co.uk>
2018-06-14 15:01:04 +0100

    Make handlebars module into generic package and component loader
     🐿 v2.9.0

---
+++ packages/x-handlebars/concerns/resolved-require.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+/**
+ * Resolve Require
+ * @param {string} dependency
+ * @returns {any}
+ */
+module.exports = function (dependency) {
+	// Always try to resolve dependencies relative to the CWD in case this module has been
+	// symlinked into the consuming project, even without --preserve-symlinks
+	// <https://nodejs.org/api/cli.html#cli_preserve_symlinks>
+	const target = path.resolve(process.cwd(), 'node_modules', dependency);
+
+	try {
+		return require(target);
+	} catch (error) {
+		const cwd = process.cwd();
+		throw new Error(`Failed to require ${dependency} from ${cwd}`);
+	}
+};
--- packages/x-handlebars/helpers/teaser.js
+++
@@ -1,9 +0,0 @@
-const { Teaser, presets } = require('@financial-times/x-teaser');
-const extend = require('../utils/extend');
-
-module.exports = (context, { hash }) => {
-	const preset = presets[hash.preset];
-	const props = extend(context, preset, hash);
-
-	return Teaser(props);
-};
