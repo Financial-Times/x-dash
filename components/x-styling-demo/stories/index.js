46cce6c9780b3cae007cc818b2217f538c144f60
Matt Brennan <matt@153.io>
2018-06-20 09:55:35 +0100

    with knobs on it  🐿 v2.9.0

--- components/x-styling-demo/stories/index.js
+++ components/x-styling-demo/stories/index.js
@@ -5,3 +5,13 @@ exports.package = require('../package.json');
 exports.stories = [
 	require('./styling'),
 ];
+
+exports.knobs = (data, { boolean }) => ({
+	danger() {
+		return boolean('Danger', data.danger);
+	},
+
+	large() {
+		return boolean('Large', data.large);
+	}
+});
