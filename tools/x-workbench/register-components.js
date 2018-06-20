f60afcfab729e95f99c9b081d63a912dbebfec75
Katerina <katerina.loschinina@ft.com>
2018-06-20 14:36:26 +0100

    fix merge related issue  🐿 v2.8.0

--- tools/x-workbench/register-components.js
+++ tools/x-workbench/register-components.js
@@ -2,7 +2,6 @@ const components = [
 	require('@financial-times/x-teaser/stories'),
 	require('@financial-times/x-follow-button/stories'),
 	require('@financial-times/x-increment/stories'),
-	require('@financial-times/x-increment/stories'),
 	require('@financial-times/x-styling-demo/stories'),
 ];
 
