c904d9c1d08a04bbfdf50da6f7dd8e81fdf19bc9
fenglish <asuka.ochi@ft.com>
2018-07-10 17:38:52 +0100

    prototype before changing class names to Origami classnames  🐿 v2.10.0

--- components/x-gift-article/stories/index.js
+++ components/x-gift-article/stories/index.js
@@ -1,13 +1,11 @@
 const { GiftArticle } = require('../');
 
-// if (typeof window !== 'undefined' && !/\.ft\.com$/.test(window.location.hostname)) {
-// 	console.warn('Due to CORS restrictions some demos may not work outside of the ft.com domain');
-// }
-
-module.exports = {
-	name: 'x-gift-article',
-	component: GiftArticle,
-	stories: [
-		require('./test')
-	]
+exports.component = GiftArticle;
+exports.package = require('../package.json');
+exports.dependencies = {
+	'o-fonts': '^3.0.0',
 };
+exports.stories = [
+	require('./with-gift-credits')
+];
+exports.knobs = require('./knobs');
