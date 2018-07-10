d7f5573d4cff14f900fd907e73be634416878921
fenglish <asuka.ochi@ft.com>
2018-07-10 17:38:52 +0100

    prototype before changing class names to Origami classnames  🐿 v2.10.0

--- components/x-gift-article/src/XGiftArticle.jsx
+++
@@ -1,7 +0,0 @@
-import { h } from '@financial-times/x-engine';
-
-const GiftArticle = ({name}) => <h1>Hello, {name}!!</h1>;
-
-export {
-	GiftArticle
-};
--- components/x-gift-article/stories/test.js
+++
@@ -1,9 +0,0 @@
-exports.title = 'Test';
-
-exports.data = {
-	name: 'ASUKA'
-}
-
-// This reference is only required for hot module loading in development
-// <https://webpack.js.org/concepts/hot-module-replacement/>
-exports.m = module;
