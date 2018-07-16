0fd36cf3f1916ba4ddf781b110ac55a773c232eb
fenglish <asuka.ochi@ft.com>
2018-07-16 16:32:14 +0100

    apply o-buttons class  🐿 v2.10.0

--- components/x-gift-article/stories/index.js
+++ components/x-gift-article/stories/index.js
@@ -4,6 +4,7 @@ exports.component = GiftArticle;
 exports.package = require('../package.json');
 exports.dependencies = {
 	'o-fonts': '^3.0.0',
+	'o-buttons': '^5.13.1'
 };
 exports.stories = [
 	require('./with-gift-credits')
