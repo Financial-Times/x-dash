253310d2d78c854cd1cc31efd2c23890662ce357
fenglish <asuka.ochi@ft.com>
2018-07-16 17:34:02 +0100

    apply o-form class  🐿 v2.10.0

--- components/x-gift-article/src/Buttons.jsx
+++ components/x-gift-article/src/Buttons.jsx
@@ -4,15 +4,17 @@ export default ({ isGift, isGiftUrlCreated, mailtoUrl, createGiftUrl }) => {
 
 	if (isGiftUrlCreated || !isGift) {
 		return (
-			<div className="gift-form__buttons">
-				<button className="o-buttons o-buttons--primary o-buttons--big js-copy-link" type="button">Copy link</button>
-				<a className="o-buttons o-buttons--primary o-buttons--big" href={ mailtoUrl } target="_blank">Email link</a>
+			<div className="o-forms__suffix">
+				<div className="gift-form__buttons-align">
+					<button className="o-buttons o-buttons--primary o-buttons--big gift-form__button--with-gap js-copy-link" type="button">Copy link</button>
+					<a className="o-buttons o-buttons--primary o-buttons--big" href={ mailtoUrl } target="_blank">Email link</a>
+				</div>
 			</div>
 		);
 	}
 
 	return (
-		<div className="gift-form__buttons">
+		<div className="o-forms__suffix">
 			<button className="o-buttons o-buttons--primary o-buttons--big" type="button" onClick={ createGiftUrl }>
 				Create gift link
 			</button>
--- components/x-gift-article/src/Url.jsx
+++ components/x-gift-article/src/Url.jsx
@@ -3,17 +3,9 @@ import { h } from '@financial-times/x-engine';
 export default ({ isGift, isGiftUrlCreated, url }) => {
 
 	if (!isGift || isGiftUrlCreated) {
-		return (
-			<div className="gift-form__link-input">
-				<input type="text" name="example-gift-link" value={ url } className="gift-form__text-input"></input>
-			</div>
-		);
+		return (<input type="text" name="example-gift-link" value={ url } className="o-forms__text"></input>);
 	}
 
-	return (
-		<div className="gift-form__link-input">
-			<input type="text" name="example-gift-link" value={ url } className="gift-form__text-input"  disabled='disabled'></input>
-		</div>
-	);
+	return (<input type="text" name="example-gift-link" value={ url } className="o-forms__text"  disabled='disabled'></input>);
 
 };
