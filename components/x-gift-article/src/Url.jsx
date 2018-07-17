9e4c0df346a0bd37861c0543ac765e43a18e103c
fenglish <asuka.ochi@ft.com>
2018-07-17 17:08:04 +0100

    tidy up  🐿 v2.10.0

--- components/x-gift-article/src/Url.jsx
+++ components/x-gift-article/src/Url.jsx
@@ -12,6 +12,6 @@ export default ({ isGift, isGiftUrlCreated, url, urlType }) => {
 		return (<input type="text" name={ urlType } value={ url } className={ urlClassNames }></input>);
 	}
 
-	return (<input type="text" name={ urlType } value={ url } className={ urlClassNames }  disabled='disabled'></input>);
+	return (<input type="text" name={ urlType } value={ url } className={ urlClassNames } disabled='disabled'></input>);
 
 };
