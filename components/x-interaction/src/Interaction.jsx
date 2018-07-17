2a68a77ee0ed2f0ea0d7fef2399eca023f15d751
Matt Brennan <matt@153.io>
2018-07-17 17:36:27 +0100

    actions from outside (e.g. another layer of wrapping) take precedence  🐿 v2.10.0

--- components/x-interaction/src/Interaction.jsx
+++ components/x-interaction/src/Interaction.jsx
@@ -4,17 +4,37 @@ import { InteractionRender } from './InteractionRender';
 import wrapComponentName from './concerns/wrap-component-name';
 import { registerComponent } from './concerns/register-component';
 
-// use the class version for interactive runtimes and the static version for static runtimes
+// use the class version for clientside and the static version for server
 const Interaction = typeof window !== 'undefined' ? InteractionClass : InteractionRender;
 
 export const withActions = (getActions) => (Component) => {
-	function Enhanced({ id, hydrating, ...initialState }) {
-		const actions = typeof getActions === 'function' ? getActions(initialState) : getActions;
+	function Enhanced({
+		id,
+		hydrating,
+		actions: extraActions,
+		...initialState
+	}) {
+		// support passing actions to withActions as an object or a function
+		// that's called with the initial state
+		const actions = typeof getActions === 'function'
+			? getActions(initialState)
+			: getActions;
 
-		return <Interaction {...{ Component, initialState, actions, id, hydrating }} />;
+		return <Interaction {...{
+			id,
+			Component,
+			initialState,
+			hydrating,
+			// if extraActions is defined, those are from another level
+			// of wrapping with withActions, so those should take precedence
+			actions: Object.assign(actions, extraActions),
+		}} />;
 	}
 
+	// set the displayName of the Enhanced component for debugging
 	wrapComponentName(Component, Enhanced);
+
+	// register the component under its name for later hydration from serialised data
 	registerComponent(Enhanced);
 
 	return Enhanced;
