5206b05c4230655ed005b627f84fc718e4c0bb6b
fenglish <asuka.ochi@ft.com>
2018-07-17 16:16:24 +0100

    add stylings  🐿 v2.10.0

--- components/x-gift-article/src/RadioButtonsSection.jsx
+++ components/x-gift-article/src/RadioButtonsSection.jsx
@@ -1,32 +1,21 @@
 import { h } from '@financial-times/x-engine';
+import styles from './GiftArticle.css';
+
+const boldTextClassName = styles.bold;
+const radioSectionClassNames = [
+	'o-forms__group',
+	'o-forms__group--inline',
+	styles['radio-button-section']
+].join(' ');
 
 export default ({ displayGiftUrlSection, displayNonGiftUrlSection }) => (
-	<div class="o-forms__group o-forms__group--inline">
+	<div className={ radioSectionClassNames }>
 
-		<input type="radio" name="gift-form__radio" value="giftLink" class="o-forms__radio" id="giftLink" defaultChecked onChange={ displayGiftUrlSection }/>
-		<label htmlFor="giftLink" class="o-forms__label">with <strong>anyone</strong></label>
+		<input type="radio" name="gift-form__radio" value="giftLink" className="o-forms__radio" id="giftLink" defaultChecked onChange={ displayGiftUrlSection }/>
+		<label htmlFor="giftLink" className="o-forms__label">with <span className={ boldTextClassName }>anyone</span></label>
 
-		<input type="radio" name="gift-form__radio" value="nonGiftLink" class="o-forms__radio" id="nonGiftLink" onChange={ displayNonGiftUrlSection }/>
-		<label htmlFor="nonGiftLink" class="o-forms__label">with <strong>other subscribers</strong></label>
+		<input type="radio" name="gift-form__radio" value="nonGiftLink" className="o-forms__radio" id="nonGiftLink" onChange={ displayNonGiftUrlSection }/>
+		<label htmlFor="nonGiftLink" className="o-forms__label">with <span className={ boldTextClassName }>other subscribers</span></label>
 
 	</div>
 );
-
-
-// <div className="o-forms__group o-forms__group--inline">
-//
-// 	<div className="gift-form__radio-input">
-// 		<input type="radio" name="gift-form__radio" value="giftLink" className="o-forms__radio" id="giftLink" defaultChecked ></input>
-// 		<label htmlFor="giftLink" className="gift-form__label">
-// 			<div className="o-forms__label"></div>
-// 		</label>
-// 	</div>
-//
-// 	<div className="gift-form__radio-input">
-// 		<input type="radio" name="gift-form__radio" value="nonGiftLink" className="o-forms__radio" id="nonGiftLink" ></input>
-// 		<label htmlFor="nonGiftLink" className="gift-form__label">
-// 			<div className="o-forms__label"></div>
-// 		</label>
-// 	</div>
-//
-// </div>
