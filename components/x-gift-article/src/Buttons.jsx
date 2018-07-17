de8ba7bbae611d80603c034cec3c9f0cf2f47f57
fenglish <asuka.ochi@ft.com>
2018-07-17 16:16:24 +0100

    add stylings  🐿 v2.10.0

--- components/x-gift-article/src/Buttons.jsx
+++ components/x-gift-article/src/Buttons.jsx
@@ -1,21 +1,35 @@
 import { h } from '@financial-times/x-engine';
+import styles from './GiftArticle.css';
+
+const ButtonsClassName = styles.buttons;
+
+const ButtonClassNames = [
+	'o-buttons',
+	'o-buttons--primary',
+	'o-buttons--big'
+].join(' ');
+
+const ButtonWithGapClassNames = [
+	ButtonClassNames,
+	'js-copy-link',
+	styles['button--with-gap']
+].join(' ');
+
 
 export default ({ isGift, isGiftUrlCreated, mailtoUrl, createGiftUrl }) => {
 
 	if (isGiftUrlCreated || !isGift) {
 		return (
-			<div className="o-forms__suffix">
-				<div className="gift-form__buttons-align">
-					<button className="o-buttons o-buttons--primary o-buttons--big gift-form__button--with-gap js-copy-link" type="button">Copy link</button>
-					<a className="o-buttons o-buttons--primary o-buttons--big" href={ mailtoUrl } target="_blank">Email link</a>
-				</div>
+			<div className={ ButtonsClassName }>
+				<button className={ ButtonWithGapClassNames } type="button">Copy link</button>
+				<a className={ ButtonClassNames } href={ mailtoUrl } target="_blank">Email link</a>
 			</div>
 		);
 	}
 
 	return (
-		<div className="o-forms__suffix">
-			<button className="o-buttons o-buttons--primary o-buttons--big" type="button" onClick={ createGiftUrl }>
+		<div className={ ButtonsClassName }>
+			<button className={ ButtonClassNames } type="button" onClick={ createGiftUrl }>
 				Create gift link
 			</button>
 		</div>
