b10d8225e203a3fc5ce48b5a01c7c24ad480a192
Matt Brennan <matt@153.io>
2018-07-18 17:08:58 +0100

    remove unused package  🐿 v2.10.0

--- tools/x-ssr-demo/src/index.js
+++
@@ -1,6 +0,0 @@
-import '@financial-times/x-increment';
-import {hydrate} from '@financial-times/x-interaction';
-
-document.addEventListener('DOMContentLoaded', hydrate);
-
-import 'preact/devtools';
