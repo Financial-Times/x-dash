39796dfeec5787f63568a77a601afa517c5ec783
fenglish <asuka.ochi@ft.com>
2018-07-18 15:08:32 +0100

    add Loading status  🐿 v2.10.0

--- components/x-gift-article/src/GetGiftUrl.jsx
+++ components/x-gift-article/src/GetGiftUrl.jsx
@@ -3,8 +3,6 @@ import { h } from '@financial-times/x-engine';
 const delay = ms => new Promise(r => setTimeout(r, ms));
 
 export default () => {
-	return delay(1000)
-		.then(() => {
-			return 'https://gift-url';
-		})
+	return delay(2000)
+		.then(() => ('https://gift-url'))
 };
