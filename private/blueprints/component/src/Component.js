628bd3ff1f25e14171ec15bebe7937f1de7a117e
Matt Hinchliffe <matt.hinchliffe@ft.com>
2018-07-19 15:27:59 +0100

    Add component template to blueprint script  🐿 v2.10.0

---
+++ private/blueprints/component/src/Component.js
@@ -0,0 +1,9 @@
+import { h } from '@financial-times/x-engine';
+
+const <%= componentName %> = (props) => (
+	<div className='<%= packageName %>'>
+		<h1>Welcome to <%= packageName %></h1>
+	</div>
+);
+
+export { <%= componentName %> };
--- private/scripts/blueprint.js
+++ private/scripts/blueprint.js
@@ -1,8 +1,34 @@
-const ejs = require('ejs');
 const fs = require('fs');
+const ejs = require('ejs');
 const path = require('path');
 
-const name = process.argv.slice(-1).pop();
+function templateFiles (target, data) {
+	const list = fs.readdirSync(target);
+
+	const output = {};
+
+	for (const file of list) {
+		const fullPath = path.join(target, file);
+		const stats = fs.statSync(fullPath);
+
+		if (stats.isFile()) {
+			const buffer = fs.readFileSync(fullPath);
+			output[file] = ejs.render(buffer.toString(), data);
+		}
+	}
+
+	return output;
+}
+
+function writeOutput (target, output) {
+	log(`Creating directory ${target}`);
+	fs.mkdirSync(target);
+
+	for (const [ file, content ] of Object.entries(output)) {
+		log(`Creating file ${file}`);
+		fs.writeFileSync(path.join(target, file), content);
+	}
+}
 
 function error (message) {
 	process.stderr.write(`ERROR: ${message}\n`);
@@ -13,6 +39,14 @@ function log (message) {
 	process.stdout.write(`${message}\n`);
 }
 
+// Collate variables
+const name = process.argv.slice(-1).pop();
+const packageName = `x-${name}`;
+const componentName = name.charAt(0).toUpperCase() + name.substr(1);
+const source = path.join(process.cwd(), 'private/blueprints/component');
+const destination = path.join(process.cwd(), 'components', packageName);
+
+// Validate input
 if (name === undefined) {
 	error('A component name is required, usage: blueprint.js {name}');
 }
@@ -21,33 +55,18 @@ if (/^x-/.test(name)) {
 	error('Component names should not include the "x-" prefix');
 }
 
-const packageName = `x-${name}`;
-const componentName = name.charAt(0).toUpperCase() + name.substr(1);
-
-const source = path.join(process.cwd(), 'private/blueprints/component');
-const destination = path.join(process.cwd(), 'components', packageName);
-
 if (fs.existsSync(destination)) {
 	error(`Directory ${destination} already exists`);
 }
 
+// Create and write blueprint files
 const templateData = { packageName, componentName };
 
-const list = fs.readdirSync(source);
+writeOutput(destination, templateFiles(source, templateData));
 
-// collect complete output
-const output = {};
+// The name of the component entry file should match the component name
+const entry = templateFiles(path.join(source, 'src'), templateData);
 
-for (const file of list) {
-	const buffer = fs.readFileSync(path.join(source, file));
-	output[file] = ejs.render(buffer.toString(), templateData);
-}
-
-// create new component directory and output files
-log(`Creating directory ${destination}`);
-fs.mkdirSync(destination);
-
-for (const [ file, content ] of Object.entries(output)) {
-	log(`Creating file ${file}`);
-	fs.writeFileSync(path.join(destination, file), content);
-}
+writeOutput(path.join(destination, 'src'), {
+	[`${componentName}.js`]: entry['Component.js']
+});
