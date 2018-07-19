df40ee36141dfcfd8ab7f770a36f9d779a2faf78
fenglish <asuka.ochi@ft.com>
2018-07-19 14:37:34 +0100

    add compose data function before loading gift article form  🐿 v2.10.0

--- components/x-gift-article/stories/with-gift-credits.js
+++ components/x-gift-article/stories/with-gift-credits.js
@@ -3,34 +3,15 @@ exports.title = 'With gift credits';
 exports.data = {
 	title: 'Share this article',
 	isFreeArticle: false,
-	credit: 20,
-	monthlyAllowance: 20,
-	dateText: 'May 1',
-	isGift: true,
-	url: 'https://dummy-url',
-	urlType: 'example-gift-link',
-	nonGiftUrl: 'https://non-gift-url',
-	mailtoUrl: 'mailto:?subject=title&amp;body=nonGiftMailtoUrl',
-	mailtoGiftUrl: 'mailto:?subject=title&amp;body=giftMailtoUrl',
-	mailtoNonGiftUrl: 'mailto:?subject=title&amp;body=nonGiftMailtoUrl',
-	isGiftUrlCreated: false,
-	tracking: 'giftLink'
+	articleUrl: 'https://www.ft.com/content/blahblah',
+	articleTitle: 'Title Title Title Title',
 };
 
 exports.knobs = [
-	'showTitle',
 	'title',
 	'isFreeArticle',
-	'credit',
-	'isGift',
-	'nonGiftUrl',
-	'url',
-	'urlType',
-	'mailtoUrl',
-	'mailtoGiftUrl',
-	'mailtoNonGiftUrl',
-	'isGiftUrlCreated',
-	'tracking'
+	'articleUrl',
+	'articleTitle'
 ];
 
 // This reference is only required for hot module loading in development
