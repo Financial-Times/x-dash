303d5f6c566a596b2c197459ac4b1f9e4b798ecd
fenglish <asuka.ochi@ft.com>
2018-07-19 16:50:43 +0100

    class => className  🐿 v2.10.0

--- components/x-gift-article/src/Loading.jsx
+++ components/x-gift-article/src/Loading.jsx
@@ -2,7 +2,7 @@ import { h } from '@financial-times/x-engine';
 import styles from './GiftArticle.css';
 
 export default () => (
-	<div class={ styles['loading-spinner__container'] }>
-		<div class="o-loading o-loading--dark o-loading--large"></div>
+	<div className={ styles['loading-spinner__container'] }>
+		<div className="o-loading o-loading--dark o-loading--large"></div>
 	</div>
 );
