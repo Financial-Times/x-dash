a2f86e28a236961fefbb9156fde18bb7b33ce591
Matt Hinchliffe <matt.hinchliffe@ft.com>
2018-07-23 18:02:39 +0100

    Add logging comments to gh-pages deploy script  🐿 v2.10.0

--- private/scripts/gh-pages.sh
+++ private/scripts/gh-pages.sh
@@ -1,22 +1,28 @@
+#!/bin/bash
+
+# Exit if any subcommand fails.
+set -e
+
 git config --global user.email $GITHUB_EMAIL
 git config --global user.name $GITHUB_NAME
 
 DEPLOY_DIR="tools/x-docs/public/*"
 DEPLOY_BRANCH="matth/gh-pages"
-
 GITHUB_REPOSITORY="https://${GITHUB_TOKEN}@github.com/Financial-Times/${CIRCLE_PROJECT_REPONAME}.git"
 
-git clone $GITHUB_REPOSITORY tmp --depth=0
+echo "Cloning github.com/Financial-Times/${CIRCLE_PROJECT_REPONAME}"
+git clone $GITHUB_REPOSITORY tmp
 
 cd tmp
 git checkout --orphan $DEPLOY_BRANCH
 cd ..
 
+echo "Copying ${DEPLOY_DIR} to tmp/"
 cp -r $DEPLOY_DIR tmp
-
 cd tmp
 
 git add -A
 git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}"
 
+echo "Pushing to ${DEPLOY_BRANCH}"
 git push -q $GITHUB_REPOSITORY $DEPLOY_BRANCH
