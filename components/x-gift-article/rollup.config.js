f0f4844997823af541a8a30b985ede78e36b4e09
fenglish <asuka.ochi@ft.com>
2018-07-23 12:28:15 +0100

    add GiftArticleWrapper  🐿 v2.10.0

--- components/x-gift-article/rollup.config.js
+++ components/x-gift-article/rollup.config.js
@@ -1,6 +1,6 @@
 import xRollup from '@financial-times/x-rollup';
 import pkg from './package.json';
 
-const input = 'src/GiftArticle.jsx';
+const input = 'src/GiftArticleWrapper.jsx';
 
 export default xRollup({input, pkg});
