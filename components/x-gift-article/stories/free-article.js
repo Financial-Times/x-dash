364786c19c9d842c61fb87fc167b2782672c5750
fenglish <asuka.ochi@ft.com>
2018-07-23 08:34:39 +0100

    free article  🐿 v2.10.0

---
+++ components/x-gift-article/stories/free-article.js
@@ -0,0 +1,19 @@
+exports.title = 'Free article';
+
+exports.data = {
+	title: 'Share this article',
+	isFreeArticle: true,
+	articleUrl: 'https://www.ft.com/content/blahblah',
+	articleTitle: 'Title Title Title Title',
+};
+
+exports.knobs = [
+	'title',
+	'isFreeArticle',
+	'articleUrl',
+	'articleTitle'
+];
+
+// This reference is only required for hot module loading in development
+// <https://webpack.js.org/concepts/hot-module-replacement/>
+exports.m = module;
