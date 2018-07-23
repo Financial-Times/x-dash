a193b53b384522eb0d031cebdb8aae0bd7fcdf7f
Matt Hinchliffe <matt@maketea.co.uk>
2018-07-23 19:14:40 +0100

    Clean up temp dir before committing
     🐿 v2.10.0

--- private/scripts/gh-pages.sh
+++ private/scripts/gh-pages.sh
@@ -10,11 +10,12 @@ DEPLOY_DIR="tools/x-docs/public/*"
 DEPLOY_BRANCH="matth/gh-pages"
 GITHUB_REPOSITORY="https://${GITHUB_TOKEN}@github.com/Financial-Times/${CIRCLE_PROJECT_REPONAME}.git"
 
-echo "Cloning github.com/Financial-Times/${CIRCLE_PROJECT_REPONAME}"
+echo "Fetching github.com/Financial-Times/${CIRCLE_PROJECT_REPONAME}"
 git clone $GITHUB_REPOSITORY tmp
 
 cd tmp
 git checkout --orphan $DEPLOY_BRANCH
+git rm -rf .
 cd ..
 
 echo "Copying ${DEPLOY_DIR} to tmp/"
@@ -22,7 +23,7 @@ cp -r $DEPLOY_DIR tmp
 cd tmp
 
 git add -A
-git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}"
+git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
 
 echo "Pushing to ${DEPLOY_BRANCH}"
 git push -q $GITHUB_REPOSITORY $DEPLOY_BRANCH
