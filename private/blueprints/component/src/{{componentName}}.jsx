d1bba6424f586b1256e40d80582f9a9b0e417fc6
Matt Hinchliffe <matt@maketea.co.uk>
2018-07-24 19:17:41 +0100

    Add example story to component blueprint

--- private/blueprints/component/src/{{componentName}}.jsx
+++ private/blueprints/component/src/{{componentName}}.jsx
@@ -3,6 +3,7 @@ import { h } from '@financial-times/x-engine';
 const {{componentName}} = (props) => (
 	<div className='{{packageName}}'>
 		<h1>Welcome to {{packageName}}</h1>
+		<p>{props.message}</p>
 	</div>
 );
 
