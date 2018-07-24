93fc79338baf5395e4acf7c8006f62f207b05f2a
fenglish <asuka.ochi@ft.com>
2018-07-24 16:08:25 +0100

    mock credit response

--- components/x-gift-article/stories/free-article.js
+++ components/x-gift-article/stories/free-article.js
@@ -14,15 +14,6 @@ exports.knobs = [
 	'articleTitle'
 ];
 
-exports.fetchMock = fetchMock => {
-	fetchMock
-		.restore()
-		.get(
-			'__myft/api',
-			{ 'some': 'response' },
-		);
-};
-
 // This reference is only required for hot module loading in development
 // <https://webpack.js.org/concepts/hot-module-replacement/>
 exports.m = module;
