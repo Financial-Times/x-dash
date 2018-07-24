e4f721d78fb2c4475da07670e19f3200d7034333
Matt Hinchliffe <matt@maketea.co.uk>
2018-07-24 12:10:47 +0100

    Prettify x-rollup module  🐿 v2.10.0

--- packages/x-rollup/src/babel-config.js
+++ packages/x-rollup/src/babel-config.js
@@ -1,38 +1,43 @@
-const resolvePlugin = (plugin) => {
-	if (Array.isArray(plugin)) {
-		return [require.resolve(plugin[0]), plugin[1]];
-	} else {
-		return require.resolve(plugin);
-	}
-};
-
 module.exports = (targets = []) => ({
 	include: '**/*.{js,jsx}',
 	plugins: [
-		// this plugin is misleadingly named, it includes a general JSX parser and helper 🙄
-		['babel-plugin-transform-react-jsx', {
-			pragma: 'h',
-			useBuiltIns: true,
-		}],
-		// although this feature is at stage 4, we'd have to use babel 7 to get the version
-		// of preset-env that supports it 😖
-		['babel-plugin-transform-object-rest-spread', {
-			useBuiltIns: true,
-		}],
-		// tell Babel to not place any internal helper declarations in the output
-		'babel-plugin-external-helpers',
-		// implements async and await using syntax transformation rather than generators
-		['fast-async', {
-			compiler: {
-				noRuntime: true,
-			},
-		}],
-	].map(resolvePlugin),
+		// this plugin is not React specific! It includes a general JSX parser and helper 🙄
+		[
+			require.resolve('babel-plugin-transform-react-jsx'),
+			{
+				pragma: 'h',
+				useBuiltIns: true
+			}
+		],
+		// Although this feature is at stage 4, we'd have to use babel 7 to get the version
+		// of preset-env that actually supports it 😖
+		[
+			require.resolve('babel-plugin-transform-object-rest-spread'),
+			{
+				useBuiltIns: true
+			}
+		],
+		// Instruct Babel to not include any internal helper declarations in the output
+		require.resolve('babel-plugin-external-helpers'),
+		// Implements async/await using syntax transformation rather than generators which require
+		// a huge runtime for browsers which do not natively support them.
+		[
+			require.resolve('fast-async'),
+			{
+				compiler: {
+					noRuntime: true
+				}
+			}
+		]
+	],
 	presets: [
-		['babel-preset-env', {
-			targets,
-			modules: false,
-			exclude: ['transform-regenerator', 'transform-async-to-generator'],
-		}],
-	].map(resolvePlugin),
+		[
+			require.resolve('babel-preset-env'),
+			{
+				targets,
+				modules: false,
+				exclude: ['transform-regenerator', 'transform-async-to-generator']
+			}
+		]
+	]
 });
