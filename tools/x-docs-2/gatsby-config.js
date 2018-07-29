b25ca336a8780a5f47d1d146f9ac19469e040a69
Matt Hinchliffe <matt@maketea.co.uk>
2018-07-29 19:32:05 +0100

    Setup Gatsby V2 site, include filesystem plugin and remark, create package json plugin

---
+++ tools/x-docs-2/gatsby-config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+	pathPrefix: '/x-dash',
+	plugins: [
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'pages',
+				path: path.resolve('../../docs')
+			},
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'components',
+				path: path.resolve('../../components')
+			},
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'packages',
+				path: path.resolve('../../packages')
+			},
+		},
+		// Handles markdown files (creates "MarkdownRemark" nodes)
+		'gatsby-transformer-remark',
+		// Handles package manifest files (creates "PackageJSON" nodes)
+		'gatsby-plugin-package-json'
+	]
+};
