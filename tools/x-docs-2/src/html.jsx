b1c1b4b9ca5eb30c2030191e96138ebaac8672d8
Matt Hinchliffe <matt@maketea.co.uk>
2018-07-30 19:01:09 +0100

    Add custom document component

---
+++ tools/x-docs-2/src/html.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default class HTML extends React.Component {
+	render() {
+		return (
+			<html {...this.props.htmlAttributes}>
+				<head>
+					{this.props.headComponents}
+					<meta charSet="utf-8" />
+					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+					<meta
+						name="viewport"
+						content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+					/>
+					<link rel="icon" href="/favicon.ico" />
+				</head>
+				<body {...this.props.bodyAttributes}>
+					<div id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
+					{this.props.postBodyComponents}
+				</body>
+			</html>
+		);
+	}
+}
