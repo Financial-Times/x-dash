a23aeb8dee69083eb3ebbba9f836a440f872f911
Matt Hinchliffe <matt@maketea.co.uk>
2018-07-30 10:26:01 +0100

    Add page template for viewing modules

---
+++ tools/x-docs-2/src/templates/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { graphql } from 'gatsby';
+import Layout from '../components/layouts/basic';
+
+export default ({ pathContext }) => {
+	return (
+		<Layout title={pathContext.title}>
+			<div dangerouslySetInnerHTML={{ __html: pathContext.html }} />
+		</Layout>
+	);
+};
