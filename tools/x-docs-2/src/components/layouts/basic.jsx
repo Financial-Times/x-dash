eacef3d037805521383b3b774ea69b155f25845c
Matt Hinchliffe <matt@maketea.co.uk>
2018-07-30 10:03:04 +0100

    Add components and packages list pages

---
+++ tools/x-docs-2/src/components/layouts/basic.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+// import { StaticQuery, graphql } from 'gatsby';
+// import { Link } from 'gatsby';
+
+export default ({ title, children }) => (
+	<>
+		<Helmet title={title} />
+		<h1>{title}</h1>
+		<div>{children}</div>
+	</>
+);
---
+++ tools/x-docs-2/src/pages/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import { Link } from 'gatsby';
+
+const IndexPage = () => (
+	<h1>Hello World</h1>
+);
+
+export default IndexPage;
