8618c905ac905451f49653b8fc543573e3b0020c
Matt Hinchliffe <matt@maketea.co.uk>
2018-07-31 09:20:26 +0100

    Apply slugs to all markdown and package nodes, fetch data inside template package pages

---
+++ tools/x-docs-2/src/templates/npm-package.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import Layout from '../components/layouts/basic';
+
+const ModuleTemplate = ({ data }) => {
+	return (
+		<Layout title={data.packageJson.manifest.name}>
+			<div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+		</Layout>
+	);
+};
+
+export default ModuleTemplate;
+
+export const pageQuery = graphql`
+	query($slug: String!) {
+		markdownRemark(fields: { slug: { eq: $slug } }) {
+			html
+		}
+		packageJson(fields: { slug: { eq: $slug } }) {
+			manifest
+		}
+	}
+`;
