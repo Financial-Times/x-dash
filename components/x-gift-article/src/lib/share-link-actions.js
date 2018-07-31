0a6a75fd2d4972f8610bb2f02a3aba58db4c5f60
fenglish <asuka.ochi@ft.com>
2018-07-31 14:59:16 +0100

    modify copyToClipboard function

--- components/x-gift-article/src/lib/share-link-actions.js
+++ components/x-gift-article/src/lib/share-link-actions.js
@@ -6,39 +6,29 @@ function createMailtoLink (articleTitle, shareUrl) {
 }
 
 
-function copyToClipboard (url) {
-	const el = document.createElement('textarea');
-	el.value = url;
-	el.setAttribute('readonly', '');
+function copyToClipboard (copyText) {
+
+	const selected =
+	document.getSelection().rangeCount > 0          // Check if there is any content selected previously
+		? document.getSelection().getRangeAt(0)       // Store selection if found
+		: false;                                      // Mark as false to know no selection existed before
+
+	const el = document.createElement('textarea');  // Create a <textarea> element
+	el.value = copyText;                            // Set its value to the string that you want copied
+	el.setAttribute('readonly', '');                // Make it readonly to be tamper-proof
 	el.style.position = 'absolute';
-	el.style.left = '-9999px';
-	document.body.appendChild(el);
-	el.select();
-	document.execCommand('copy');
-	document.body.removeChild(el);
-	//
-	// const oldContentEditable = inputEl.contentEditable;
-	// const oldReadOnly = inputEl.readOnly;
-	// const range = document.createRange();
-	//
-	// inputEl.contenteditable = true;
-	// inputEl.readonly = false;
-	// inputEl.focus();
-	// range.selectNodeContents(inputEl);
-	//
-	// const selection = window.getSelection();
-	//
-	// try {
-	// 	selection.removeAllRanges();
-	// 	selection.addRange(range);
-	// 	inputEl.setSelectionRange(0, 999999);
-	// } catch (err) {
-	// 	inputEl.select(); // IE11 etc.
-	// }
-	// inputEl.contentEditable = oldContentEditable;
-	// inputEl.readOnly = oldReadOnly;
-	// document.execCommand('copy');
-	// inputEl.blur();
+	el.style.left = '-9999px';                      // Move outside the screen to make it invisible
+	document.body.appendChild(el);                  // Append the <textarea> element to the HTML document
+
+	el.select();                                    // Select the <textarea> content
+	document.execCommand('copy');                   // Copy
+	document.body.removeChild(el);                  // Remove the <textarea> element
+
+	if (selected) {                                 // If a selection existed before copying
+		document.getSelection().removeAllRanges();    // Unselect everything on the HTML document
+		document.getSelection().addRange(selected);   // Restore the original selection
+	}
+
 }
 
 module.exports = {
