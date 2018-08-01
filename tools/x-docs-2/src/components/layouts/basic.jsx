f2c5a25579272a62bebc82fce64dae12214b171f
Matt Hinchliffe <matt@maketea.co.uk>
2018-08-01 08:38:07 +0100

    Add simple navigation and document structure

--- tools/x-docs-2/src/components/layouts/basic.jsx
+++ tools/x-docs-2/src/components/layouts/basic.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import Banner from '../header/banner';
 // import { StaticQuery, graphql } from 'gatsby';
 // import { Link } from 'gatsby';
 
 export default ({ title, children }) => (
-	<>
+	<div className="site-layout">
 		<Helmet title={title} />
-		<div>{children}</div>
-	</>
+		<header className="site-header">
+			<Banner />
+		</header>
+		<main role="main">{children}</main>
+		<nav role="complementary" className="site-sidebar"></nav>
+		<footer role="contentinfo" className="site-footer"></footer>
+	</div>
 );
