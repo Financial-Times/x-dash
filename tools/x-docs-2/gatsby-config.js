44374f5f5fe09e3a0392bd6373d110e95d82d131
Matt Hinchliffe <matt@maketea.co.uk>
2018-08-02 10:07:13 +0100

    Add prism highlighting and theme and external links plugins

--- tools/x-docs-2/gatsby-config.js
+++ tools/x-docs-2/gatsby-config.js
@@ -27,7 +27,16 @@ module.exports = {
 			},
 		},
 		// Handles markdown files (creates "MarkdownRemark" nodes)
-		'gatsby-transformer-remark',
+		{
+			resolve: 'gatsby-transformer-remark',
+			options: {
+				plugins: [
+					'gatsby-remark-prismjs',
+					'gatsby-remark-autolink-headers',
+					'gatsby-remark-external-links'
+				]
+			}
+		},
 		// Handles package manifest files (creates "NpmPackage" nodes)
 		'gatsby-transformer-npm-package'
 	]
