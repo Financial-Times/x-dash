d5f57c3e89a5aefd63553bbd8883c22db3c4c08c
fenglish <asuka.ochi@ft.com>
2018-08-02 18:15:02 +0100

    use async await

--- components/x-gift-article/src/lib/api.js
+++ components/x-gift-article/src/lib/api.js
@@ -1,49 +1,61 @@
 const REDEMPTION_LIMIT = 3;
 
 module.exports = {
-	getGiftArticleAllowance: () => {
-		return fetch('/article-email/credits', { credentials: 'same-origin' })
-			.then(response => response.json())
-			.then(json => {
-				return { monthlyAllowance: json.credits.allowance, giftCredits: json.credits.remainingCredits };
-			});
-	},
-	getGiftUrl: (articleId, sessionId) => {
-		return fetch('/article-email/gift-link', {
-			credentials: 'same-origin',
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				contentUUID: articleId,
-				ftSessionSecure: sessionId
-			})
-		})
-			.then(response => response.ok ? response.json() : {})
-			.then(body => {
-				if (body.errors) {
-					throw new Error(`Failed to get gift article link: ${body.errors.join(', ')}`);
-				}
+	getGiftArticleAllowance: async () => {
+		try {
+			const response = await fetch('/article-email/credits', { credentials: 'same-origin' });
+			const json = await response.json();
 
-				return Object.assign({}, body, { redemptionLimit: REDEMPTION_LIMIT });
-			});
+			return {
+				monthlyAllowance: json.credits.allowance,
+				giftCredits: json.credits.remainingCredits
+			};
+
+		} catch (e) {
+			return { monthlyAllowance: undefined, giftCredits: undefined };
+		}
+
 	},
-	getShorterUrl: (originalUrl) => {
+
+	getGiftUrl: async (articleId, sessionId) => {
+		try {
+			const response = await fetch('/article-email/gift-link', {
+				credentials: 'same-origin',
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({
+					contentUUID: articleId,
+					ftSessionSecure: sessionId
+				})
+			});
+
+			const body = response.ok ? await response.json() : {};
+			if (body.errors) {
+				throw new Error(`Failed to get gift article link: ${body.errors.join(', ')}`);
+			}
+
+			return Object.assign({}, body, { redemptionLimit: REDEMPTION_LIMIT });
+
+		} catch (e) {
+			return { redemptionUrl: undefined, redemptionLimit: undefined };
+		}
+	},
+
+	getShorterUrl: async (originalUrl) => {
 		let url = originalUrl;
 		let isShortened = false;
 
-		return fetch('/article/shorten-url/' + encodeURIComponent(originalUrl), { credentials: 'same-origin' })
-			.then(response => response.json())
-			.then(json => {
-				if (json.shortenedUrl) {
-					isShortened = true;
-					url = json.shortenedUrl;
-				}
-				return { url, isShortened };
-			})
-			.catch(() => {
-				return { url, isShortened };
-			});
+		try {
+			const fetchUrl = '/article/shorten-url/' + encodeURIComponent(originalUrl);
+			const response = await fetch(fetchUrl, { credentials: 'same-origin' });
+			const json = await response.json();
+
+			if (json.shortenedUrl) {
+				isShortened = true;
+				url = json.shortenedUrl;
+			}
+		} catch (e) {}
+
+		return { url, isShortened };
 	},
 }
