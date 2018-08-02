23e8f1a6002cf15216c26edd02be4e4f58c11495
fenglish <asuka.ochi@ft.com>
2018-08-02 12:06:05 +0100

    reflect review: using constants to apply share types

--- components/x-gift-article/src/Url.jsx
+++ components/x-gift-article/src/Url.jsx
@@ -1,4 +1,5 @@
 import { h } from '@financial-times/x-engine';
+import { SHARE_TYPE_GIFT } from './lib/constants';
 import styles from './GiftArticle.css';
 
 const urlClassNames = [
@@ -6,14 +7,14 @@ const urlClassNames = [
 	styles.url
 ].join(' ');
 
-export default ({ isGift, isGiftUrlCreated, url, urlType }) => {
+export default ({ shareType, isGiftUrlCreated, url, urlType }) => {
 	return (
 		<input
 			type="text"
 			name={ urlType }
 			value={ url }
 			className={ urlClassNames }
-			disabled={ isGift && !isGiftUrlCreated }
+			disabled={ shareType === SHARE_TYPE_GIFT && !isGiftUrlCreated }
 			readOnly/>
 	);
 };
