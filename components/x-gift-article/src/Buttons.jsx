87a70eb09a3b8c654836d4168592b562d4655787
fenglish <asuka.ochi@ft.com>
2018-08-02 12:06:05 +0100

    reflect review: using constants to apply share types

--- components/x-gift-article/src/Buttons.jsx
+++ components/x-gift-article/src/Buttons.jsx
@@ -1,4 +1,5 @@
 import { h } from '@financial-times/x-engine';
+import { SHARE_TYPE_GIFT, SHARE_TYPE_NON_GIFT } from './lib/constants';
 import styles from './GiftArticle.css';
 
 const ButtonsClassName = styles.buttons;
@@ -16,12 +17,12 @@ const ButtonWithGapClassNames = [
 ].join(' ');
 
 
-export default ({ isGift, isGiftUrlCreated, mailtoUrl, createGiftUrl, copyGiftUrl, copyNonGiftUrl, showCopyButton }) => {
+export default ({ shareType, isGiftUrlCreated, mailtoUrl, createGiftUrl, copyGiftUrl, copyNonGiftUrl, showCopyButton }) => {
 
-	if (isGiftUrlCreated || !isGift) {
+	if (isGiftUrlCreated || shareType === SHARE_TYPE_NON_GIFT) {
 		return (
 			<div className={ ButtonsClassName }>
-				{ showCopyButton ? <button className={ ButtonWithGapClassNames } type="button" onClick={ isGift ? copyGiftUrl : copyNonGiftUrl }>Copy link</button> : null }
+				{ showCopyButton ? <button className={ ButtonWithGapClassNames } type="button" onClick={ shareType === SHARE_TYPE_GIFT ? copyGiftUrl : copyNonGiftUrl }>Copy link</button> : null }
 				<a className={ ButtonClassNames } href={ mailtoUrl } target="_blank">Email link</a>
 			</div>
 		);
