97d57d8907b1d690a440a3345b6003e98188bdb9
Matt Hinchliffe <matt@maketea.co.uk>
2018-08-04 10:16:16 +0100

    Add sidebar to package and component landing pages and individual pages

--- tools/x-docs-2/src/components/modules/list.jsx
+++ tools/x-docs-2/src/components/modules/list.jsx
@@ -3,11 +3,11 @@ import { Link } from 'gatsby';
 
 export const ListItem = ({ node }) => (
 	<li className="module-list__item">
-		<Link to={node.path}>{node.context.pageName}</Link>
+		<Link to={node.path}>{node.context.packageName}</Link>
 	</li>
 );
 
-export const List = ({ items, children }) => (
+export const List = ({ children }) => (
 	<ul className="module-list">
 		{children}
 	</ul>
