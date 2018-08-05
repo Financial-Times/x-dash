9d6187d156666eff0335588504d569dc998877a0
Matt Hinchliffe <matt@maketea.co.uk>
2018-08-05 20:04:53 +0100

    Style the front page hero and include logo

--- tools/x-docs-2/src/pages/index.jsx
+++ tools/x-docs-2/src/pages/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Layout from '../components/layouts/splash';
+import Hero from '../components/hero';
 
 export default () => (
 	<Layout title="Welcome">
+		<Hero />
 	</Layout>
 );
