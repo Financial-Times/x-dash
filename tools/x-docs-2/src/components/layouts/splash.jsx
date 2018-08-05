e9617edeb04233fdc9bc3bcc1ef03869a9bc4702
Matt Hinchliffe <matt@maketea.co.uk>
2018-08-05 11:18:50 +0100

    Update browser support list, add polyfill service

--- tools/x-docs-2/src/components/layouts/splash.jsx
+++ tools/x-docs-2/src/components/layouts/splash.jsx
@@ -2,21 +2,15 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import Banner from '../header/banner';
 
-export default ({ title, children, sidebar, support }) => (
+export default ({ title, children }) => (
 	<div className="site-layout">
 		<Helmet title={title} />
 		<header className="site-layout__header">
 			<Banner />
 		</header>
-		<main className="site-layout__content" role="main">
+		<main className="site-layout__hero" role="main">
 			{children}
 		</main>
-		<div className="site-layout__support" role="complementary">
-			{support}
-		</div>
-		<div className="site-layout__sidebar" role="complementary">
-			{sidebar}
-		</div>
 		<footer className="site-layout__footer" role="contentinfo" />
 	</div>
 );
