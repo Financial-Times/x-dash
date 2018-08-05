e54561e03c61a4814cb447c0be66522f587dd9da
Matt Hinchliffe <matt@maketea.co.uk>
2018-08-05 20:04:53 +0100

    Style the front page hero and include logo

--- tools/x-docs-2/src/components/layouts/basic.jsx
+++ tools/x-docs-2/src/components/layouts/basic.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import Banner from '../header/banner';
+import Header from '../header';
 
 export default ({ title, children, sidebar, support }) => (
 	<div className="site-layout">
 		<Helmet title={title} />
 		<header className="site-layout__header">
-			<Banner />
+			<Header />
 		</header>
 		<main className="site-layout__content" role="main">
 			{children}
 		</main>
-		<div className="site-layout__support" role="complementary">
-			{support}
-		</div>
 		<div className="site-layout__sidebar" role="complementary">
 			{sidebar}
 		</div>
--- tools/x-docs-2/src/components/layouts/splash.jsx
+++ tools/x-docs-2/src/components/layouts/splash.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import Banner from '../header/banner';
+import Header from '../header';
 
 export default ({ title, children }) => (
 	<div className="site-layout">
 		<Helmet title={title} />
 		<header className="site-layout__header">
-			<Banner />
+			<Header />
 		</header>
 		<main className="site-layout__hero" role="main">
 			{children}
