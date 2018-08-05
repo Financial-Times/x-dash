262b8eca01bb53ff90410ee0ef29778c90f68c25
Matt Hinchliffe <matt@maketea.co.uk>
2018-08-05 21:24:19 +0100

    Link up header logo

--- tools/x-docs-2/src/components/header/index.jsx
+++ tools/x-docs-2/src/components/header/index.jsx
@@ -4,7 +4,7 @@ import { Link } from 'gatsby';
 export default () => (
 	<div className="site-banner">
 		<div className="site-banner__logo">
-			x-dash
+			<Link to="/">x-dash</Link>
 		</div>
 		<nav role="navigation" className="site-banner__menu">
 			<Link to="/docs" activeClassName="is-active">Docs</Link>
