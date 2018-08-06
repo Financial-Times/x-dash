314f3d806ddd5fbe2b05e6cc1ced4534e8d2f80a
ludrob <robin.ludovic@gmail.com>
2018-08-06 17:27:38 +0100

    added knobs  🐿 v2.10.2

--- components/x-eventpromo/src/Link.jsx
+++ components/x-eventpromo/src/Link.jsx
@@ -1,7 +1,6 @@
 import { h } from '@financial-times/x-engine';
 
 const Link = ({url, attrs = {}, children}) => {
-	console.log('url', url);
 	return (
 		<a href={url} {...attrs}>{children}</a>
 	);
