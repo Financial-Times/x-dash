9f9bd0a02eb5e285cee37e234c253219274082b9
Matt Hinchliffe <matt@maketea.co.uk>
2018-08-06 08:46:21 +0100

    Add simple footer to docs site

---
+++ tools/x-docs-2/src/components/footer/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => (
+	<footer className="site-footer">
+		<p className="site-footer__small-print">
+			<small>
+				© The Financial Times Ltd.
+				FT and 'Financial Times' are trademarks of The Financial Times Ltd.
+			</small>
+		</p>
+		<a className="site-footer__github" href="https://github.com/financial-times/x-dash">
+			View x-dash on GitHub
+		</a>
+	</footer>
+);
