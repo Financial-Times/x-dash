bce2dc7ebc3e043f15812fcf42af78f4928a1001
Matt Hinchliffe <matt.hinchliffe@ft.com>
2018-08-07 16:32:44 +0100

    pageName => title, sourceName => source  🐿 v2.10.1

--- tools/x-docs-2/src/pages/packages.jsx
+++ tools/x-docs-2/src/pages/packages.jsx
@@ -7,7 +7,7 @@ import { List, ListItem } from '../components/module-list';
 export const query = graphql`
 	query {
 		modules: allNpmPackage(
-			filter: { private: { eq: false }, fields: { sourceName: { eq: "packages" } } }
+			filter: { private: { eq: false }, fields: { source: { eq: "packages" } } }
 		) {
 			edges {
 				node {
@@ -24,11 +24,9 @@ export const query = graphql`
 
 export default ({ data }) => (
 	<Layout title="Packages" sidebar={<Sidebar data={data.modules.edges} title="Packages" />}>
-		<div className="">
-			<h1>Packages</h1>
-			<List>
-				{data.modules.edges.map(({ node }, i) => <ListItem key={`module-${i}`} node={node} />)}
-			</List>
-		</div>
+		<h1>Packages</h1>
+		<List>
+			{data.modules.edges.map(({ node }, i) => <ListItem key={`module-${i}`} node={node} />)}
+		</List>
 	</Layout>
 );
