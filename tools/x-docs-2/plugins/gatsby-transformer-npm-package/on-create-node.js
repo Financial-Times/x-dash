1f877a73c44ec44ae059ba2f8ed7f76495ec5d05
Matt Hinchliffe <matt@maketea.co.uk>
2018-08-07 08:47:28 +0100

    Add component stories node type

--- tools/x-docs-2/plugins/gatsby-transformer-npm-package/on-create-node.js
+++ tools/x-docs-2/plugins/gatsby-transformer-npm-package/on-create-node.js
@@ -1,9 +1,11 @@
 const crypto = require('crypto');
 
+const hash = (string) => crypto.createHash('md5').update(string).digest('hex');
+
 module.exports = async ({ node, loadNodeContent, actions }) => {
 	const { createNode, createParentChildLink } = actions;
 
-	if (node.base === 'package.json') {
+	if (node.internal.type === 'File' && node.base === 'package.json') {
 		const content = await loadNodeContent(node);
 		const json = JSON.parse(content);
 
@@ -13,23 +15,37 @@ module.exports = async ({ node, loadNodeContent, actions }) => {
 			children: [],
 			parent: node.id,
 			internal: {
-				content,
-				type: 'NpmPackage',
+				type: 'NpmPackage'
 			},
 			manifest: json,
-			name: json.name,
+			name: json.name.replace('@financial-times/', ''),
 			private: Boolean(json.private),
-			// mimic remark transformer
+			// Mimic remark transformer
 			fileAbsolutePath: node.absolutePath
 		};
 
 		// Append unique node hash
-		packageNode.internal.contentDigest = crypto
-			.createHash('md5')
-			.update(JSON.stringify(packageNode))
-			.digest('hex');
+		packageNode.internal.contentDigest = hash(JSON.stringify(packageNode));
 
 		createNode(packageNode);
 		createParentChildLink({ parent: node, child: packageNode });
+	}
+
+	if (node.internal.type === 'File' && node.absolutePath.endsWith('stories/index.js')) {
+		const storiesNode = {
+			id: `${node.id} >>> Stories`,
+			children: [],
+			parent: node.id,
+			internal: {
+				type: 'Stories'
+			},
+			fileAbsolutePath: node.absolutePath
+		};
+
+		// Append unique node hash
+		storiesNode.internal.contentDigest = hash(JSON.stringify(storiesNode));
+
+		createNode(storiesNode);
+		createParentChildLink({ parent: node, child: storiesNode });
 	}
 };
--- tools/x-docs-2/src/pages/components.jsx
+++ tools/x-docs-2/src/pages/components.jsx
@@ -11,6 +11,7 @@ export const query = graphql`
 		) {
 			edges {
 				node {
+					name
 					manifest
 					fields {
 						slug
