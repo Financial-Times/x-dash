c8c83785f96fa6219fa9d2f1b446bb8079df2cf1
ludrob <robin.ludovic@gmail.com>
2018-08-07 11:41:25 +0100

    fixed tests, removed test css  🐿 v2.10.2

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
 	testURL: 'https://local.ft.com/',
 	testMatch: ['**/__tests__/**/*.test.js'],
-	testPathIgnorePatterns: ['/node_modules/', '/bower_components/']
+	testPathIgnorePatterns: ['/node_modules/', '/bower_components/'],
+	verbose: true,
+	testURL: "http://localhost/",
 };
