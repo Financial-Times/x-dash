fb7187b307122053261cb363bc6ba3c26607ab75
ludrob <robin.ludovic@gmail.com>
2018-08-07 15:15:25 +0100

    renamed vars, removed prefix  🐿 v2.10.2

--- components/x-eventpromo/stories/eventpromo.js
+++ components/x-eventpromo/stories/eventpromo.js
@@ -1,25 +1,25 @@
 exports.title = 'Eventpromo';
 
 exports.data = {
-	'eventpromoId': 'fancyEventId',
-	'eventpromoLink': 'https://live.ft.com/Events/2018/FT-Brexit-and-Beyond-Summit?segmentId=some-segment-id&amp;variant=false',
-	'eventpromoTitle': 'FT Brexit and Beyond Summit',
-	'location': 'planet earth',
 	'dates': '12 November - 14 November 2018',
+	'id': 'fancyEventId',
 	'image1': 'https://www.ft.com/__assets/creatives/better-promo/break_out.jpg',
 	'image2': 'https://www.ft.com/__assets/creatives/better-promo/audiance_clapping.jpg',
-	'image3': 'https://live.ft.com/var/ftlive/storage/images/events/2018/ft-brexit-and-beyond-summit/888258-7-eng-GB/FT-Brexit-and-Beyond-Summit.png'
+	'image3': 'https://live.ft.com/var/ftlive/storage/images/events/2018/ft-brexit-and-beyond-summit/888258-7-eng-GB/FT-Brexit-and-Beyond-Summit.png',
+	'link': 'https://live.ft.com/Events/2018/FT-Brexit-and-Beyond-Summit?segmentId=some-segment-id&amp;variant=false',
+	'location': 'planet earth',
+	'title': 'FT Brexit and Beyond Summit'
 };
 
 exports.knobs = [
-	'eventpromoId',
-	'eventpromoLink',
-	'eventpromoTitle',
 	'dates',
+	'id',
 	'image1',
 	'image2',
 	'image3',
-	'location'
+	'link',
+	'location',
+	'title'
 ];
 
 // This reference is only required for hot module loading in development
--- components/x-eventpromo/stories/index.js
+++ components/x-eventpromo/stories/index.js
@@ -10,14 +10,8 @@ exports.knobs = (data, { text }) => ({
 	dates() {
 		return text('Dates', data.dates);
 	},
-	eventpromoId() {
-		return text('Eventpromo Id', data.eventpromoId);
-	},
-	eventpromoLink() {
-		return text('Eventpromo link', data.eventpromoLink);
-	},
-	eventpromoTitle() {
-		return text('Eventpromo title', data.eventpromoTitle);
+	id() {
+		return text('Id', data.id);
 	},
 	image1() {
 		return text('Image1', data.image1);
@@ -28,8 +22,14 @@ exports.knobs = (data, { text }) => ({
 	image3() {
 		return text('Image3', data.image3);
 	},
+	link() {
+		return text('Link', data.link);
+	},
 	location() {
 		return text('Location', data.location);
+	},
+	title() {
+		return text('Title', data.title);
 	}
 });
 
