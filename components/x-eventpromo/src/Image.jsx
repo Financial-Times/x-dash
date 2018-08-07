6200f28a0aea79fd7214b43e47ec3be81bf98f07
ludrob <robin.ludovic@gmail.com>
2018-08-07 15:41:07 +0100

    fixes for Image.jsx  🐿 v2.10.2

--- components/x-eventpromo/src/Image.jsx
+++ components/x-eventpromo/src/Image.jsx
@@ -7,8 +7,13 @@ const Image = ({linkUrl, imageUrl}) => {
 	const imgSrc = `${baseSrc}width=340 340w,${baseSrc}width=400 740w`;
 
 	return (
-		<Link url={linkUrl} attrs={{ 'className': 'js-event-promo--animate', 'data-trackable': 'event-promo'}}>
-			<img className="js-event-promo--animate event-promo-inarticle__img fade-0" srcSet={imgSrc} alt="" data-n-image-lazy-load-js="" role="presentation" styles={{size: '(max-width: 46.24em) 100vw, 45vw'}} />
+		<Link url={linkUrl} attrs={{
+			'className': 'js-event-promo--animate',
+			'data-trackable': 'event-promo',
+			'aria-hidden': true,
+			'tabindex': -1
+		}}>
+			<img className="js-event-promo--animate event-promo-inarticle__img fade-0" srcSet={imgSrc} alt="" data-n-image-lazy-load-js="" role="presentation" sizes="(max-width: 46.24em) 100vw, 45vw" />
 		</Link>
 	);
 };
