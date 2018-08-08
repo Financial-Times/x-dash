5e82d4898555e5cd6033d24bc32e229e848e00ed
Katerina <katerina.loschinina@ft.com>
2018-08-08 22:05:34 +0100

    display headings with depths 2 and 3 as a subitems  🐿 v2.10.2

--- tools/x-docs-2/src/components/sidebar/module-list.jsx
+++ tools/x-docs-2/src/components/sidebar/module-list.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-const List = ({ data, title }) => (
+const createUrlFromName = (name) => {
+	return name.toLowerCase().replace(/ /g, '-');
+}
+
+const buildListItem = (item, currentNode) => {
+	return (
+		<Link to={`${currentNode.fields.slug}#${createUrlFromName(item.value)}`}>
+			<li>{item.value}</li>
+		</Link>
+	);
+}
+
+const buildSubheadingsList = (menu, currentNode) => {
+	const currentMenu = menu.filter(item => item.node.headings[0].value === currentNode.name)[0].node.headings;
+	return currentMenu.map(item => {
+		if (item.depth === 2) {
+			return buildListItem(item, currentNode);
+		} else if (item.depth === 3) {
+			return (
+				<ul>
+					{ buildListItem(item, currentNode) }
+				</ul>
+			);
+		}
+		return null;
+	});
+}
+
+const Sidebar = ({ data, title, menu }) => (
 	<ul className="site-sidebar">
 		<li className="site-sidebar__item site-sidebar__item--title">{title}</li>
 		{data.map(({ node }, i) => (
@@ -9,9 +37,14 @@ const List = ({ data, title }) => (
 				<Link to={node.path} exact activeClassName="is-active">
 					{node.context.title}
 				</Link>
+				{ menu && node.fields.slug === document.location.pathname ? (
+					<ul>
+						{ buildSubheadingsList(menu, node) }
+					</ul>
+				) : null }
 			</li>
 		))}
 	</ul>
 );
 
-export default List;
+export default Sidebar;
