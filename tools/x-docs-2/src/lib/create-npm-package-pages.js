d466c8b593f60f6786f38b10b5c404e4ceebf0dd
Matt Hinchliffe <matt.hinchliffe@ft.com>
2018-08-09 14:56:28 +0100

    Neaten up npm package page creation using new slugs  🐿 v2.10.1

--- tools/x-docs-2/src/lib/create-npm-package-pages.js
+++ tools/x-docs-2/src/lib/create-npm-package-pages.js
@@ -23,10 +23,13 @@ module.exports = async (actions, graphql) => {
 	}
 
 	result.data.npmPackages.edges.map(({ node }) => {
+		// Package manifest slug will be /package so remove it
+		const pagePath = path.dirname(node.fields.slug);
+
 		actions.createPage({
 			component: path.resolve('src/templates/npm-package.jsx'),
 			// Remove the file name from the slug
-			path: path.resolve(node.fields.slug, '..'),
+			path: pagePath,
 			// Data passed to context is available in page queries as GraphQL variables.
 			context: {
 				type: `npm-package-${node.fields.source}`,
@@ -34,9 +37,9 @@ module.exports = async (actions, graphql) => {
 				packageName: node.manifest.name,
 				packageDescription: node.manifest.description,
 				// Associate readme and story nodes via slug
-				readmePath: path.resolve(node.fields.slug, '../readme'),
-				storiesPath: path.resolve(node.fields.slug, '../stories'),
-				packagePath: path.resolve(node.fields.slug, '../package')
+				storiesPath: path.join(pagePath, 'stories'),
+				packagePath: path.join(pagePath, 'package'),
+				readmePath: path.join(pagePath, 'readme')
 			}
 		});
 	});
