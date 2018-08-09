ecb8e5e1dcd131fac7db9f5806855a4757024ffd
Katerina <katerina.loschinina@ft.com>
2018-08-09 18:37:34 +0100

    fix path search and subheadings title  🐿 v2.10.2

--- tools/x-docs-2/src/components/sidebar/module-list.jsx
+++ tools/x-docs-2/src/components/sidebar/module-list.jsx
@@ -28,15 +28,15 @@ const buildSubheadingsList = (menu, currentNode) => {
 	});
 }
 
-const Sidebar = ({ data, title, menu }) => (
+const Sidebar = ({ data, title, menu, pageContext }) => (
 	<ul className="site-sidebar">
-		<li className="site-sidebar__item site-sidebar__item--title">{title}</li>
+		<li className="site-sidebar__item site-sidebar__item--title">{title || pageContext.source}</li>
 		{ data.map(({ node }, i) => (
 			<li key={`list-${i}`} className="site-sidebar__item site-sidebar__item--link">
 				<Link to={node.path} exact activeClassName="is-active">
 					{node.context.title}
 				</Link>
-				{ menu && menu.headings.length > 0 && node.path === document.location.pathname ? (
+				{ menu && menu.headings.length > 0 && pageContext && pageContext.source && pageContext.title && node.path === `/${pageContext.source}/${pageContext.title}` ? (
 					<ul>
 						{ buildSubheadingsList(menu, node) }
 					</ul>
