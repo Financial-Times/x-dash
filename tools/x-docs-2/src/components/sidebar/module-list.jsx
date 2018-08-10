ca4e136a69b12310486746dc13d0e2472c76dd83
Katerina <katerina.loschinina@ft.com>
2018-08-10 17:20:57 +0100

    through props pass location and title instaed of all page context  🐿 v2.10.2

--- tools/x-docs-2/src/components/sidebar/module-list.jsx
+++ tools/x-docs-2/src/components/sidebar/module-list.jsx
@@ -36,15 +36,15 @@ const buildSubheadingsList = (menu, currentNode) => {
 	});
 }
 
-const Sidebar = ({ data, title, menu, pageContext }) => (
+const Sidebar = ({ data, title, menu, location }) => (
 	<ul className="site-sidebar">
-		<li className="site-sidebar__item site-sidebar__item--title">{title || pageContext.source}</li>
+		<li className="site-sidebar__item site-sidebar__item--title">{title}</li>
 		{ data.map(({ node }, i) => (
 			<li key={`list-${i}`} className="site-sidebar__item site-sidebar__item--link">
 				<Link to={node.path} exact activeClassName="is-active">
 					{node.context.title}
 				</Link>
-				{ menu && menu.headings.length > 0 && pageContext && pageContext.source && pageContext.title && node.path === `/${pageContext.source}/${pageContext.title}` ? (
+				{ menu && menu.headings.length > 0 && node.path === location ? (
 					<ul>
 						{ buildSubheadingsList(menu, node) }
 					</ul>
