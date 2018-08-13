b2d006e55b1f16f7ef015c64c2ad200413a7627c
Matt Hinchliffe <matt.hinchliffe@ft.com>
2018-08-13 17:07:03 +0100

    Avoid JSDOM 11.4 localstorage error  🐿 v2.10.1

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	testURL: 'https://local.ft.com/',
 	testMatch: ['**/__tests__/**/*.test.js'],
-	testPathIgnorePatterns: ['/node_modules/', '/bower_components/']
+	testPathIgnorePatterns: ['/node_modules/', '/bower_components/'],
+	testURL: 'http://localhost/'
 };
