bb5fafc88b52ae007b51688a4eeb56edb40d5ef8
fenglish <asuka.ochi@ft.com>
2018-08-13 15:32:37 +0100

    delete knobs  🐿 v2.10.2

--- components/x-gift-article/stories/knobs.js
+++
@@ -1,9 +0,0 @@
-// To ensure that component stories do not need to depend on Storybook themselves we return a
-// function that may be passed the required dependencies.
-module.exports = (data, { object, text, number, boolean, date, selectV2 }) => {
-	return {
-		title() {
-			return text('Title', data.title);
-		}
-	}
-};
