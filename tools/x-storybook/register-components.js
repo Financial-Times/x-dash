14d20be7c66159de38806e4542c339b756779182
fenglish <asuka.ochi@ft.com>
2018-08-14 16:14:29 +0100

    tidy up  🐿 v2.10.2

--- tools/x-storybook/register-components.js
+++ tools/x-storybook/register-components.js
@@ -1,10 +1,8 @@
-// TODO Need to change the relative path to '@financial-times/x-gift-article/stories'
-// after releasing new version of x-dash with x-gift-article
 const components = [
-	require('../../components/x-gift-article/stories'),
 	require('@financial-times/x-teaser/stories'),
 	require('@financial-times/x-increment/stories'),
 	require('@financial-times/x-styling-demo/stories'),
+	require('@financial-times/x-gift-article/stories'),
 ];
 
 module.exports = components;
