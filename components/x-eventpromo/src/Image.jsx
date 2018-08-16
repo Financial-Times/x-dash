75e5a76633fb7971f41d1a0ca7163f0020b47207
ludrob <robin.ludovic@gmail.com>
2018-08-16 17:07:33 +0100

    more minor changes to address PR comments  🐿 v2.10.2

--- components/x-eventpromo/src/Image.jsx
+++ components/x-eventpromo/src/Image.jsx
@@ -1,20 +1,24 @@
 import { h } from '@financial-times/x-engine';
-import Link from './Link';
 
-const Image = ({linkUrl, imageUrl}) => {
-
+const Image = ({ linkUrl, imageUrl }) => {
 	const baseSrc = `https://www.ft.com/__origami/service/image/v2/images/raw/${imageUrl}?source=next&amp;fit=cover&amp;compression=best&amp;`;
 	const imgSrc = `${baseSrc}width=340 340w,${baseSrc}width=400 740w`;
 
 	return (
-		<Link url={linkUrl} attrs={{
-			'className': 'js-event-promo--animate',
-			'data-trackable': 'event-promo',
-			'aria-hidden': true,
-			'tabindex': -1
-		}}>
-			<img className="js-event-promo--animate event-promo-inarticle__img fade-0" srcSet={imgSrc} alt="" data-n-image-lazy-load-js="" role="presentation" sizes="(max-width: 46.24em) 100vw, 45vw" />
-		</Link>
+		<a href={linkUrl}
+						className="js-event-promo--animate"
+						data-trackable="event-promo"
+						aria-hidden="true"
+						tabIndex="-1"
+		>
+			<img
+				className="js-event-promo--animate event-promo-inarticle__img fade-0"
+				srcSet={imgSrc} alt=""
+				data-n-image-lazy-load-js=""
+				role="presentation"
+				sizes="(max-width: 46.24em) 100vw, 45vw"
+			/>
+		</a>
 	);
 };
 
--- components/x-eventpromo/stories/eventpromo.js
+++ components/x-eventpromo/stories/eventpromo.js
@@ -3,9 +3,11 @@ exports.title = 'Eventpromo';
 exports.data = {
 	'dates': '12 November - 14 November 2018',
 	'id': 'fancyEventId',
-	'image1': 'https://www.ft.com/__assets/creatives/better-promo/break_out.jpg',
-	'image2': 'https://www.ft.com/__assets/creatives/better-promo/audiance_clapping.jpg',
-	'image3': 'https://live.ft.com/var/ftlive/storage/images/events/2018/ft-brexit-and-beyond-summit/888258-7-eng-GB/FT-Brexit-and-Beyond-Summit.png',
+	'images': [
+		'https://live.ft.com/var/ftlive/storage/images/events/2018/ft-brexit-and-beyond-summit/888258-7-eng-GB/FT-Brexit-and-Beyond-Summit.png',
+		'https://www.ft.com/__assets/creatives/better-promo/audiance_clapping.jpg',
+		'https://www.ft.com/__assets/creatives/better-promo/break_out.jpg',
+	],
 	'link': 'https://live.ft.com/Events/2018/FT-Brexit-and-Beyond-Summit?segmentId=some-segment-id&amp;variant=false',
 	'location': 'planet earth',
 	'title': 'FT Brexit and Beyond Summit'
@@ -14,9 +16,7 @@ exports.data = {
 exports.knobs = [
 	'dates',
 	'id',
-	'image1',
-	'image2',
-	'image3',
+	'images',
 	'link',
 	'location',
 	'title'
