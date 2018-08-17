971010d531446dbb196c40844f479a7159352d92
ludrob <robin.ludovic@gmail.com>
2018-08-17 16:51:52 +0100

    updated snapshots  🐿 v2.10.2

--- components/x-eventpromo/src/ImagesContainer.jsx
+++ components/x-eventpromo/src/ImagesContainer.jsx
@@ -6,8 +6,8 @@ const ImagesContainer = ({link, images}) => {
 	return (
 		<div className="event-promo-inarticle__blocks event-promo-inarticle__img-block">
 			<button className="event-promo__control js-event-promo__control" aria-label="control animation"></button>
-			{images.map((image) =>
-				<Image linkUrl={link} imageUrl={image}/>
+			{images.map((image, index) =>
+				<Image key={index} linkUrl={link} imageUrl={image}/>
 			)}
 		</div>
 	);
