517503f01dce17137e15e87389a043a6fc577e15
ludrob <robin.ludovic@gmail.com>
2018-08-17 14:45:22 +0100

    added header for event-promo__details  🐿 v2.10.2

--- components/x-eventpromo/src/Details.jsx
+++ components/x-eventpromo/src/Details.jsx
@@ -5,8 +5,8 @@ import Meta from './Meta';
 const Details = ({link, title, ...props}) => {
 
 	return (
-		<div className="event-promo-inarticle__blocks">
-
+		<div className="event-promo-inarticle__blocks" aria-labelledby="details-header">
+			<h1 id="details-header" style={{display: 'none'}}>Event details and information</h1>
 			<div className="event-promo__details">
 				<a href={link} className="event-promo__title" data-trackable="event-promo">
 					{title}
