3de7bc747c6949cb5e7e363810f74a2ee8198f1c
Katerina <katerina.loschinina@ft.com>
2018-08-20 13:04:28 +0100

    solve merge inconsistency  🐿 v2.10.2

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
 	testURL: 'https://local.ft.com/',
 	testMatch: ['**/__tests__/**/*.test.js'],
-	testPathIgnorePatterns: ['/node_modules/', '/bower_components/'],
-	testURL: 'http://local.ft.com'
+	testPathIgnorePatterns: ['/node_modules/', '/bower_components/']
 };
