a747f17e2049abdc81f536ec5b3bb8d3a888006a
ludrob <robin.ludovic@gmail.com>
2018-08-20 10:32:30 +0100

    removed duplicate key on jest.config.js  🐿 v2.10.2

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,5 @@ module.exports = {
 	testURL: 'https://local.ft.com/',
 	testMatch: ['**/__tests__/**/*.test.js'],
 	testPathIgnorePatterns: ['/node_modules/', '/bower_components/'],
-	verbose: true,
-	testURL: "http://localhost/",
+	verbose: true
 };
