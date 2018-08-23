b08b86b6636c97becaaaa2325f5801c68f9819f2
ludrob <robin.ludovic@gmail.com>
2018-08-23 13:42:36 +0100

    changed details-header  🐿 v2.10.2

--- components/x-eventpromo/src/Details.jsx
+++ components/x-eventpromo/src/Details.jsx
@@ -6,7 +6,7 @@ const Details = ({link, title, ...props}) => {
 
 	return (
 		<div className="event-promo-inarticle__blocks" aria-labelledby="details-header">
-			<h1 id="details-header" style={{display: 'none'}}>Event details and information</h1>
+			<div id="details-header" hidden>Event details and information</div>
 			<div className="event-promo__details">
 				<a href={link} className="event-promo__title" data-trackable="event-promo">
 					{title}
