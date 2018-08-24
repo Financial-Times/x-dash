de38beff88dac59136093b45112f6a0f49cacbc9
Matt Hinchliffe <matt@maketea.co.uk>
2018-08-24 13:04:21 +0100

    Fix sidebar submenu markup and indent using CSS rather than nested list items

---
+++ tools/x-docs-2/src/components/sidebar/module-menu.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import GithubSlugger from 'github-slugger';
+
+const createHash = (name) => {
+	const slugger = new GithubSlugger();
+	// As slugger checks if it is not duplicating created urls, like this:
+	// const slugger = new GithubSlugger();
+	// slugger.slug('url one') // url-one
+	// slugger.slug('url one') // url-one-2
+	// therefore there is a need to create a new class instance every time when the function is applied
+	return slugger.slug(name)
+};
+
+const submenuItemOnClick = (e) => {
+	e.preventDefault();
+
+	document.querySelector(e.currentTarget.hash).scrollIntoView({
+		behavior: 'smooth'
+	});
+};
+
+const submenuItemIndent = (depth) => {
+	return {
+		paddingLeft: `${depth}em`
+	};
+};
+
+const Submenu = (items, maxDepth = 3) => {
+	const headings = items.filter((item) => item.depth > 1 && item.depth <= maxDepth);
+
+	return (
+		<ul className="site-sidebar__submenu">
+			{headings.map((item, i) => (
+				<li key={`submenu-${i}`} className="site-sidebar__submenu-item" style={submenuItemIndent(item.depth)}>
+					<Link to={`#${createHash(item.value)}`} onClick={submenuItemOnClick}>
+						{item.value}
+					</Link>
+				</li>
+			))}
+		</ul>
+	);
+};
+
+const Sidebar = ({ heading, modules, submenu, location }) => (
+	<div className="site-sidebar">
+		<p className="site-sidebar__heading">{heading}</p>
+		<ul className="site-sidebar__menu">
+			{modules.map(({ node }, i) => (
+				<li key={`menu-${i}`} className="site-sidebar__menu-item">
+					<Link to={node.path} exact activeClassName="is-active">
+						{node.context.title}
+					</Link>
+					{submenu && submenu.length > 0 && node.path === location ? Submenu(submenu) : null}
+				</li>
+			))}
+		</ul>
+	</div>
+);
+
+export default Sidebar;
