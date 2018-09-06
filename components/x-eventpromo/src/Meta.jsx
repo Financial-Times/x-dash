970c98ccf68e3be342e2fc40057f6477592f1842
ludrob <robin.ludovic@gmail.com>
2018-09-06 18:12:04 +0100

    move styles to x-dash  🐿 v2.10.2

--- components/x-eventpromo/src/Meta.jsx
+++ components/x-eventpromo/src/Meta.jsx
@@ -1,8 +1,9 @@
 import { h } from '@financial-times/x-engine';
+import styles from './Meta.scss';
 
 const Meta = ({location, dates}) => {
 	return (
-		<div className="event-promo__meta">
+		<div className={styles.meta}>
 			<p>{location}</p>
 			<p>{dates}</p>
 		</div>
