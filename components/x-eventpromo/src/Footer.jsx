e8de6b6a39eebca3b92db5fde6038497ef759eb1
ludrob <robin.ludovic@gmail.com>
2018-09-06 18:12:04 +0100

    move styles to x-dash  🐿 v2.10.2

--- components/x-eventpromo/src/Footer.jsx
+++ components/x-eventpromo/src/Footer.jsx
@@ -1,15 +1,16 @@
 import { h } from '@financial-times/x-engine';
+import styles from './Footer.scss';
 
 const Footer = ({url}) => {
-	return <div className="event-promo-inarticle__cta">
-		<div className="event-promo-inarticle__btn-block">
-			<a href={url} className="event-promo-inarticle__btn" data-trackable="event-promo">
+	return <div className={styles['cta-container']}>
+		<div className={styles['btn-block']}>
+			<a href={url} className={styles['btn']} data-trackable="event-promo">
 				Register now
 			</a>
 		</div>
-		<div className="event-promo-inarticle__brand">
+		<div className={styles['brand']}>
 			Presented by
-			<span className="event-promo__visually-hidden">FT live</span>
+			<span className={styles['visually-hidden']}>FT live</span>
 		</div>
 	</div>
 };
