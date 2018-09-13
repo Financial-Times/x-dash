ab5389fa64a6716450de684bf67e1d0d61bdbc8c
Dan Searle <dan@interfacer.co.uk>
2018-09-13 15:19:17 +0100

    Knobs!

--- components/x-article-save-button/stories/basic.js
+++ components/x-article-save-button/stories/basic.js
@@ -1,6 +1,20 @@
 exports.title = 'Basic ArticleSaveButton';
 
-exports.data = {};
+exports.data = {
+	saved: false,
+	csrfToken: 'dummy-token',
+	action: '/article-save',
+	contentId: '0000-0000-0000-0000',
+	method: 'POST'
+};
+
+exports.knobs = [
+	'action',
+	'method',
+	'csrfToken',
+	'contentId',
+	'saved'
+];
 
 // This reference is only required for hot module loading in development
 // <https://webpack.js.org/concepts/hot-module-replacement/>
