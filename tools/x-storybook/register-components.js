bcaede362acb98a52c6f45e13b1fbaaeef177fcc
Dan Searle <dan@interfacer.co.uk>
2018-09-14 09:43:20 +0100

    Register x-article-save-button component in x-workbench correctly.

--- tools/x-storybook/register-components.js
+++ tools/x-storybook/register-components.js
@@ -1,5 +1,5 @@
 const components = [
-	require('../../components/x-article-save-button/stories'),
+	require('@financial-times/x-article-save-button/stories'),
 	require('@financial-times/x-teaser/stories'),
 	require('@financial-times/x-increment/stories'),
 	require('@financial-times/x-styling-demo/stories'),
