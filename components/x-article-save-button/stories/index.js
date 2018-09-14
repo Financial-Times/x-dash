275223c226158f6720abd6d97a19d00d95805913
Dan Searle <dan@interfacer.co.uk>
2018-09-14 14:32:42 +0100

    Include o-fonts in storybook.

--- components/x-article-save-button/stories/index.js
+++ components/x-article-save-button/stories/index.js
@@ -2,7 +2,10 @@ const { ArticleSaveButton } = require('../');
 
 exports.component = ArticleSaveButton;
 exports.package = require('../package.json');
+exports.knobs = require('./knobs');
+exports.dependencies = {
+	'o-fonts': '^3.0.0'
+};
 exports.stories = [
 	require('./basic')
 ];
-exports.knobs = require('./knobs');
