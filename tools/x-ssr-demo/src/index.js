056100d3e291d7dc34b5b4387ccc80f6848215a9
Matt Brennan <matt@153.io>
2018-09-17 18:06:54 +0100

    Revert "remove unused package"
    This reverts commit b10d8225e203a3fc5ce48b5a01c7c24ad480a192.

---
+++ tools/x-ssr-demo/src/index.js
@@ -0,0 +1,6 @@
+import '@financial-times/x-increment';
+import {hydrate} from '@financial-times/x-interaction';
+
+document.addEventListener('DOMContentLoaded', hydrate);
+
+import 'preact/devtools';
