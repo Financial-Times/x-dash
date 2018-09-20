9aa09cf88a28350d0fd6c9d858b2b99ff60eb483
Matt Hinchliffe <i-like-robots@users.noreply.github.com>
2018-09-20 12:08:26 +0100

    Add build service dependencies to blueprint for base document styling

--- private/blueprints/component/stories/index.js
+++ private/blueprints/component/stories/index.js
@@ -4,6 +4,12 @@ exports.component = {{componentName}};
 
 exports.package = require('../package.json');
 
+// Set up basic document styling using the Origami build service
+exports.dependencies = {
+	'o-normalise': '^1.6.0',
+	'o-typography': '^5.5.0'
+};
+
 exports.stories = [
 	require('./example')
 ];
