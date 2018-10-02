f7ab48e826464848ea1e8396176a7027c4d01187
ludrob <robin.ludovic@gmail.com>
2018-10-02 11:58:50 +0100

    added strapline

--- components/x-eventpromo/src/Details.jsx
+++ components/x-eventpromo/src/Details.jsx
@@ -3,7 +3,7 @@ import Footer from './Footer';
 import Meta from './Meta';
 import styles from './Details.scss';
 
-const Details = ({ link, title, ...props }) => {
+const Details = ({ link, title, strapline, ...props }) => {
 	return (
 		<div className={styles.block} aria-labelledby="details-header">
 			<div id="details-header" hidden>
@@ -13,8 +13,10 @@ const Details = ({ link, title, ...props }) => {
 				<a href={link} className={styles.title} data-trackable="event-promo">
 					{title}
 				</a>
-
 				<Meta {...props} />
+				<div className={styles.strapline}>
+					<p>{strapline}</p>
+				</div>
 				<Footer url={link} />
 			</div>
 		</div>
