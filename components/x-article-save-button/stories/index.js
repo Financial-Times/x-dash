fda4d7ecb4296554a2e15b24f7d65cc2dbd07b87
Dan Searle <dan@interfacer.co.uk>
2018-10-08 16:55:39 +0100

    Include all dependencies in story

--- components/x-article-save-button/stories/index.js
+++ components/x-article-save-button/stories/index.js
@@ -4,6 +4,8 @@ exports.component = ArticleSaveButton;
 exports.package = require('../package.json');
 exports.knobs = require('./knobs');
 exports.dependencies = {
+	"o-colors": "^4.7.2",
+	"o-icons": "^5.7.1",
 	'o-normalise': '^1.6.0',
 	'o-typography': '^5.5.0'
 };
