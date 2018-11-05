79ee588d00b6b197b7da06a85f106fab82a226f9
Matt Brennan <matt@153.io>
2018-11-05 17:39:20 +0000

    lolololol

--- components/x-gift-article/src/Url.jsx
+++ components/x-gift-article/src/Url.jsx
@@ -1,5 +1,5 @@
 import { h } from '@financial-times/x-engine';
-import { shareType.gift } from './lib/constants';
+import { ShareType } from './lib/constants';
 import styles from './GiftArticle.css';
 
 const urlClassNames = [
@@ -14,7 +14,7 @@ export default ({ shareType, isGiftUrlCreated, url, urlType }) => {
 			name={ urlType }
 			value={ url }
 			className={ urlClassNames }
-			disabled={ shareType === shareType.gift && !isGiftUrlCreated }
+			disabled={ shareType === ShareType.gift && !isGiftUrlCreated }
 			readOnly
 		/>
 	);
--- components/x-gift-article/src/lib/constants.js
+++ components/x-gift-article/src/lib/constants.js
@@ -1,9 +1,9 @@
-export const shareType = {
+export const ShareType = {
 	gift: 'gift',
 	nonGift: 'nonGift'
 };
 
-export const urlType = {
+export const UrlType = {
 	dummy: 'example-gift-link',
 	gift: 'gift-link',
 	nonGift: 'non-gift-link'
