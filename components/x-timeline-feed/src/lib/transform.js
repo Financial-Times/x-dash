1dda5854facbc6ab64a565e4cefbc9caf64f90b4
Dan Searle <dan@interfacer.co.uk>
2018-11-06 14:06:06 +0000

    Don't create real Date objects when getting just date from an ISO date-time, as it will cause the timezone to affect the results.

--- components/x-timeline-feed/src/lib/transform.js
+++ components/x-timeline-feed/src/lib/transform.js
@@ -1,16 +1,17 @@
 import {
-	formatToDateOnly,
 	getLocalisedISODate,
 	getTitleForArticleDateGroup,
 	splitLatestEarlier
 } from './date';
+
+export const getDateOnly = date => date.substr(0, 10);
 
 export const groupArticlesByLocalisedDate = (articles, timezoneOffset) => {
 	const articlesByLocalisedDate = {};
 
 	articles.forEach((article, index) => {
 		const localDateTime = getLocalisedISODate(article.publishedDate, timezoneOffset);
-		const localDate = formatToDateOnly(localDateTime);
+		const localDate = getDateOnly(localDateTime);
 
 		if (!articlesByLocalisedDate.hasOwnProperty(localDate)) {
 			articlesByLocalisedDate[localDate] = [];
@@ -28,7 +29,7 @@ export const groupArticlesByLocalisedDate = (articles, timezoneOffset) => {
 
 export const splitTodaysArticles = (articleGroups, localTodayDate, latestArticlesTime) => {
 	const firstGroupIsToday = articleGroups[0].date === localTodayDate;
-	const latestTimeIsToday = formatToDateOnly(latestArticlesTime) === localTodayDate;
+	const latestTimeIsToday = getDateOnly(latestArticlesTime) === localTodayDate;
 
 	if (!firstGroupIsToday || !latestTimeIsToday) {
 		return articleGroups;
@@ -64,7 +65,7 @@ export const getArticleGroups = props => {
 	const {
 		articles,
 		timezoneOffset = now.getTimezoneOffset(),
-		localTodayDate = formatToDateOnly(now),
+		localTodayDate = getDateOnly(now.toISOString()),
 		latestArticlesTime
 	} = props;
 
