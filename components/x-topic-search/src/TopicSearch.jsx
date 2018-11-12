e5d3803061f01e5980c8bbe060ef4d73486a630d
fenglish <asuka.ochi@ft.com>
2018-11-12 12:15:13 +0000

    first commit

---
+++ components/x-topic-search/src/TopicSearch.jsx
@@ -0,0 +1,10 @@
+import { h } from '@financial-times/x-engine';
+
+const TopicSearch = (props) => (
+	<div className='x-topic-search'>
+		<h1>Welcome to x-topic-search</h1>
+		<p>{props.message}</p>
+	</div>
+);
+
+export { TopicSearch };
