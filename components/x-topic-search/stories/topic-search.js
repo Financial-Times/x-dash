f1cba1c2eb47d17d89e71e2552086429db975347
fenglish <asuka.ochi@ft.com>
2018-11-12 16:47:26 +0000

    apply existing functions from ft.com topic search bar

---
+++ components/x-topic-search/stories/topic-search.js
@@ -0,0 +1,17 @@
+exports.title = 'Topic Search Bar';
+
+exports.data = {
+	minSearchLength: 2,
+	maxSuggestions: 10,
+	apiUrl: '//tag-facets-api.ft.com/annotations',
+	currentlyFollowingTopics: [
+		{
+			name: 'World Elephant Polo Association',
+			uuid: 'f95d1e16-2307-4feb-b3ff-6f224798aa49'
+		}
+	]
+};
+
+// This reference is only required for hot module loading in development
+// <https://webpack.js.org/concepts/hot-module-replacement/>
+exports.m = module;
