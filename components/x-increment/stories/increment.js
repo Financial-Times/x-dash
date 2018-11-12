50572f61e14c7157a0a1f3334e10811da2ec2a47
Dan Searle <dan@interfacer.co.uk>
2018-11-12 10:22:18 +0000

    More cleanup after rebase.

--- components/x-increment/stories/increment.js
+++ components/x-increment/stories/increment.js
@@ -5,8 +5,6 @@ exports.data = {
 	id: 'base-increment-static-id',
 };
 
-exports.knobs = ['count'];
-
 // This reference is only required for hot module loading in development
 // <https://webpack.js.org/concepts/hot-module-replacement/>
 exports.m = module;
