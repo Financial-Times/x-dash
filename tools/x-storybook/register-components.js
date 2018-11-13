87cb998eae66e6f4ec942d6b1835fdf9cc8f2a15
Dan Searle <dan@interfacer.co.uk>
2018-11-13 10:24:56 +0000

    x-teaser-list with x-article-save-button child component

--- tools/x-storybook/register-components.js
+++ tools/x-storybook/register-components.js
@@ -1,5 +1,6 @@
 const components = [
 	require('@financial-times/x-teaser/stories'),
+	require('@financial-times/x-teaser-list/stories'),
 	require('@financial-times/x-increment/stories'),
 	require('@financial-times/x-styling-demo/stories'),
 ];
