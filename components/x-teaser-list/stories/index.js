b49a8d4cb5a611a95fd56b3db63342ba9292ec06
Dan Searle <dan@interfacer.co.uk>
2018-11-13 10:24:56 +0000

    x-teaser-list with x-article-save-button child component

---
+++ components/x-teaser-list/stories/index.js
@@ -0,0 +1,15 @@
+const { TeaserList } = require('../');
+
+exports.component = TeaserList;
+
+exports.package = require('../package.json');
+
+// Set up basic document styling using the Origami build service
+exports.dependencies = {
+	'o-normalise': '^1.6.0',
+	'o-typography': '^5.5.0'
+};
+
+exports.stories = [
+	require('./example')
+];
