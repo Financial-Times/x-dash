ae1da791b11629ca6313b0536a28eb5aab36fd25
Dan Searle <dan@interfacer.co.uk>
2018-11-13 15:40:13 +0000

    Include o-teaser styles.

--- components/x-teaser-list/stories/index.js
+++ components/x-teaser-list/stories/index.js
@@ -7,7 +7,8 @@ exports.package = require('../package.json');
 // Set up basic document styling using the Origami build service
 exports.dependencies = {
 	'o-normalise': '^1.6.0',
-	'o-typography': '^5.5.0'
+	'o-typography': '^5.5.0',
+	'o-teaser': '^2.3.1'
 };
 
 exports.stories = [
