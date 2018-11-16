90cb87068fd4406488b09d0e8d66a861104bdb59
fenglish <asuka.ochi@ft.com>
2018-11-16 11:07:56 +0000

    make JSX being its own JSX component

--- components/x-topic-search/src/NoSuggestions.jsx
+++ components/x-topic-search/src/NoSuggestions.jsx
@@ -3,20 +3,20 @@ import styles from './TopicSearch.scss';
 import classNames from 'classnames';
 
 export default ({ searchTerm }) => (
-	<li className={ classNames(styles["no-suggestions"]) }>
+	<div className={ classNames(styles["no-suggestions"]) } aria-live="polite">
 
-		<h2 className={ classNames(styles["no-suggestions__title"]) }>
-			No topics matching <b>{ searchTerm }</b>
-		</h2>
+			<h2 className={ classNames(styles["no-suggestions__title"]) }>
+				No topics matching <b>{ searchTerm }</b>
+			</h2>
 
-		<p>Suggestions:</p>
+			<p>Suggestions:</p>
 
-		<ul className={ classNames(styles["no-suggestions__message"]) }>
-			<li>Make sure that all words are spelled correctly.</li>
-			<li>Try different keywords.</li>
-			<li>Try more general keywords.</li>
-			<li>Try fewer keywords.</li>
-		</ul>
+			<ul className={ classNames(styles["no-suggestions__message"]) }>
+				<li>Make sure that all words are spelled correctly.</li>
+				<li>Try different keywords.</li>
+				<li>Try more general keywords.</li>
+				<li>Try fewer keywords.</li>
+			</ul>
 
-	</li>
+	</div>
 );
--- components/x-topic-search/src/SuggestionList.jsx
+++ components/x-topic-search/src/SuggestionList.jsx
@@ -5,18 +5,25 @@ import classNames from 'classnames';
 export default ({ suggestions, searchTerm }) => {
 	 // TODO use x-follow-button
 	const listResults = suggestions.map((suggestion, index) => (
-		<li className={ classNames(styles["suggestion"]) }
-			key={ index }
-			data-trackable="myft-topic"
-			data-concept-id={ suggestion.conceptId }
-			data-trackable-meta={ '{"search-term":"' + searchTerm + '"}' }>
 
-			<a data-trackable="topic-link" className={ classNames(styles["suggestion__name"]) } href={ suggestion.url }>{ suggestion.prefLabel }</a>
+			<li className={ classNames(styles["suggestion"]) }
+				key={ index }
+				data-trackable="myft-topic"
+				data-concept-id={ suggestion.conceptId }
+				data-trackable-meta={ '{"search-term":"' + searchTerm + '"}' }>
 
-			<button>add to myFT</button>
+				<a data-trackable="topic-link" className={ classNames(styles["suggestion__name"]) } href={ suggestion.url }>
+					{ suggestion.prefLabel }
+				</a>
 
-		</li>
+				<button>add to myFT</button>
+
+			</li>
+
 	))
 
-	return listResults;
+	return (
+		<ul className={ classNames(styles["suggestions"]) } aria-live="polite">
+			{ listResults }
+		</ul>);
 };
