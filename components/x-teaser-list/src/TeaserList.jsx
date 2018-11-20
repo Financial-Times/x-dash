8672d69ff99ea3c3a9296688b7b5f7504a24ed55
Dan Searle <dan@interfacer.co.uk>
2018-11-20 12:10:15 +0000

    Simplify... no need for with-actions CSS class. CSS grid actions area size is dictated by its content. Remove redundant wrapper div.

--- components/x-teaser-list/src/TeaserList.jsx
+++ components/x-teaser-list/src/TeaserList.jsx
@@ -5,35 +5,33 @@ import classNames from 'classnames';
 import styles from './TeaserList.scss';
 
 const TeaserList = ({ items = [], showSaveButtons = true }) => (
-	<div className={classNames(styles.teaserList)}>
-		<ul className={classNames(styles.list)}>
-		{items.map(item => {
-			return (
-				<li
-					key={item.id}
-					className={classNames(styles.listItem, { [styles.listItemWithActions]: showSaveButtons })}
-				>
-					<div className={classNames(styles.listItem__article)}>
-						<Teaser
-							{...item}
-							{...presets.Small}
-							theme="teaser-list"
-						/>
-					</div>
-					{showSaveButtons && <div className={classNames(styles.listItem__actions)}>
-						<ArticleSaveButton
-							id={`${item.id}-save-button`}
-							contentId={item.id}
-							contentTitle={item.title}
-							csrfToken="dummy-token"
-							saved={item.saved || false}
-						/>
-					</div>}
-				</li>
-			);
-		})}
-		</ul>
-	</div>
+	<ul className={classNames(styles.list)}>
+	{items.map(item => {
+		return (
+			<li
+				key={item.id}
+				className={classNames(styles.listItem)}
+			>
+				<div className={classNames(styles.listItem__article)}>
+					<Teaser
+						{...item}
+						{...presets.Small}
+						theme="teaser-list"
+					/>
+				</div>
+				{showSaveButtons && <div className={classNames(styles.listItem__actions)}>
+					<ArticleSaveButton
+						id={`${item.id}-save-button`}
+						contentId={item.id}
+						contentTitle={item.title}
+						csrfToken="dummy-token"
+						saved={item.saved || false}
+					/>
+				</div>}
+			</li>
+		);
+	})}
+	</ul>
 );
 
 export { TeaserList };
