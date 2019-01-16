186edf8ba19672e4c56e488222a69b7c544829d3
dan-searle <dan.m.searle@gmail.com>
2019-01-16 10:33:51 +0000

    Support custom slot

--- components/x-teaser-timeline/src/TeaserTimeline.jsx
+++ components/x-teaser-timeline/src/TeaserTimeline.jsx
@@ -1,42 +1,59 @@
 import { h } from '@financial-times/x-engine';
 import { ArticleSaveButton } from '@financial-times/x-article-save-button';
 import { Teaser, presets } from '@financial-times/x-teaser';
-import { getItemGroups } from './lib/transform';
+import { getGroupAndIndex, getItemGroups } from './lib/transform';
 import styles from './TeaserTimeline.scss';
 import classNames from 'classnames';
 
 const TeaserTimeline = props => {
 	const {
 		csrfToken = null,
+		customSlotContent,
+		customSlotPosition = 2,
+		items,
 		showSaveButtons = true
 	} = props;
 	const itemGroups = getItemGroups(props);
 
-	return itemGroups.length && (
+	if (customSlotContent) {
+		const insertPosition = Math.min(customSlotPosition, items.length);
+		const insert = getGroupAndIndex(itemGroups, insertPosition);
+
+		itemGroups[insert.group].items.splice(insert.index, 0, customSlotContent);
+	}
+
+	return itemGroups.length > 0 && (
 		<div>
 			{itemGroups.map(group => (
 				<section key={group.date} className={classNames(styles.itemGroup)}>
 					<h2 className={classNames(styles.itemGroup__heading)}>{group.title}</h2>
 					<ul className={classNames(styles.itemGroup__items)}>
-						{group.items.map(item => (
-							<li key={item.id} className={styles.item}>
-								<Teaser
-									{...item}
-									{...presets.SmallHeavy}
-									theme="timeline-teaser"
-								/>
-								{showSaveButtons &&
-								<div className={classNames(styles.itemActions)}>
-									<ArticleSaveButton
-										id={`${item.id}-save-button`}
-										contentId={item.id}
-										contentTitle={item.title}
-										csrfToken={csrfToken}
-										saved={item.saved || false}
-									/>
-								</div>}
-							</li>
-						))}
+						{group.items.map(item => {
+							if (item.id) {
+								return (
+									<li key={item.id} className={styles.item}>
+										<Teaser
+											{...item}
+											{...presets.SmallHeavy}
+											theme="timeline-teaser"
+										/>
+										{showSaveButtons &&
+										<div className={classNames(styles.itemActions)}>
+											<ArticleSaveButton
+												id={`${item.id}-save-button`}
+												contentId={item.id}
+												contentTitle={item.title}
+												csrfToken={csrfToken}
+												saved={item.saved || false}
+											/>
+										</div>}
+									</li>
+								);
+							}
+							if (typeof item === 'string') {
+								return (<li key="custom-slot" dangerouslySetInnerHTML={{ __html: item }} />);
+							}
+						})}
 					</ul>
 				</section>
 			))}
@@ -44,4 +61,4 @@ const TeaserTimeline = props => {
 	);
 };
 
-export { TeaserTimeline };
+export { TeaserTimeline, getGroupAndIndex };
