b3ac0184d1e4b8f147eb4decf304fb8e2fc84113
fenglish <asuka.ochi@ft.com>
2019-01-22 12:20:51 +0000

    Separate api response into followed topics and not followed topics

--- components/x-topic-search/src/lib/get-suggestions.js
+++ components/x-topic-search/src/lib/get-suggestions.js
@@ -3,7 +3,23 @@ const addQueryParamToUrl = (name, value, url, append = true) => {
 	return append === true ? `${url}&${queryParam}` : `${url}?${queryParam}`;
 };
 
+const separateFollowedAndNot = (suggestions, followedTopics) => {
+	const matchingFollowedTopics = [];
+	const matchingNotFollowedTopics = [];
+
+	followedTopics.forEach(followedTopic => suggestions.forEach(suggestion => {
+		if (suggestion.id === followedTopic.uuid) {
+			matchingFollowedTopics.push(suggestion);
+		} else {
+			matchingNotFollowedTopics.push(suggestion)
+		}
+	}));
+
+	return { matchingFollowedTopics, matchingNotFollowedTopics };
+}
+
 const suggest = function (suggestions, followedTopics, searchTerm) {
+
 	if (suggestions.length) {
 		suggestions.forEach((suggestion) => {
 			if (suggestion && !suggestion.url){
@@ -11,15 +27,19 @@ const suggest = function (suggestions, followedTopics, searchTerm) {
 				suggestion.url = '/stream/' + suggestion.id;
 			}
 		});
-		return { status: 'suggestions', suggestions };
-	} else {
-		const matchingFollowedTopics = followedTopics
-			.filter(topic => topic.name.toLowerCase().includes(searchTerm.toLowerCase()));
 
-		if(matchingFollowedTopics.length > 0) {
+		const {
+			matchingFollowedTopics,
+			matchingNotFollowedTopics
+		} = separateFollowedAndNot(suggestions, followedTopics);
+
+		if (matchingNotFollowedTopics.length) {
+			return { status: 'suggestions', suggestions: matchingNotFollowedTopics };
+		} else {
 			return { status: 'all-followed', matchingFollowedTopics };
 		}
 
+	} else {
 		return { status: 'no-suggestions' };
 	}
 };
