b0149e159dc3a357fb91696a7d841b8c73550b55
Pavel Pichrt <pavel@heepo.co>
2019-03-11 15:51:55 +0000

    AT-2015: Use a render prop for the follow button to make it work with x-follow-button 0.0.12

--- components/x-topic-search/src/SuggestionList.jsx
+++ components/x-topic-search/src/SuggestionList.jsx
@@ -3,30 +3,38 @@ import { FollowButton } from '@financial-times/x-follow-button';
 import styles from './TopicSearch.scss';
 import classNames from 'classnames';
 
-export default ({ suggestions, searchTerm, csrfToken, followedTopicIds = [] }) => (
-	<ul className={classNames(styles["suggestions"])} aria-live="polite">
-
-		{suggestions.map(suggestion => (
-			<li className={classNames(styles["suggestion"])}
-				key={suggestion.id}
-				data-trackable="myft-topic"
-				data-concept-id={suggestion.id}
-				data-trackable-meta={'{"search-term":"' + searchTerm + '"}'}>
-
-				<a data-trackable="topic-link"
-					className={classNames(styles["suggestion__name"])}
-					href={suggestion.url || `/stream/${suggestion.id}`}>
-					{suggestion.prefLabel}
-				</a>
-
-				<FollowButton
-					conceptId={suggestion.id}
-					conceptName={suggestion.prefLabel}
-					csrfToken={csrfToken}
-					isFollowed={followedTopicIds.includes(suggestion.id)}/>
-
-			</li>
-		))}
-
-	</ul>
+const defaultFollowButtonRender = (concept, csrfToken, followedTopicIds) => (
+	<FollowButton
+		conceptId={concept.id}
+		conceptName={concept.prefLabel}
+		csrfToken={csrfToken}
+		isFollowed={followedTopicIds.includes(concept.id)}
+	/>
 );
+
+export default ({ suggestions, renderFollowButton, searchTerm, csrfToken, followedTopicIds = [] }) => {
+	renderFollowButton = typeof renderFollowButton === 'function' ? renderFollowButton : defaultFollowButtonRender;
+
+	return (
+		<ul className={classNames(styles["suggestions"])} aria-live="polite">
+
+			{suggestions.map(suggestion => (
+				<li className={classNames(styles["suggestion"])}
+					key={suggestion.id}
+					data-trackable="myft-topic"
+					data-concept-id={suggestion.id}
+					data-trackable-meta={'{"search-term":"' + searchTerm + '"}'}>
+
+					<a data-trackable="topic-link"
+						className={classNames(styles["suggestion__name"])}
+						href={suggestion.url || `/stream/${suggestion.id}`}>
+						{suggestion.prefLabel}
+					</a>
+
+					{renderFollowButton(suggestion, csrfToken, followedTopicIds)}
+				</li>
+			))}
+
+		</ul>
+	);
+};
