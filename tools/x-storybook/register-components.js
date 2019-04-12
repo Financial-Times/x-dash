a9ed37e3e386f669ea14a37a4895d8f3b7a7e575
Rob Squires <rob.squires@ft.com>
2019-04-12 17:15:35 +0100

    wippingh

--- tools/x-storybook/register-components.js
+++ tools/x-storybook/register-components.js
@@ -2,6 +2,7 @@ const components = [
 	require('@financial-times/x-teaser/stories'),
 	require('@financial-times/x-increment/stories'),
 	require('@financial-times/x-styling-demo/stories'),
+	require('@financial-times/x-audio/stories'),
 ];
 
 module.exports = components;
