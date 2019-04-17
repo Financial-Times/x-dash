b7e45a09b4d5a19d0a5e870285c5a10c9fb3c6f0
Asuka.Ochi <asuka.ochi@FT-MW5508.local>
2019-04-17 17:05:13 +0100

    Check window exists

--- components/x-audio/src/redux/store.js
+++ components/x-audio/src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { initialState, reducer, middleware } from './player-logic';
 
-const devToolsCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const devToolsCompose = typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 const composeEnhancers = devToolsCompose ? devToolsCompose({}) : compose;
 
 export default () => createStore(
