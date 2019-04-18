af19875badfb6889f90dfb3d4d1ddb209f0b6e24
Asuka.Ochi <asuka.ochi@FT-MW5508.local>
2019-04-18 10:34:42 +0100

    Fix eslint errors

--- components/x-audio/src/redux/store.js
+++ components/x-audio/src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux'
-import { initialState, reducer, middleware } from './player-logic';
+import { reducer, middleware } from './player-logic';
 
 const devToolsCompose = typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 const composeEnhancers = devToolsCompose ? devToolsCompose({}) : compose;
