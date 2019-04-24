092d7e47338341f0e419f8eaa4afaf3dd6384376
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-04-24 16:39:34 +0100

    Remove old Storybook implementation in tools directory

--- tools/x-storybook/register-components.js
+++
@@ -1,7 +0,0 @@
-const components = [
-	require('@financial-times/x-teaser/stories'),
-	require('@financial-times/x-increment/stories'),
-	require('@financial-times/x-styling-demo/stories'),
-];
-
-module.exports = components;
