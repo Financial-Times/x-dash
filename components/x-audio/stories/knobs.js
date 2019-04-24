0e458e995eac1117c3565f1b6595b6c3424e42a5
Dawn Budge <dawn.budge@ft.com>
2019-04-24 15:02:05 +0100

    Fix eslint errors

--- components/x-audio/stories/knobs.js
+++ components/x-audio/stories/knobs.js
@@ -1,10 +1,12 @@
+/*eslint-disable no-console */
 module.exports = (data, { boolean, text }) => ({
 	playing: boolean('Playing', data.playing),
 	loading: boolean('Loading', data.loading),
 	expanded: boolean('Expanded', data.expanded),
 	title: text('Title', data.title),
 	seriesName: text('Series name', data.seriesName),
-	onPlay: () => console.log('Pressed play'),
+	onPlay: () => console.log('Pressed play'), 
 	onPause: () => console.log('Pressed pause'),
 	onClose: () => console.log('Pressed pause')
 });
+/*eslint-enable no-console */
