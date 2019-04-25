948a8c1d30089c7744d64c6e385d025271a1e908
bren <bren@153.io>
2019-04-25 15:30:24 +0100

    less redundant/wrong test names

--- packages/x-test-utils/snapshots.js
+++ packages/x-test-utils/snapshots.js
@@ -4,7 +4,7 @@ const { h } = require('@financial-times/x-engine');
 module.exports = ({ stories, component, presets = { default: {} } }) => {
 	for (const { title, data } of stories) {
 		for (const [preset, options] of Object.entries(presets)) {
-			it(`renders a ${preset} ${title} ${name}`, () => {
+			it(`renders a ${preset} ${title}`, () => {
 				const props = { ...data, ...options };
 				const tree = renderer.create(h(component, props)).toJSON();
 				expect(tree).toMatchSnapshot();
