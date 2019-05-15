7f6c528669abaf30897e26117314ea5eb6497294
Rob Squires <rob.squires@ft.com>
2019-05-15 18:01:16 +0100

    no need to wrap onCloseClick

--- components/x-audio/src/redux/index.jsx
+++ components/x-audio/src/redux/index.jsx
@@ -24,7 +24,6 @@ export default function connectPlayer (Player) {
 		constructor(props) {
 			super(props);
 			this.unsubscribe = store.subscribe(this.storeUpdated.bind(this));
-			this.onCloseClick = this.onCloseClick.bind(this);
 			this.state = initialState;
 		}
 
@@ -34,12 +33,8 @@ export default function connectPlayer (Player) {
 		}
 
 		componentWillUnmount() {
-			this.unsubscribe();
-		}
-
-		onCloseClick() {
 			playerActions.willClose();
-			this.props.onCloseClick();
+			this.unsubscribe();
 		}
 
 		storeUpdated() {
@@ -93,9 +88,8 @@ export default function connectPlayer (Player) {
 		}
 
 		render() {
-			const { title, seriesName, duration, expanded } = this.props;
+			const { title, seriesName, duration, expanded, onCloseClick } = this.props;
 			const { playing, currentTime, loading } = this.state;
-			const onCloseClick = this.onCloseClick;
 			return <Player
 				{...playerActions}
 				{...{
