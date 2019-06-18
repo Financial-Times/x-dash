0a9673cd152d56cc818a02abe136248ad09ad070
Ben Barnett <ben@brushfiredesign.net>
2019-06-18 15:39:07 +0100

    Introduce Marquee component
    To show the full text for titles or series that span a wider area than we can display.

---
+++ components/x-audio/src/components/Marquee.jsx
@@ -0,0 +1,130 @@
+import { h, Component } from '@financial-times/x-engine';
+import * as PropTypes from 'prop-types';
+
+export class Marquee extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			positionOffset: 0,
+			overflowWidth: 0
+		};
+	}
+
+	componentDidMount() {
+		this.measureText();
+		this.start();
+	}
+
+	componentDidUpdate() {
+		this.measureText();
+		this.start();
+	}
+
+	componentWillUnmount() {
+		clearTimeout(this.marqueeTimer);
+		cancelAnimationFrame(this.marqueeRaf);
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if (this.props.text.length != nextProps.text.length) {
+			clearTimeout(this.marqueeTimer);
+			cancelAnimationFrame(this.marqueeRaf);
+			this.setState({ positionOffset: 0 });
+		}
+	}
+
+	render() {
+		const { text, className } = this.props;
+		const style = {
+			display: 'inline-block',
+			transform: `translate3d(-${Math.floor(this.state.positionOffset)}px, 0, 0)`,
+			whiteSpace: 'nowrap',
+			willChange: 'transform'
+		};
+
+		return (
+			<div ref={ref => this.containerElement = ref} className={className} style={{ overflow: 'hidden' }}>
+				<span ref={ref => this.textElement = ref} style={style} title={text}>
+					{text}
+				</span>
+			</div>
+		);
+	}
+
+	measureText() {
+		const container = this.containerElement;
+		const text = this.textElement;
+
+		if (container && text) {
+			const containerWidth = container.offsetWidth;
+			const textWidth = text.offsetWidth;
+			const overflowWidth = textWidth - containerWidth;
+
+			if (overflowWidth !== this.state.overflowWidth) {
+				this.setState({ overflowWidth });
+			}
+		}
+	}
+
+	start() {
+		const needsToAnimate = this.state.overflowWidth > 0;
+
+		if (!needsToAnimate) return;
+
+		const draw = () => {
+			const nextPositionOffset = this.state.positionOffset + 0.5;
+			const isAtEnd = nextPositionOffset > this.state.overflowWidth;
+
+			if (isAtEnd) {
+				this.pauseForLeadOut()
+					.then(() => this.setState({ positionOffset: 0 }))
+					.then(() => this.pauseForLeadIn())
+					.then(() => this.requestAnimationFrame(draw));
+			} else {
+				this.setState({ positionOffset: nextPositionOffset });
+				this.requestAnimationFrame(draw);
+			}
+		};
+
+		const waitForLeadIn = this.state.positionOffset === 0;
+
+		if (waitForLeadIn) {
+			this.pauseForLeadIn().then(() => this.requestAnimationFrame(draw));
+		} else {
+			this.requestAnimationFrame(draw);
+		}
+	}
+
+	pauseForLeadIn() {
+		const { leadInDuration } = this.props;
+
+		return new Promise(resolve => {
+			setTimeout(resolve, leadInDuration);
+		});
+	}
+
+	pauseForLeadOut() {
+		const { leadOutDuration } = this.props;
+		return new Promise(resolve => {
+			setTimeout(resolve, leadOutDuration);
+		});
+	}
+
+	requestAnimationFrame(fn) {
+		cancelAnimationFrame(this.marqueeRaf);
+		this.marqueeRaf = requestAnimationFrame(fn);
+	}
+}
+
+Marquee.defaultProps = {
+	text: '',
+	leadInDuration: 3000,
+	leadOutDuration: 3000
+};
+
+Marquee.propTypes = {
+	text: PropTypes.string,
+	leadInDuration: PropTypes.number,
+	leadOutDuration: PropTypes.number,
+	className: PropTypes.string
+};
