3c446787f70d66e987b8a68d0744f3395be0520b
Ben Barnett <ben@brushfiredesign.net>
2019-07-09 14:36:09 +0100

    Come along, test coverage

--- components/x-audio/src/redux/__tests__/utils.test.js
+++ components/x-audio/src/redux/__tests__/utils.test.js
@@ -1,6 +1,10 @@
 import { shallowEquals } from '../utils';
 
 describe('shallowEquals', () => {
+	test('returns true with empty arguments', () => {
+		expect(shallowEquals()).toBe(true);
+	});
+
 	test('returns true if objects are equal to each other', () => {
 		const first = { key: 'value', anotherKey: 'anotherValue' };
 		const second = { key: 'value', anotherKey: 'anotherValue' };
@@ -13,6 +17,7 @@ describe('shallowEquals', () => {
 		const second = { key: 'another value' };
 
 		expect(shallowEquals(first, second)).toBe(false);
+		expect(shallowEquals(second, first)).toBe(false);
 	});
 
 	test('returns false if object keys differ', () => {
@@ -20,7 +25,17 @@ describe('shallowEquals', () => {
 		const second = { anotherKey: 'value' };
 
 		expect(shallowEquals(first, second)).toBe(false);
+		expect(shallowEquals(second, first)).toBe(false);
 	});
+
+	test('returns false if one object contains keys that another does not', () => {
+		const first = { key: 'value' };
+		const second = { };
+
+		expect(shallowEquals(first, second)).toBe(false);
+		expect(shallowEquals(second, first)).toBe(false);
+	});
+
 
 	test('returns false if comparing objects with a depth greater than one', () => {
 		const first = { key: { deep: 'value' } };
