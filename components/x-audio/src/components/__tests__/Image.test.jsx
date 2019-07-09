2995f4d12c41e8581596c895645612a07081869a
Asuka Ochi <asuka.ochi@ft.com>
2019-07-09 11:10:38 +0100

    Add tests for Image.jsx

---
+++ components/x-audio/src/components/__tests__/Image.test.jsx
@@ -0,0 +1,36 @@
+const { h } = require('@financial-times/x-engine');
+const { shallow } = require('@financial-times/x-test-utils/enzyme');
+const { Image } = require('../Image');
+
+jest.mock('../classnames-helper', () => {
+	return (...classes) => classes.join(' ');
+});
+
+describe('Image', () => {
+	test('should have necessary elements', () => {
+		const props = {
+			imageDataSet: {
+				url: 'image-url',
+				resolutions: []
+			}
+		};
+		const subject = shallow(<Image {...props}/>);
+
+		expect(subject).toMatchSnapshot();
+	});
+
+	test('should set resolutions into srcSet correctly', () => {
+		const props = {
+			imageDataSet: {
+				url: 'image-url',
+				resolutions: [
+					{ url: 'resolution-url-300px', resolution: '300px' },
+					{ url: 'resolution-url-500px', resolution: '500px' }
+				]
+			}
+		};
+		const subject = shallow(<Image {...props}/>);
+
+		expect(subject).toMatchSnapshot();
+	});
+});
