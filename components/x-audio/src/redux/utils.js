96d66d44ac4a38e3d10e6b37427686c8065afd0a
Ben Barnett <ben@brushfiredesign.net>
2019-07-10 09:41:51 +0100

    Neaten up the update function

--- components/x-audio/src/redux/utils.js
+++ components/x-audio/src/redux/utils.js
@@ -1,13 +1,5 @@
 export const idempotentUpdate = (state, updates = {}) => {
-	let neededUpdate = false;
-	const newState = { ...state };
-
-	Object.entries(updates).forEach(([key, value]) => {
-		if (state[key] !== value) {
-			neededUpdate = true;
-			newState[key] = value;
-		}
-	});
-
-	return neededUpdate ? newState : state;
+	return Object.entries(updates).some(([key, value]) => state[key] !== value)
+		? { ...state, ...updates }
+		: state;
 };
