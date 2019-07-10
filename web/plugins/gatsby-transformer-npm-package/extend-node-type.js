440d91dbe8c2c426bf00aa12558dd19702f9ca51
Matt Hinchliffe <matt@maketea.co.uk>
2019-07-10 18:16:10 +0100

    Fix import of JSON GraphQL type as .default is no longer implied

--- web/plugins/gatsby-transformer-npm-package/extend-node-type.js
+++ web/plugins/gatsby-transformer-npm-package/extend-node-type.js
@@ -1,11 +1,11 @@
-const GraphQlJson = require('graphql-type-json');
+const { GraphQLJSONObject } = require('graphql-type-json');
 
 // This allows us to fetch the entire manifest without specifying every field \0/
 module.exports = ({ type }) => {
 	if (type.name === 'NpmPackage') {
 		return {
 			manifest: {
-				type: GraphQlJson,
+				type: GraphQLJSONObject,
 				resolve: (node) => node.manifest
 			}
 		};
