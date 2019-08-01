5bfd0c65c30739a01185c7c90f9d42d457a53ecf
Rob Squires <rob.squires@ft.com>
2019-08-01 12:35:59 +0100

    create x-podcast-launchers component

---
+++ components/x-podcast-launchers/src/PodcastLaunchers.jsx
@@ -0,0 +1,10 @@
+import { h } from '@financial-times/x-engine';
+
+const PodcastLaunchers = (props) => (
+	<div className='x-podcast-launchers'>
+		<h1>Welcome to x-podcast-launchers</h1>
+		<p>{props.message}</p>
+	</div>
+);
+
+export { PodcastLaunchers };
