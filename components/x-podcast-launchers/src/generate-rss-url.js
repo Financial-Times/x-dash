d3d5e9629b3c0d64a65c8d8110d2563df3b21561
Rob Squires <rob.squires@ft.com>
2019-08-02 16:26:22 +0100

    include exmple URL

--- components/x-podcast-launchers/src/generate-rss-url.js
+++ components/x-podcast-launchers/src/generate-rss-url.js
@@ -1,4 +1,6 @@
 import nanoid from 'nanoid';
-export default function generateRSSUrl(seriesId, baseURL, token) {
-	return `${baseURL}/${seriesId}/${token || nanoid(10)}`;
+
+export default function generateRSSUrl(seriesId, acastHost, token) {
+	// ie. http://access.acast.cloud/rss/ft-test/tYPWWHla
+	return `${acastHost}/rss/${seriesId}/${token || nanoid(10)}`;
 }
