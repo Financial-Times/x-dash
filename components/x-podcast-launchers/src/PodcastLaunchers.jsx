eec77ad3b1cd9f87de11434caeaebe3b8c906c75
Asuka Ochi <asuka.ochi@ft.com>
2019-08-06 14:52:08 +0100

    Add Styles

--- components/x-podcast-launchers/src/PodcastLaunchers.jsx
+++ components/x-podcast-launchers/src/PodcastLaunchers.jsx
@@ -1,6 +1,33 @@
 import { h, Component } from '@financial-times/x-engine';
 import generateAppLinks from './generate-app-links';
 import generateRSSUrl from './generate-rss-url';
+import styles from './PodcastLaunchers.css';
+
+const basicButtonStyles = [
+	'o-buttons',
+	'o-buttons--primary',
+	'o-buttons--big'
+].join(' ');
+
+const podcastAppLinkStyles = [
+	basicButtonStyles,
+	styles['podcast-app-link']
+].join(' ');
+
+const rssUrlWrapperStyles = [
+	'o-forms__affix-wrapper',
+	styles['rss-url__wrapper']
+].join(' ');
+
+const rssUrlInputStyles = [
+	'o-forms__text',
+	styles['rss-url__input']
+].join(' ');
+
+const rssUrlCopyButtonWrapperStyles = [
+	'o-forms__suffix',
+	styles['rss-url__copy-button']
+].join(' ');
 
 function mapConceptToAcastSeries (/** concept */) {
 	return 'ft-test';
@@ -10,7 +37,7 @@ class PodcastLaunchers extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			rssUrl: null
+			rssUrl: ''
 		}
 	}
 
@@ -28,20 +55,22 @@ class PodcastLaunchers extends Component {
 	render() {
 		const { rssUrl } = this.state;
 		return (
-			<div className='x-podcast-launchers'>
+			<div className={styles['container']}>
 				{rssUrl && (
-					<ul>
+					<ul className={styles['podcast-app-links__wrapper']}>
 					{generateAppLinks(rssUrl).map(({ name, url }) => (
 						<li key={name}>
-							<a href={url}>{name}</a>
+							<a href={url} className={podcastAppLinkStyles}>{name}</a>
 						</li>
 					))}
-					<li>
-						<input value={rssUrl}/>
-						<button>Copy RSS</button>
-					</li>
 					</ul>
 				)}
+				<div className={rssUrlWrapperStyles}>
+					<input className={rssUrlInputStyles} value={rssUrl} type='text' readOnly/>
+					<div className={rssUrlCopyButtonWrapperStyles}>
+						<button className={basicButtonStyles}>Copy RSS</button>
+					</div>
+				</div>
 			</div>
 		)
 	}
