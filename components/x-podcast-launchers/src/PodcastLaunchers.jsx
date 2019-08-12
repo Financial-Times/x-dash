db3ef9985d07d35b931e2beed01f12cf01688f7b
Asuka Ochi <asuka.ochi@ft.com>
2019-08-12 11:49:54 +0100

    Delete unnecessary div

--- components/x-podcast-launchers/src/PodcastLaunchers.jsx
+++ components/x-podcast-launchers/src/PodcastLaunchers.jsx
@@ -70,25 +70,23 @@ class PodcastLaunchers extends Component {
 		return rssUrl && (
 			<div className={styles['container']}>
 				<h2 className={styles['heading']}>Subscribe on a podcast app</h2>
-				<div>
-					<ul className={styles['podcast-app-links__wrapper']}>
-					{generateAppLinks(rssUrl).map(({ name, url }) => (
-						<li key={name}>
-							<a href={url} className={podcastAppLinkStyles}>{name}</a>
-						</li>
-					))}
-					</ul>
+				<ul className={styles['podcast-app-links__wrapper']}>
+				{generateAppLinks(rssUrl).map(({ name, url }) => (
+					<li key={name}>
+						<a href={url} className={podcastAppLinkStyles}>{name}</a>
+					</li>
+				))}
+				</ul>
 
-					<div className={rssUrlWrapperStyles}>
-						<input className={rssUrlInputStyles} value={rssUrl} type='text' readOnly/>
-						<div className={rssUrlCopyButtonWrapperStyles}>
-							<button
-								className={basicButtonStyles}
-								onClick={copyToClipboard}
-								data-url={rssUrl}>
-								Copy RSS
-							</button>
-						</div>
+				<div className={rssUrlWrapperStyles}>
+					<input className={rssUrlInputStyles} value={rssUrl} type='text' readOnly/>
+					<div className={rssUrlCopyButtonWrapperStyles}>
+						<button
+							className={basicButtonStyles}
+							onClick={copyToClipboard}
+							data-url={rssUrl}>
+							Copy RSS
+						</button>
 					</div>
 				</div>
 
