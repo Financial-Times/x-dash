7ea96216ce7c670a153ba1e2959e98c3561fbe14
Rob Squires <rob.squires@ft.com>
2019-08-14 11:31:27 +0100

    otionally include protocol in app links

--- components/x-podcast-launchers/src/app-links.js
+++ components/x-podcast-launchers/src/app-links.js
@@ -3,26 +3,31 @@ export default
 	{
 		"name": "Apple Podcasts",
 		"template" : "podcast://{url}",
+		"includeProtocol": false,
 		"trackingId": "apple-podcasts",
 	},
 	{
 		"name": "Overcast",
-		"template": "overcast://x-callback-url/add?url=http://{url}",
+		"template": "overcast://x-callback-url/add?url={url}",
+		"includeProtocol": true,
 		"trackingId": "overcast",
 	},
 	{
 		"name": "Pocket Casts",
 		"template": "pktc://subscribe/{url}",
+		"includeProtocol": false,
 		"trackingId": "pocket-casts",
 	},
 	{
 		"name": "Podcast Addict",
 		"template": "podcastaddict://{url}",
+		"includeProtocol": false,
 		"trackingId": "podcast-addict",
 	},
 	{
 		"name": "Acast",
-		"template" : "acast://subscribe/http://{url}",
+		"template" : "acast://subscribe/{url}",
+		"includeProtocol": true,
 		"trackingId": "acast",
 	}
 ]
--- components/x-podcast-launchers/src/generate-app-links.js
+++ components/x-podcast-launchers/src/generate-app-links.js
@@ -2,8 +2,12 @@ import appLinksConfig from './app-links';
 
 export default function generateAppLinks(rssUrl) {
 	return appLinksConfig.map(data => {
+		const url = data.includeProtocol
+					? rssUrl
+					: rssUrl.replace(/^https?:\/\//, '');
+
 		return Object.assign({}, data , {
-			url: data.template.replace(/{url}/, rssUrl)
+			url: data.template.replace(/{url}/, url)
 		})
 	});
 }
