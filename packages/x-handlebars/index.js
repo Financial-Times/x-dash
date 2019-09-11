9d108cd73badaa9641beeac0c44acf21cbe09c7d
Matt Hinchliffe <i-like-robots@users.noreply.github.com>
2019-09-11 14:49:58 +0100

    Remove check for "root" data property from x-handlebars as it may be used safely

--- packages/x-handlebars/index.js
+++ packages/x-handlebars/index.js
@@ -43,15 +43,9 @@ module.exports = (userOptions = {}) => {
 			throw TypeError(`The included component (${hash.component} from ${hash.local || hash.package}) is not a function, it is of type "${type}"`);
 		}
 
-		// "this" is the current Handlebars context. don't merge it in if it's the root context
-		const props = Object.assign(
-			{},
-			this === data.root ? {} : this,
-			mixins,
-			hash
-		);
+		const props = Object.assign({}, this, mixins, hash);
 
-		// if this key is defined they've passed the root context in themselves, which is naughty
+		// Don't allow implementors to pass in the root context when using Express as the "locals" object may include sensitive data.
 		if (props.hasOwnProperty('_locals')) {
 			throw new Error(`The root handlebars context shouldn't be passed to a component, as it may contain sensitive data.`);
 		}
