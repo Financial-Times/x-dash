481ed42fa5ff338a51e52f486de05b3face41cf7
Ben Barnett <ben@brushfiredesign.net>
2019-11-06 15:30:40 +0000

    Satisfy eslint's describe() demands

--- __tests__/snapshots.test.js
+++ __tests__/snapshots.test.js
@@ -21,16 +21,14 @@ for(const pkg of packageDirs) {
 		const { presets = { default: {} } } = require(pkgDir);
 		const name = path.basename(pkg.name);
 
-		describe(`${pkg.name}`, () => {
-			for (const { title, data } of stories) {
-				for (const [preset, options] of Object.entries(presets)) {
-					it(`renders a ${preset} ${title} ${name}`, () => {
-						const props = { ...data, ...options };
-						const tree = renderer.create(h(component, props)).toJSON();
-						expect(tree).toMatchSnapshot();
-					});
-				}
+		for (const { title, data } of stories) {
+			for (const [preset, options] of Object.entries(presets)) {
+				it(`${pkg.name} renders a ${preset} ${title} ${name}`, () => {
+					const props = { ...data, ...options };
+					const tree = renderer.create(h(component, props)).toJSON();
+					expect(tree).toMatchSnapshot();
+				});
 			}
-		});
+		}
 	}
 }
