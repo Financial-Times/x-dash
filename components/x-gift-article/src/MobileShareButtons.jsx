33a89b48161821b08abc0d4d2931f896dbb8b46d
Alice Bartlett <alice.bartlett@gmail.com>
2019-11-27 11:51:51 +0000

    Tidy up MobileShareButtons

--- components/x-gift-article/src/MobileShareButtons.jsx
+++ components/x-gift-article/src/MobileShareButtons.jsx
@@ -2,56 +2,28 @@ import { h } from '@financial-times/x-engine';
 import Title from './Title';
 
 import styles from './MobileShareButtons.scss';
-const containerClassNames = [
-	styles.container
-].join(' ');
-
-const buttonClassNames = [
-	styles.button
-].join(' ');
-
-const whatsappButtonClassNames = [
-	buttonClassNames,
-	styles.whatsapp
-].join(' ');
-
-const facebookClassNames = [
-	styles.facebook
-].join(' ');
-
-const twitterClassNames = [
-	styles.twitter
-].join(' ');
-
-const linkedinClassNames = [
-	styles.linkedin
-].join(' ');
-
-const whatsappClassNames = [
-	styles.whatsapp
-].join(' ');
 
 export default ({ mobileShareLinks }) => (
-	<div className={ containerClassNames }>
+	<div className={ styles.container }>
 		<Title title={ 'Share on Social' }/>
 		<div className={ styles["container-inner"] }>
-			<span className={ buttonClassNames } data-share="facebook">
-				<a className={ facebookClassNames } rel="noopener" href={ mobileShareLinks.facebook } data-trackable="facebook">
+			<span className={ styles.button } data-share="facebook">
+				<a className={ styles.facebook } rel="noopener" href={ mobileShareLinks.facebook } data-trackable="facebook">
 					Facebook <span className={ styles["hidden-button-text"] }>(opens new window)</span>
 				</a>
 			</span>
-			<span className={ buttonClassNames } data-share="twitter">
-				<a className={ twitterClassNames } rel="noopener" href={ mobileShareLinks.twitter } data-trackable="twitter">
+			<span className={ styles.button } data-share="twitter">
+				<a className={ styles.twitter } rel="noopener" href={ mobileShareLinks.twitter } data-trackable="twitter">
 					Twitter <span className={ styles["hidden-button-text"] }>(opens new window)</span>
 				</a>
 			</span>
-			<span className={ buttonClassNames } data-share="linkedin">
-				<a className={ linkedinClassNames } rel="noopener" href={ mobileShareLinks.linkedin } data-trackable="linkedin">
+			<span className={ styles.button } data-share="linkedin">
+				<a className={ styles.linkedin } rel="noopener" href={ mobileShareLinks.linkedin } data-trackable="linkedin">
 					LinkedIn <span className={ styles["hidden-button-text"] }>(opens new window)</span>
 				</a>
 			</span>
-			<span className={ buttonClassNames } data-share="whatsapp">
-				<a className={ whatsappClassNames } rel="noopener" href={ mobileShareLinks.whatsapp } data-trackable="whatsapp">
+			<span className={ styles.button } data-share="whatsapp">
+				<a className={ styles.whatsapp } rel="noopener" href={ mobileShareLinks.whatsapp } data-trackable="whatsapp">
 					Whatsapp <span className={ styles["hidden-button-text"] }>(opens new window)</span>
 				</a>
 			</span>
