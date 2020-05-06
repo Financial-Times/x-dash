caa60418bc06fad8c880b49a40adf63bbe801a4e
Keran Braich <keran.braich@ft.com>
2020-05-06 10:44:49 +0100

    Add styling using Origami component
    This is my attempt at adding styling to our component.
    See x-dash documentation:
    https://financial-times.github.io/x-dash/docs/components/styling

--- components/x-content-block/src/ContentBlock.jsx
+++ components/x-content-block/src/ContentBlock.jsx
@@ -1,8 +1,9 @@
 import { h } from '@financial-times/x-engine';
+import styles from './ContentBlock.scss';
 
 const ContentBlock = (props) => (
 	<div className='x-content-block'>
-		<h1>{props.title}</h1>
+		<h1 className={styles.heading}>{props.title}</h1>
 		{props.isBreakingNews && <span>Breaking News!!!</span>}
 		<p dangerouslySetInnerHTML={{__html: props.content}} />
 	</div>
