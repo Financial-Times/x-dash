025228aa8d6a6f0413fc4624df738a459749e7f5
Keran Braich <keran.braich@ft.com>
2020-05-19 17:33:50 +0100

    Move share buttons into their own component

--- components/x-content-block/src/ContentBlock.jsx
+++ components/x-content-block/src/ContentBlock.jsx
@@ -1,4 +1,5 @@
 import { h } from '@financial-times/x-engine';
+import ShareButtons from './ShareButtons';
 import styles from './ContentBlock.scss';
 
 const ContentBlock = (props) => {
@@ -13,15 +14,6 @@ const ContentBlock = (props) => {
 		keytext,
 		authordisplayname
 	} = props;
-
-	const shareUrl = articleUrl ? new URL(articleUrl) : null;
-	if (shareUrl) {
-		shareUrl.hash = `post-${mid}`;
-	}
-
-	const twitterUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(shareUrl)}&amp;text=${encodeURIComponent(title)}&amp;via=financialtimes`;
-	const facebookUrl = `http://www.facebook.com/sharer.php?u=${encodeURIComponent(shareUrl)}&amp;t=${encodeURIComponent(title)}`;
-	const linkedInUrl = `http://www.linkedin.com/shareArticle?mini=true&amp;url=${encodeURIComponent(shareUrl)}&amp;title=${encodeURIComponent(title)}&amp;source=Financial+Times`;
 
 	return (
 		<article className="live-post" id={`post-${mid}`} data-trackable="live-post">
@@ -40,40 +32,7 @@ const ContentBlock = (props) => {
 			{isBreakingNews && <span>Breaking news!</span>}
 			<div className="live-post__contents" dangerouslySetInnerHTML={{ __html: content }} />
 
-			<div
-				data-o-component="o-share"
-				data-o-share-location={`live-blog-post-${mid}`}
-				className="o-share o-share--small">
-				<ul data-toolbar="share">
-					<li className="o-share__action" data-share="twitter">
-						<a
-							className="o-share__icon o-share__icon--twitter"
-							rel="noopener"
-							href={twitterUrl}
-							data-trackable="twitter">
-							<span className="o-share__text">Share on Twitter (opens new window)</span>
-						</a>
-					</li>
-					<li className="o-share__action" data-share="facebook">
-						<a
-							className="o-share__icon o-share__icon--facebook"
-							rel="noopener"
-							href={facebookUrl}
-							data-trackable="facebook">
-							<span className="o-share__text">Share on Facebook (opens new window)</span>
-						</a>
-					</li>
-					<li className="o-share__action" data-share="linkedin">
-						<a
-							className="o-share__icon o-share__icon--linkedin"
-							rel="noopener"
-							href={linkedInUrl}
-							data-trackable="linkedin">
-							<span className="o-share__text">Share on LinkedIn (opens new window)</span>
-						</a>
-					</li>
-				</ul>
-			</div>
+			<ShareButtons mid={mid} articleUrl={articleUrl} title={title} />
 		</article>
 	);
 };
