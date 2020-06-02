dd78bb88c3a58c6636da5071c562848b8963e43e
tbergmen <5130615+tbergmen@users.noreply.github.com>
2020-06-02 17:57:44 +0100

    Update x-content-block styles

--- components/x-content-block/src/TimeStamp.jsx
+++ components/x-content-block/src/TimeStamp.jsx
@@ -1,10 +1,11 @@
 import { h } from '@financial-times/x-engine';
+import styles from './ContentBlock.scss';
 
 export default ({ publishedTimestamp }) => {
 	return (
 		<time
 			data-o-component="o-date"
-			className="o-date article-info__timestamp"
+			className={`o-date ${styles['content-block__timestamp']}`}
 			itemProp="datePublished"
 			data-o-date-format={publishedTimestamp.format}
 			dateTime={publishedTimestamp.iso}>{publishedTimestamp.formatted}
