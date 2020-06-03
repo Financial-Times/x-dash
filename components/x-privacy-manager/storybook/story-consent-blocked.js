dfbad01cec81726f2f0eed5b7deeb208e29e7452
Oliver Turner <oliver.turner@codedsignal.co.uk>
2020-06-03 15:14:09 +0100

    Make consentApiUrl configurable, allow callbacks on save
    This PR exposes 2 new props:
    1. consentApiUrl
    1. onConsentSavedCallbacks
    
    1. **consentApiUrl**: The Consent API endpoint is dynamically calculated
    on the server based on userId.
    
    1. **onConsentSavedCallbacks**: Apps embedding this module can supply
    callbacks - e.g. saving cookies - to be run on confirmation from the
    Consent Proxy that the user's choice has been persisted.
    
      These callbacks will be run with an object argument of type
      CCPAConsentPayload
    
    Additional changes
    
    - Type definitions added and exposed via package.json#types

---
+++ components/x-privacy-manager/storybook/story-consent-blocked.js
@@ -0,0 +1,18 @@
+const { CONSENT_API, defaults } = require('./data');
+
+exports.title = 'Consent: blocked';
+
+exports.data = {
+	...defaults,
+	consent: false
+};
+
+exports.knobs = Object.keys(exports.data);
+
+exports.fetchMock = (fetchMock) => {
+	fetchMock.mock(CONSENT_API, 200, { delay: 1000 });
+};
+
+// This reference is only required for hot module loading in development
+// <https://webpack.js.org/concepts/hot-module-replacement/>
+exports.m = module;
