5ff7d2bea984aff31f57a493c720745fdfd5f1af
tbergmen <5130615+tbergmen@users.noreply.github.com>
2020-06-11 12:05:55 +0100

    Refactor time string code
    In order to make getTimeString function more portable, I moved the logic of whether to display the time string into the main function.

--- components/x-content-block/src/TimeStamp.jsx
+++ components/x-content-block/src/TimeStamp.jsx
@@ -2,17 +2,7 @@ import { h } from '@financial-times/x-engine';
 import styles from './ContentBlock.scss';
 
 // this function extracts time in HH:mm format from publishedTimestamp
-function getTimeString(publishedTimestamp) {
-	const now = new Date();
-	const date = new Date(publishedTimestamp.iso);
-	const oneDay = 24 * 60 * 60 * 1000;
-
-	// don't display time string if the post is older than one day
-	// because it is already included in the formatted timestamp
-	if (now.getTime() - date.getTime() > oneDay) {
-		return '';
-	}
-
+function getTimeString(date) {
 	// get hours, minutes and seconds into an array
 	const parts = date.toLocaleTimeString().split(':');
 	// return 'HH:mm'
@@ -20,6 +10,16 @@ function getTimeString(publishedTimestamp) {
 }
 
 export default ({ publishedTimestamp }) => {
+	const now = new Date();
+	const date = new Date(publishedTimestamp.iso);
+	const oneDay = 24 * 60 * 60 * 1000;
+
+	// don't display time string if the post is older than one day
+	// because it is already included in the formatted timestamp
+	const timeString = (now.getTime() - date.getTime() < oneDay) ?
+		getTimeString(date) :
+		'';
+
 	return (
 		<div className={styles['content-block__timestamp-container']}>
 			<time
@@ -29,7 +29,7 @@ export default ({ publishedTimestamp }) => {
 				data-o-date-format={publishedTimestamp.format}
 				dateTime={publishedTimestamp.iso}>{publishedTimestamp.formatted}
 			</time>
-			<span className={styles['content-block__timestamp-exact-time']}>{getTimeString(publishedTimestamp)}</span>
+			<span className={styles['content-block__timestamp-exact-time']}>{timeString}</span>
 		</div>
 	);
 };
