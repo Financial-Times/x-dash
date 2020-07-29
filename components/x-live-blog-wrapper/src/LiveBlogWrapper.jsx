a3681d49678bffb13afc25c1ea07739f25248b4f
tbergmen <5130615+tbergmen@users.noreply.github.com>
2020-07-29 17:04:53 +0100

    Listen to live events

--- components/x-live-blog-wrapper/src/LiveBlogWrapper.jsx
+++ components/x-live-blog-wrapper/src/LiveBlogWrapper.jsx
@@ -2,6 +2,47 @@ import { h, Fragment } from '@financial-times/x-engine';
 import { LiveBlogPost } from '@financial-times/x-live-blog-post';
 import { withActions } from '@financial-times/x-interaction';
 
+const parsePost = (event) => {
+	const post = JSON.parse(event.data);
+
+	if (!post || !post.mid || !post.textrendered) {
+		return;
+	}
+
+	const textElement = document.createElement('div');
+	textElement.innerHTML = post.textrendered;
+
+	const titleElement = textElement.querySelector('p strong');
+	const breakingNewsElement = textElement.querySelector('img[src*="breaking_news.gif"]');
+	const isBreakingNews = Boolean(breakingNewsElement);
+
+	if (isBreakingNews) {
+		textElement.removeChild(breakingNewsElement.parentNode);
+	}
+
+	textElement.removeChild(titleElement.parentNode);
+
+	const content = textElement.innerHTML.trim();
+	const title = titleElement.textContent;
+	const publishedTimestamp = (new Date(Number(post.emb) * 1000)).toISOString();
+
+	return {
+		postId: post.mid,
+		author: post.authordisplayname,
+		publishedTimestamp,
+		isBreakingNews,
+		isKeyEvent: Boolean(post.keytext),
+		title,
+		content
+	};
+};
+
+const dispatchLiveUpdateEvent = (eventType, data) => {
+	// consumer app will need to consume this event after the component is rendered. therefore,
+	// we defer dispatching of this event.
+	window.setTimeout(() => document.dispatchEvent(new CustomEvent(eventType, { detail: data })), 0);
+};
+
 const liveBlogWrapperActions = withActions({
 	insertPost (post) {
 		return ({ posts }) => {
@@ -25,79 +66,65 @@ const liveBlogWrapperActions = withActions({
 		return ({ posts }) => {
 			const updatedPosts = posts.filter((post) => post.postId !== postId);
 			return { posts: updatedPosts };
-		}
+		};
 	},
 
-	startListeningToLiveEvents(blogPath) {
-		// TODO: this is a pseudo implmentation at the moment. we need to parse and process posts from the events first.
-
-		const source = new EventSource(`https://next-live-event.ft.com?eventid=${blogPath}&formatted=false`, { withCredentials: true });
-
-		source.addEventListener('msg', this.insertPost);
-
-		source.addEventListener('editmsg', this.updatePost);
-
-		source.addEventListener('delete', this.deletePost);
-
-		// TODO: do we handle live blog status updates in this component?
-		// source.addEventListener('close', updateLiveBlogStatus);
+	startListeningToLiveEvents () {
 	}
-
 });
 
-const LiveBlogWrapper = liveBlogWrapperActions(({ posts = [], blogPath, articleUrl, showShareButtons, actions }) => {
-	const addPostTest = () => {
-		const newPost = {
-			postId: `${Math.random()}`,
-			title: `Title ${Math.random()}`,
-			content: `<p>Post ${Math.random()}</p>`,
-			isBreakingNews: false,
-			publishedTimestamp: '2020-05-13T20:52:28.000Z',
-			articleUrl: 'https://www.ft.com/content/2b665ec7-a88f-3998-8f39-5371f9c791ed',
-			showShareButtons: true,
-		}
+const LiveBlogWrapper = liveBlogWrapperActions(({ posts = [], articleUrl, showShareButtons, blogPath, isLoading, actions }) => {
+	if (isLoading) {
+		// TODO: currently we add event listeners on every render of this component. we need to do it only once.
+		// possibly make this a separate component for client apps to render.
 
-		actions.insertPost(newPost);
+		// const eventSource = new EventSource(`https://next-live-event.ft.com?eventid=${blogPath}&formatted=false`, { withCredentials: true });
+		const eventSource = new EventSource(`http://localhost:3000/events`, { withCredentials: false });
 
-		// window.setTimeout needed here because the internal state of this component will update
-		// after this function returns. this means document updates will occur only after this line
-		// executes.
-		// consumer app will need to consume this event after the component is rendered. therefore,
-		// we defer dispatching of this event.
-		window.setTimeout(() => document.dispatchEvent(new CustomEvent('LiveBlogWrapper.INSERT_POST', { detail: { post: newPost } })), 0);
-	};
+		eventSource.addEventListener('msg', (event) => {
+			const post = parsePost(event);
 
-	const updatePostTest = () => {
-		const updatedPost = {
-			postId: 12346,
-			title: 'Title 2 - Updated',
-			content: '<p>Post 2 - Updated</p>',
-			isBreakingNews: true,
-			publishedTimestamp: '2020-05-13T19:52:28.000Z',
-			articleUrl: 'https://www.ft.com/content/2b665ec7-a88f-3998-8f39-5371f9c791ed',
-			showShareButtons: true,
-		};
+			if (!post) {
+				return;
+			}
 
-		actions.updatePost(updatedPost);
-	};
+			actions.insertPost(post);
+			dispatchLiveUpdateEvent('LiveBlogWrapper.INSERT_POST', { post });
+		});
 
-	const deletePostTest = () => {
-		actions.deletePost(12345);
-	};
+		eventSource.addEventListener('editmsg', (event) => {
+			const post = parsePost(event);
 
-	const postElements = posts.map((post) => <LiveBlogPost key={`live-blog-post-${post.postId}`} {...post} articleUrl={articleUrl} showShareButtons={showShareButtons} />);
+			if (!post) {
+				return;
+			}
+
+			actions.updatePost(post);
+			dispatchLiveUpdateEvent('LiveBlogWrapper.UPDATE_POST', { post });
+		});
+
+		eventSource.addEventListener('delete', (event) => {
+			const post = parsePost(event);
+
+			if (!post) {
+				return;
+			}
+
+			actions.deletePost(post.postId);
+			dispatchLiveUpdateEvent('LiveBlogWrapper.DELETE_POST', { postId: post.postId });
+		});
+
+		// TODO: do we handle live blog status updates in this component?
+		eventSource.addEventListener('close', (event) => {
+			// TODO
+		});
+	}
+
+	const postElements = posts.map((post) => <LiveBlogPost key={`live-blog-post-${post.postId}`} {...post} articleUrl={articleUrl} showShareButtons={showShareButtons}/>);
 	return (
-		<Fragment>
-			<!-- TODO: remove these test buttons -->
-			<div className='x-live-blog-wrapper-test-buttons'>
-				<button onClick={addPostTest}>Test Add Post</button>
-				<button onClick={updatePostTest}>Test Update Post</button>
-				<button onClick={deletePostTest}>Test Delete Post</button>
-			</div>
-			<div className='x-live-blog-wrapper'>
-				{postElements}
-			</div>
-		</Fragment>
+		<div className='x-live-blog-wrapper'>
+			{postElements}
+		</div>
 	);
 });
 
