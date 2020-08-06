41e29f492bd6d9e780c008c7a307baea76cb9e15
tbergmen <5130615+tbergmen@users.noreply.github.com>
2020-08-06 11:44:22 +0100

    Use next-live-event-api v2 endpoint to listen to live updates

--- components/x-live-blog-wrapper/src/LiveEventListener.js
+++ components/x-live-blog-wrapper/src/LiveEventListener.js
@@ -1,37 +1,11 @@
-// TODO: parsing posts will not be needed when we use v2 route on next-live-events-api
 const parsePost = (event) => {
 	const post = JSON.parse(event.data);
 
-	if (!post || !post.mid || !post.textrendered) {
+	if (!post || !post.postId) {
 		return;
 	}
 
-	const textElement = document.createElement('div');
-	textElement.innerHTML = post.textrendered;
-
-	const titleElement = textElement.querySelector('p strong');
-	const breakingNewsElement = textElement.querySelector('img[src*="breaking_news.gif"]');
-	const isBreakingNews = Boolean(breakingNewsElement);
-
-	if (isBreakingNews) {
-		textElement.removeChild(breakingNewsElement.parentNode);
-	}
-
-	textElement.removeChild(titleElement.parentNode);
-
-	const content = textElement.innerHTML.trim();
-	const title = titleElement.textContent;
-	const publishedTimestamp = (new Date(Number(post.emb) * 1000)).toISOString();
-
-	return {
-		postId: post.mid,
-		author: post.authordisplayname,
-		publishedTimestamp,
-		isBreakingNews,
-		isKeyEvent: Boolean(post.keytext),
-		title,
-		content
-	};
+	return post;
 };
 
 const dispatchLiveUpdateEvent = (eventType, data) => {
@@ -40,9 +14,9 @@ const dispatchLiveUpdateEvent = (eventType, data) => {
 	window.setTimeout(() => document.dispatchEvent(new CustomEvent(eventType, { detail: data })), 0);
 };
 
-const listenToLiveBlogEvents = ({ liveBlogWrapperId, blogPath }) => {
+const listenToLiveBlogEvents = ({ liveBlogWrapperElementId, liveBlogArticleUuid }) => {
 	const invokeAction = (action, args) => {
-		const wrapper = document.querySelector(`[data-x-dash-id="${liveBlogWrapperId}"]`);
+		const wrapper = document.querySelector(`[data-x-dash-id="${liveBlogWrapperElementId}"]`);
 		wrapper.dispatchEvent(
 			new CustomEvent(
 				'x-interaction.trigger-action',
@@ -51,11 +25,9 @@ const listenToLiveBlogEvents = ({ liveBlogWrapperId, blogPath }) => {
 		);
 	};
 
-	// TODO: use next-live-events-api v2 routes
-	// const eventSource = new EventSource(`https://next-live-event.ft.com?eventid=${blogPath}&formatted=false`, { withCredentials: true });
-	const eventSource = new EventSource(`http://localhost:3000/events`, { withCredentials: false });
+	const eventSource = new EventSource(`https://next-live-event.ft.com/v2/liveblog/${liveBlogArticleUuid}`, { withCredentials: true });
 
-	eventSource.addEventListener('msg', (event) => {
+	eventSource.addEventListener('insert-post', (event) => {
 		const post = parsePost(event);
 
 		if (!post) {
@@ -66,7 +38,7 @@ const listenToLiveBlogEvents = ({ liveBlogWrapperId, blogPath }) => {
 		dispatchLiveUpdateEvent('LiveBlogWrapper.INSERT_POST', { post });
 	});
 
-	eventSource.addEventListener('editmsg', (event) => {
+	eventSource.addEventListener('update-post', (event) => {
 		const post = parsePost(event);
 
 		if (!post) {
@@ -77,7 +49,7 @@ const listenToLiveBlogEvents = ({ liveBlogWrapperId, blogPath }) => {
 		dispatchLiveUpdateEvent('LiveBlogWrapper.UPDATE_POST', { post });
 	});
 
-	eventSource.addEventListener('delete', (event) => {
+	eventSource.addEventListener('delete-post', (event) => {
 		const post = parsePost(event);
 
 		if (!post) {
