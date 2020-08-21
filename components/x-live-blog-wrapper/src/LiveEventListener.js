51c297f654ee717d52d01de228e2446def97ad51
tbergmen <5130615+tbergmen@users.noreply.github.com>
2020-08-21 16:32:02 +0100

    Explain usage of setTimeout(fn, 0) for delaying event dispatch

--- components/x-live-blog-wrapper/src/LiveEventListener.js
+++ components/x-live-blog-wrapper/src/LiveEventListener.js
@@ -22,9 +22,35 @@ const listenToLiveBlogEvents = ({ liveBlogWrapperElementId, liveBlogPackageUuid 
 	};
 
 	const dispatchLiveUpdateEvent = (eventType, data) => {
-		// consumer app will need to consume this event after the component is rendered.
-		// therefore, we defer dispatching of this event.
-		window.setTimeout(() => wrapper.dispatchEvent(new CustomEvent(eventType, { detail: data })), 0);
+		/*
+		We dispatch live update events to notify the consuming app about added / updated posts.
+
+		Consuming app uses these events to execute tasks like initialising Origami components
+		on the updated elements.
+
+		We want the rendering of the updates in the DOM to finish before dispatching this event,
+		because the consumer needs to reference the updated DOM elements.
+
+		If we dispatch the event in the same event loop with DOM element updates, consumer app
+		will handle the event before the updates are complete.
+
+		window.setTimeout(fn, 0) will defer the execution of the inner function until the
+		current event loop completes, which is enough time for the DOM updates to finish.
+
+		More information can be found in MDN setTimeout documentation. Please refer to
+		"Late timeouts" heading in this page:
+		https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout#Late_timeouts
+
+		> ... the timeout can also fire later when the page (or the OS/browser itself) is busy
+		> with other tasks. One important case to note is that the function or code snippet
+		> cannot be executed until the thread that called setTimeout() has terminated.
+		> ...
+		> This is because even though setTimeout was called with a delay of zero, it's placed on
+		> a queue and scheduled to run at the next opportunity; not immediately.
+		 */
+		window.setTimeout(
+			() => wrapper.dispatchEvent(new CustomEvent(eventType, { detail: data })),
+			0);
 	};
 
 	const eventSource = new EventSource(`https://next-live-event.ft.com/v2/liveblog/${liveBlogPackageUuid}`, { withCredentials: true });
