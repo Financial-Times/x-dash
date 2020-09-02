fb7aaeb680b5b10f1911047ba1432d9ffb88da32
tbergmen <5130615+tbergmen@users.noreply.github.com>
2020-09-02 17:32:35 +0100

    Updates to use this component at the client side
    There are two major updates to use this component at the client side:
    
    ## 1. Add property named `data-x-live-blog-wrapper-id`
    
    We need to render a property named `data-x-live-blog-wrapper-id` in the main element. This is now used to identify the element at the client side for live updates.
    
    Previously we used `data-x-dash-id` for this purpose, but that property is only rendered when using this component with server side rendering.
    
    ## 2. Pass actions object to listenToLiveBlogEvents function
    
    Add `actions` argument to `listenToLiveBlogEvents` function. With server side rendering and hydrating at the client side, component actions can be triggered by dispatching a custom event named `x-interaction.trigger-actions`. This method can't be used with client side rendering. Therefore we need pass the actions object to this function as an argument.
    
    Actions object can be gathered by passing a callback function to the component in a property named actionsRef.
    
    For more information, see https://github.com/Financial-Times/x-dash/tree/master/components/x-interaction#triggering-actions-externally
    
    Usage of this function is now documented in more detail in the readme file.

--- components/x-live-blog-wrapper/src/LiveEventListener.js
+++ components/x-live-blog-wrapper/src/LiveEventListener.js
@@ -9,16 +9,36 @@ const parsePost = (event) => {
 };
 
 
-const listenToLiveBlogEvents = ({ liveBlogWrapperElementId, liveBlogPackageUuid }) => {
-	const wrapper = document.querySelector(`[data-x-dash-id="${liveBlogWrapperElementId}"]`);
+const listenToLiveBlogEvents = ({ liveBlogWrapperElementId, liveBlogPackageUuid, actions }) => {
+	const wrapper = document.querySelector(`[data-live-blog-wrapper-id="${liveBlogWrapperElementId}"]`);
 
 	const invokeAction = (action, args) => {
-		wrapper.dispatchEvent(
-			new CustomEvent(
-				'x-interaction.trigger-action',
-				{ detail: { action, args } }
-			)
-		);
+		if (actions) {
+			// When the component is rendered at the client side, we get a reference to the
+			// actions via setting the actionsRef property.
+			//
+			// In that case 'actions' argument should be passed when calling the
+			// listenToLiveBlogEvents function. We use those actions directly when that
+			// argument is defined.
+			//
+			// For more information:
+			// https://github.com/Financial-Times/x-dash/tree/master/components/x-interaction#triggering-actions-externally
+			actions[action](...args);
+		} else {
+			// When the component is rendered at the server side, we don't have a reference to
+			// the actions object. HydrationWrapper in x-interaction listens to this specific
+			// event and triggers the action supplied in the event detail.
+			//
+			// If no 'actions' argument is passed when calling listenToLiveBlogEvents
+			// function, we assume the component is rendered at the server side and trigger
+			// the actions using this method.
+			wrapper.dispatchEvent(
+				new CustomEvent(
+					'x-interaction.trigger-action',
+					{ detail: { action, args } }
+				)
+			);
+		}
 	};
 
 	const dispatchLiveUpdateEvent = (eventType, data) => {
