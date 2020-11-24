bafbb37b0a4daecc5cfd2d7dd7b15b469524e8b7
Oliver Turner <oliver.turner@codedsignal.co.uk>
2020-11-24 15:23:23 +0000

    Test that data-trackable attrs are customisable

--- components/x-privacy-manager/src/__tests__/helpers.js
+++ components/x-privacy-manager/src/__tests__/helpers.js
@@ -1,6 +1,5 @@
-export const CONSENT_PROXY_HOST = 'https://consent.ft.com';
-export const CONSENT_PROXY_ENDPOINT =
-	'https://consent.ft.com/__consent/consent-record/FTPINK/abcde';
+export const CONSENT_PROXY_HOST = 'https://consent.ft.com'
+export const CONSENT_PROXY_ENDPOINT = 'https://consent.ft.com/__consent/consent-record/FTPINK/abcde'
 
 export const buildPayload = (consent) => ({
 	consentSource: 'consuming-app',
@@ -10,52 +9,53 @@ export const buildPayload = (consent) => ({
 				fow: 'privacyCCPA/H0IeyQBalorD.6nTqqzhNTKECSgOPJCG',
 				lbi: true,
 				source: 'consuming-app',
-				status: consent,
-			},
+				status: consent
+			}
 		},
 		demographicAds: {
 			onsite: {
 				fow: 'privacyCCPA/H0IeyQBalorD.6nTqqzhNTKECSgOPJCG',
 				lbi: true,
 				source: 'consuming-app',
-				status: consent,
-			},
+				status: consent
+			}
 		},
 		programmaticAds: {
 			onsite: {
 				fow: 'privacyCCPA/H0IeyQBalorD.6nTqqzhNTKECSgOPJCG',
 				lbi: true,
 				source: 'consuming-app',
-				status: consent,
-			},
-		},
+				status: consent
+			}
+		}
 	},
 	cookieDomain: '.ft.com',
-	formOfWordsId: 'privacyCCPA',
-});
+	formOfWordsId: 'privacyCCPA'
+})
 
 export function checkPayload(opts, expected) {
-	const consents = JSON.parse(String(opts.body)).data;
+	const consents = JSON.parse(String(opts.body)).data
 
-	let worked = true;
+	let worked = true
 	for (const category in consents) {
-		worked = worked && consents[category].onsite.status === expected;
+		worked = worked && consents[category].onsite.status === expected
 	}
-	return worked;
+	return worked
 }
 
 export const defaultProps = {
 	userId: 'abcde',
+	legislationId: 'ccpa',
 	consentProxyApiHost: CONSENT_PROXY_HOST,
 	consentSource: 'consuming-app',
 	referrer: 'www.ft.com',
 	cookieDomain: '.ft.com',
 	fow: {
 		id: 'privacyCCPA',
-		version: 'H0IeyQBalorD.6nTqqzhNTKECSgOPJCG',
+		version: 'H0IeyQBalorD.6nTqqzhNTKECSgOPJCG'
 	},
 	actions: {
 		onConsentChange: jest.fn(() => {}),
-		sendConsent: jest.fn().mockReturnValue({ _response: { ok: undefined } }),
-	},
-};
+		sendConsent: jest.fn().mockReturnValue({ _response: { ok: undefined } })
+	}
+}
