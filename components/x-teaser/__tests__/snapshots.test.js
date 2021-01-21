42da1eaeb6d76c9d1fab3f6e3b4a2840fefd3bb3
Jennifer Johnson <jkerr321@gmail.com>
2021-01-21 13:13:14 +0000

    check for image url and create snapshot test

--- components/x-teaser/__tests__/snapshots.test.js
+++ components/x-teaser/__tests__/snapshots.test.js
@@ -5,6 +5,7 @@ const { Teaser, presets } = require('../')
 const storyData = {
 	article: require('../__fixtures__/article.json'),
 	'article-with-data-image': require('../__fixtures__/article-with-data-image.json'),
+	'article-with-missing-image-url': require('../__fixtures__/article-with-missing-image-url.json'),
 	opinion: require('../__fixtures__/opinion.json'),
 	contentPackage: require('../__fixtures__/content-package.json'),
 	packageItem: require('../__fixtures__/package-item.json'),
