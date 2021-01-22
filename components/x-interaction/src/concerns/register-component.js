bfd919e953d0716e6c9125256c33adc450fe55be
circle-auto-deploy <estefania.morton@ft.com>
2021-01-22 15:01:06 +0000

    remove reliance on component displayName in registerComponent

--- components/x-interaction/src/concerns/register-component.js
+++ components/x-interaction/src/concerns/register-component.js
@@ -1,9 +1,15 @@
 const registeredComponents = {}
 
-export function registerComponent(component) {
-	registeredComponents[component.wrappedDisplayName] = component
+export function registerComponent(component, name) {
+	const xInteractionName = Symbol('x-interaction-name')
+	component[xInteractionName] = name
+	registeredComponents[name] = component
 }
 
-export function getComponent(name) {
+export function getComponent(component) {
+	const xInteractionSymbol = Object.getOwnPropertySymbols(component).find(
+		(key) => key.toString() === `Symbol(x-interaction-name)`
+	)
+	const name = component[xInteractionSymbol]
 	return registeredComponents[name]
 }
