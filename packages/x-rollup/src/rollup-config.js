51f7cce32fb10ceed7f21bed1548a13e8bc8a72a
Oliver Turner <oliver.turner@codedsignal.co.uk>
2021-03-02 11:56:41 +0000

    Quote target versions for consistency

--- packages/x-rollup/src/rollup-config.js
+++ packages/x-rollup/src/rollup-config.js
@@ -28,7 +28,7 @@ module.exports = ({ input, pkg }) => {
 				plugins: [
 					babel(
 						babelConfig({
-							targets: { node: 12 }
+							targets: { node: '12' }
 						})
 					),
 					...plugins
@@ -46,7 +46,7 @@ module.exports = ({ input, pkg }) => {
 				plugins: [
 					babel(
 						babelConfig({
-							targets: { node: 12 }
+							targets: { node: '12' }
 						})
 					),
 					...plugins
