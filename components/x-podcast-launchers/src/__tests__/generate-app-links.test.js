1c8787eccf1dc9396be20dca89db1e7c0a94daea
Kara Brightwell <kara@153.io>
2022-02-25 15:33:44 +0000

    delete x-podcast-launchers
    it's no longer used since it was removed from next-article last year (financial-times/next-article#4113) and the podcast it was built for became free

--- components/x-podcast-launchers/src/__tests__/generate-app-links.test.js
+++
@@ -1,8 +0,0 @@
-import generateAppLinks from '../generate-app-links'
-
-describe('generate-app-links', () => {
-	it('should generate each app link with the urlencoded RSS url', () => {
-		const rssUrl = 'https://acast.access/rss/ft-news/&£@1234'
-		expect(generateAppLinks(rssUrl)).toMatchSnapshot()
-	})
-})
