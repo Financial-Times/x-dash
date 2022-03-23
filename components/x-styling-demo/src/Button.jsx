8eb186e8ea6fe21aae285c7ffd73f1ef71ba353e
Kara Brightwell <kara@153.io>
2022-03-23 11:17:09 +0000

    unmodule styling demo

--- components/x-styling-demo/src/Button.jsx
+++ components/x-styling-demo/src/Button.jsx
@@ -1,13 +1,13 @@
 import { h } from '@financial-times/x-engine'
-import buttonStyles from './Button.css'
 import classNames from 'classnames'
 
 export const Button = ({ large, danger }) => (
 	<button
-		className={classNames(buttonStyles.button, {
-			[buttonStyles.large]: large,
-			[buttonStyles.danger]: danger
-		})}>
+		className={classNames('button', {
+			'button--large': large,
+			'button--danger': danger
+		})}
+	>
 		Click me!
 	</button>
 )
