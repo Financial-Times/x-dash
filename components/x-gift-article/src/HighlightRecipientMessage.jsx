023fca1a944d2f6d9ebc19f60c2f16807bddc89b
Anna Draganova <anna.draganova@ft.com>
2023-06-05 08:53:11 +0300

    Mobile design improvements

--- components/x-gift-article/src/HighlightRecipientMessage.jsx
+++ components/x-gift-article/src/HighlightRecipientMessage.jsx
@@ -6,14 +6,14 @@ export default ({ handleSaveAnnotations, handleCloseRecipientMessage }) => {
 			<p className="x-gift-article__highlight-recipient-message__title">Share the highlighted article</p>
 			<div className="x-gift-article__highlight-recipient-message__divider"></div>
 			<p className="o-typography-professional o-typography-inverse x-gift-article__highlight-recipient-message__description">
-				If you wish to share the highlighted article,
+				If you wish to share the highlighted article,{' '}
 				<button
 					type="button"
 					className="o-typography-link x-gift-article__highlight-recipient-message__save-highlights-button"
 					onClick={handleSaveAnnotations}
 				>
 					save the highlights
-				</button>
+				</button>{' '}
 				as your own before creating the link.
 			</p>
 			<button
