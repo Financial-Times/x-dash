fde8b189a045ea3b9adbfa1d4d7034ef8ec84f43
Abshir <abshir.ahmed@ft.com>
2023-09-26 14:53:22 +0100

    Update the link in the “Send to multiple people with Advanced Sharing” copy
    This change redirects the users to the request access page of Advanced Sharing, change required by the marketing team.

--- components/x-gift-article/src/FooterMessage.jsx
+++ components/x-gift-article/src/FooterMessage.jsx
@@ -37,7 +37,7 @@ export const FooterMessage = ({
 				Send to multiple people with{' '}
 				<a
 					className="o-typography-professional o-typography-link"
-					href="https://enterprise.ft.com/ft-enterprise-sharing-trial"
+					href="https://professional.ft.com/advanced-sharing-request-access"
 					target="_blank"
 					rel="noreferrer"
 				>
