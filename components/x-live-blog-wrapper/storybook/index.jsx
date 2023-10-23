8ee7f6f9ab9b33df3fa7a84438215692b46ee30c
Juan Sanchez Alcala <102036944+juanSanchezAlcala@users.noreply.github.com>
2023-10-23 13:55:04 +0200

    feat: truncated posts in live blog

--- components/x-live-blog-wrapper/storybook/index.jsx
+++ components/x-live-blog-wrapper/storybook/index.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { LiveBlogWrapper } from '../src/LiveBlogWrapper'
+import './index.scss'
 import '@financial-times/x-live-blog-post/src/LiveBlogPost.scss'
 import BuildService from '../../../.storybook/build-service'
-
+import oTracking from '@financial-times/o-tracking';
+import Expander  from '@financial-times/o-expander'
 const dependencies = {
 	'o-fonts': '^5.3.0'
 }
@@ -100,6 +102,79 @@ const defaultProps = {
 	}
 }
 
+
+
+const defaultPropsTruncated = {
+	message: 'Test',
+	id: 'live-blog-wrapper',
+	truncated: true,
+	postTrackerConfig: {
+		onEntersViewport: () => {},
+		onRead: () => {},
+		onError: () => {},
+		usePostTracker: true
+	},
+	posts: [
+		{
+			id: 12345,
+			title: 'Title 1',
+			bodyHTML: `<div data-trackable="header-menu">
+			<span>
+				<div data-trackable="country-selector">
+					<ul>
+						<li data-trackable="china"><button>China</button></li>
+					</ul>
+				</div>
+			</span>
+		</div><p>Post 1</p><p>Post 1</p><p>Post 1</p><p>Post 1</p><p>Post 1</p><p>Post 1</p>`,
+			isBreakingNews: false,
+			publishedDate: '2020-05-13T18:52:28.000Z',
+			articleUrl: 'https://www.ft.com/content/2b665ec7-a88f-3998-8f39-5371f9c791ed',
+			showShareButtons: true
+		},
+		{
+			id: 12346,
+			title: 'Title 2',
+			bodyHTML: '<p>Post 2</p><p>Post 2</p><p>Post 2</p><p>Post 2</p><p>Post 2</p><p>Post 2</p><p>Post 2</p>',
+			isBreakingNews: true,
+			publishedDate: '2020-05-13T19:52:28.000Z',
+			articleUrl: 'https://www.ft.com/content/2b665ec7-a88f-3998-8f39-5371f9c791ed',
+			showShareButtons: true
+		},
+		{
+			id: 12347,
+			title: 'Title 3',
+			bodyHTML: '<p>Post 3</p>',
+			isBreakingNews: false,
+			publishedDate: '2020-05-13T20:52:28.000Z',
+			articleUrl: 'https://www.ft.com/content/2b665ec7-a88f-3998-8f39-5371f9c791ed',
+			showShareButtons: true
+		},
+		{
+			id: 12348,
+			title: 'Title 4',
+			bodyHTML: '<p>Post 4</p><p>Post 4</p><p>Post 4</p><p>Post 4</p><p>Post 4</p><p>Post 4</p>',
+			isBreakingNews: false,
+			publishedDate: '2020-05-13T20:52:28.000Z',
+			articleUrl: 'https://www.ft.com/content/2b665ec7-a88f-3998-8f39-5371f9c791ed',
+			showShareButtons: true
+		},
+		{
+			id: 12349,
+			title: 'Title 5',
+			bodyHTML: '<p>Post 5</p><p>Post 5</p><p>Post 5</p><p>Post 5</p><p>Post 5</p>',
+			isBreakingNews: false,
+			publishedDate: '2020-05-13T20:52:28.000Z',
+			articleUrl: 'https://www.ft.com/content/2b665ec7-a88f-3998-8f39-5371f9c791ed',
+			showShareButtons: true
+		}
+	],
+	ads: {
+		1: <Ad />,
+		2: <Ad />
+	}
+}
+
 export default {
 	title: 'x-live-blog-wrapper',
 	parameters: {
@@ -115,3 +190,20 @@ export const ContentBody = (args) => (
 )
 
 ContentBody.args = defaultProps
+
+export const ContentBodyTruncated = (args) => {
+	useEffect(() => {console.log("INIT O TRACKER")
+		oTracking.init({test: true})
+		oTracking.click.init("truncated-post");
+		
+			const posts = document.querySelectorAll('.x-live-blog-wrapper .truncated-post')
+			if (posts) posts.forEach((post) => Expander.init(post))
+		
+	})
+	return <div className="story-container">
+		<BuildService dependencies={dependencies} />
+		<LiveBlogWrapper {...args} />
+	</div>
+}
+
+ContentBodyTruncated.args = defaultPropsTruncated
