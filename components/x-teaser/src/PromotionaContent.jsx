2b7253bc55bbf5d358fe14f1552430a7999f64ea
metart43 <artem.metelskyi@ft.com>
2024-04-15 12:41:34 +0100

    feat: rename promo content wrapper to match image container
    This change applies the existing logic added to the image container. So we can avoiding duplication of this logic for the promotional content wrapper in the ft-app.

--- components/x-teaser/src/PromotionaContent.jsx
+++ components/x-teaser/src/PromotionaContent.jsx
@@ -1,7 +1,7 @@
 import { h } from '@financial-times/x-engine'
 
 const PromotionalContent = ({ promotionalContent }) => (
-	<div className="o-teaser__promotional-content">{promotionalContent}</div>
+	<div className="o-teaser__image-container js-teaser-image-container">{promotionalContent}</div>
 )
 
 export default PromotionalContent
