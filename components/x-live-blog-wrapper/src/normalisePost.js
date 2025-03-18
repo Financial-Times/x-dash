c2b8988fe112b55e7ef4042b69c80d3d6570cb4c
Kara Brightwell <kara@153.io>
2025-03-18 10:03:38 +0000

    chore: remove unused live blog components

--- components/x-live-blog-wrapper/src/normalisePost.js
+++
@@ -1,9 +0,0 @@
-// Remove this helper when we no longer need to handle incoming WordPress events.
-const normalisePost = (post) => {
-	if (post && !post.id && post.postId) {
-		post.id = post.postId
-	}
-	return post
-}
-
-export { normalisePost }
