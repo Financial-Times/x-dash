0a5a274a1f566d6718d7757e95d4bf6973026f3b
Asuka Ochi <asuka.ochi@ft.com>
2025-11-03 12:38:46 +0000

    fix: Add new o-teaser-labels classes
    This class styling hasn't existed in o-teaser yet.
    o-teaser__timestamp doesn't make sense as the class name therefore we are going to remove the class
    after we added the same stylings to the new classes

--- components/x-teaser/src/ScoopLabel.jsx
+++ components/x-teaser/src/ScoopLabel.jsx
@@ -1,9 +1,8 @@
 import { h } from '@financial-times/x-engine'
 
 export default function ScoopLabel() {
-	// TODO, create o-teaser__labels o-teaser__labels--scoop and swap with o-teaser__timestamp later
 	return (
-		<div className="o-teaser__timestamp">
+		<div className="o-teaser__labels o-teaser__labels--scoop">
 			<span className="o-labels o-labels--content-scoop">Exclusive</span>
 			<span className="o3-visually-hidden">&nbsp;content</span>
 		</div>
